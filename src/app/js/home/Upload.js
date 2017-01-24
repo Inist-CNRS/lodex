fc7a4c3a80058d8000a391ce2166e8a012a57d3c
ThieryMichel <thiery@marmelab.com>
2017-01-24 10:26:42 +0100

    tyle upload button

--- src/app/js/home/Upload.js
+++ src/app/js/home/Upload.js
@@ -1,19 +1,45 @@
 import React, { PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import ArchiveIcon from 'material-ui/svg-icons/content/archive';
 
 import { uploadFile } from './homeActions';
 
 export const UploadComponent = ({ onFileLoad }) => (
-    <FlatButton
-        containerElement="label"
+    <div
+        style={{
+            position: 'relative',
+            margin: 100,
+        }}
     >
-        <input
-            type="file"
-            onChange={e => onFileLoad(e.target.files[0])}
-        />
-    </FlatButton>
+        <RaisedButton
+            containerElement="label"
+            secondary
+            style={{
+                width: 500,
+                height: 200,
+                color: 'white',
+            }}
+            icon={<ArchiveIcon />}
+        >
+            <input
+                type="file"
+                onChange={e => onFileLoad(e.target.files[0])}
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    opacity: 0,
+                    width: '100%',
+                    cursor: 'pointer',
+                }}
+            />
+            Import file
+        </RaisedButton>
+    </div>
 );
 
 UploadComponent.propTypes = {
