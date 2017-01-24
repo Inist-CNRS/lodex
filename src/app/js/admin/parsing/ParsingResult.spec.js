a5a0feeafa2e7963d21c9f33e8deced25dd6dc24
ThieryMichel <thiery@marmelab.com>
2017-01-24 18:01:15 +0100

    fix app test

--- src/app/js/admin/parsing/ParsingResult.spec.js
+++ src/app/js/admin/parsing/ParsingResult.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import expect from 'expect';
 import { shallow } from 'enzyme';
-import ParsingErrors from './ParsingErrors';
 import ParsingExcerpt from './ParsingExcerpt';
 import ParsingSummary from './ParsingSummary';
 
@@ -13,15 +12,12 @@ describe('<ParsingResult />', () => {
             failedLines={[{}, {}]}
             p={{ t: () => {} }}
             totalLoadedLines={24}
-            totalParsedLines={100}
         />);
 
         const parsingSummary = wrapper.find(ParsingSummary).at(0);
 
         expect(parsingSummary.prop('showErrors')).toEqual(false);
-        expect(parsingSummary.prop('totalFailedLines')).toEqual(2);
         expect(parsingSummary.prop('totalLoadedLines')).toEqual(24);
-        expect(parsingSummary.prop('totalParsedLines')).toEqual(100);
     });
 
     it('should render the ParsingExcerpt if showErrors is false', () => {
@@ -31,29 +27,11 @@ describe('<ParsingResult />', () => {
             failedLines={[]}
             p={{ t: () => {} }}
             totalLoadedLines={24}
-            totalParsedLines={100}
         />);
 
         const parsingExcerpt = wrapper.find(ParsingExcerpt).at(0);
 
         expect(parsingExcerpt.prop('columns')).toEqual(['foo']);
         expect(parsingExcerpt.prop('lines')).toEqual(['bar']);
-    });
-
-    it('should render the ParsingExcerpt if showErrors is true', () => {
-        const wrapper = shallow(<ParsingResult
-            excerptColumns={['foo']}
-            excerptLines={['bar']}
-            failedLines={['error']}
-            p={{ t: () => {} }}
-            totalLoadedLines={24}
-            totalParsedLines={100}
-        />);
-
-        wrapper.setState({ showErrors: true });
-
-        const parsingErrors = wrapper.find(ParsingErrors).at(0);
-
-        expect(parsingErrors.prop('lines')).toEqual(['error']);
     });
 });
--- src/app/js/admin/parsing/ParsingSummary.spec.js
+++ src/app/js/admin/parsing/ParsingSummary.spec.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import expect, { createSpy } from 'expect';
+import expect from 'expect';
 import { shallow } from 'enzyme';
 import { ListItem } from 'material-ui/List';
-import { red400 } from 'material-ui/styles/colors';
 
 import { ParsingSummaryComponent as ParsingSummary } from './ParsingSummary';
 import ParsingSummaryItem from './ParsingSummaryItem';
@@ -11,64 +10,9 @@ describe('<ParsingSummary />', () => {
     it('should render the ListItem for totalLoadedLines', () => {
         const wrapper = shallow(<ParsingSummary
             p={{ t: key => key }}
-            totalFailedLines={42}
             totalLoadedLines={24}
-            totalParsedLines={100}
         />);
         const line = wrapper.find(ListItem).at(0);
         expect(line.prop('primaryText')).toEqual(<ParsingSummaryItem count={24} label="total lines" />);
-    });
-
-    it('should render the ListItem for totalParsedLines', () => {
-        const wrapper = shallow(<ParsingSummary
-            p={{ t: key => key }}
-            totalFailedLines={42}
-            totalLoadedLines={24}
-            totalParsedLines={100}
-        />);
-        const line = wrapper.find(ListItem).at(1);
-        expect(line.prop('primaryText')).toEqual(<ParsingSummaryItem count={100} label="successfully parsed" />);
-    });
-
-    it('should render the ListItem for totalFailedLines', () => {
-        const wrapper = shallow(<ParsingSummary
-            p={{ t: key => key }}
-            totalFailedLines={42}
-            totalLoadedLines={24}
-            totalParsedLines={100}
-        />);
-        const line = wrapper.find(ListItem).at(2);
-        expect(line.prop('primaryText'))
-            .toEqual(<ParsingSummaryItem color={red400} count={42} label="with errors" />);
-    });
-
-    it('should call onShowErrors when totalFailedLines is clicked', () => {
-        const onShowErrors = createSpy();
-
-        const wrapper = shallow(<ParsingSummary
-            onShowErrors={onShowErrors}
-            p={{ t: key => key }}
-            totalFailedLines={42}
-            totalLoadedLines={24}
-            totalParsedLines={100}
-        />);
-        wrapper.find(ListItem).at(2).simulate('click');
-
-        expect(onShowErrors).toHaveBeenCalled();
-    });
-
-    it('should call onShowErrors when totalParsedLines is clicked', () => {
-        const onShowExcerpt = createSpy();
-
-        const wrapper = shallow(<ParsingSummary
-            onShowExcerpt={onShowExcerpt}
-            p={{ t: key => key }}
-            totalFailedLines={42}
-            totalLoadedLines={24}
-            totalParsedLines={100}
-        />);
-        wrapper.find(ListItem).at(1).simulate('click');
-
-        expect(onShowExcerpt).toHaveBeenCalled();
     });
 });
