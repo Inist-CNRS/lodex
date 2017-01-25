7b5f663a702df2b96fb637fa15b0196bac7a8378
ThieryMichel <thiery@marmelab.com>
2017-01-25 16:26:14 +0100

    add asyncCompose utils

---
+++ src/common/lib/asyncCompose.js
@@ -0,0 +1,7 @@
+export default funcs =>
+funcs
+.reduce(
+    (composedTransformer, transformer) => (...args) =>
+    composedTransformer(...args).then(transformer),
+    v => Promise.resolve(v),
+);
