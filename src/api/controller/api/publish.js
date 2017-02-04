dc2e4d1c937065d91f8e20c470b85fbe642ed4b7
ThieryMichel <thiery@marmelab.com>
2017-02-04 21:23:42 +0100

    add test on publish

--- src/api/controller/api/publish.js
+++ src/api/controller/api/publish.js
@@ -1,56 +1,81 @@
 import omit from 'lodash.omit';
+import Koa from 'koa';
 
 /* eslint no-await-in-loop: off */
 import getDocumentTransformer from '../../../common/getDocumentTransformer';
 
-export const tranformAllDocument = async (count, findLimitFromSkip, insertMany, transformer) => {
+const app = new Koa();
+
+export const tranformAllDocument = async (count, findLimitFromSkip, insertBatch, transformer) => {
     let handled = 0;
     while (handled < count) {
         const dataset = await findLimitFromSkip(100, handled);
         const transformedDataset = await Promise.all(dataset.map(transformer));
-        await insertMany(transformedDataset);
+        await insertBatch(transformedDataset);
         handled += dataset.length;
     }
 };
 
-export const publishMiddleware = async (ctx) => {
+export const addTransformResultToDoc = transform => async doc => ({
+    ...omit(doc, ['_id']),
+    ...await transform(doc),
+});
+
+export const addUriToTransformResult = transformDocument => async doc => ({
+    ...await transformDocument(doc),
+    uri: doc.uri,
+});
+
+export const doPublish = async (ctx) => {
     const count = await ctx.dataset.count({});
 
     const columns = await ctx.field.findAll();
 
     const uriIndex = columns.findIndex(col => col.name === 'uri');
     const getUri = ctx.getDocumentTransformer({ env: 'node', dataset: ctx.dataset }, [columns[uriIndex]]);
-    const addUri = async doc => ({
-        ...omit(doc, ['_id']),
-        ...await getUri(doc),
-    });
+    const addUri = ctx.addTransformResultToDoc(getUri);
 
-    await tranformAllDocument(
+    await ctx.tranformAllDocument(
         count,
-        ctx.dataset.findLimitFromSkip.bind(ctx.dataset),
-        ctx.uriDataset.insertMany.bind(ctx.uriDataset),
+        ctx.dataset.findLimitFromSkip,
+        ctx.uriDataset.insertBatch,
         addUri,
     );
 
     const transformDocument = ctx
-        .getDocumentTransformer({ env: 'node', dataset: ctx.uriDataset }, columns);
+        .getDocumentTransformer({
+            env: 'node',
+            dataset: ctx.uriDataset,
+        }, columns.filter((_, index) => index !== uriIndex));
 
-    const transformDocumentWithUri = async doc => ({
-        ...await transformDocument(doc),
-        uri: doc.uri,
-    });
+    const transformDocumentAndKeepUri = ctx.addUriToTransformResult(transformDocument);
 
-    await tranformAllDocument(
+    await ctx.tranformAllDocument(
         count,
-        ctx.uriDataset.findLimitFromSkip.bind(ctx.uriDataset),
-        ctx.publishedDataset.insertMany.bind(ctx.publishedDataset),
-        transformDocumentWithUri,
+        ctx.uriDataset.findLimitFromSkip,
+        ctx.publishedDataset.insertBatch,
+        transformDocumentAndKeepUri,
     );
 
     ctx.redirect('/api/publication');
 };
 
-export default async (ctx) => {
+export const tryPublish = async (ctx, next) => {
+    ctx.tranformAllDocument = tranformAllDocument;
     ctx.getDocumentTransformer = getDocumentTransformer;
-    await publishMiddleware(ctx);
+    ctx.addTransformResultToDoc = addTransformResultToDoc;
+    ctx.addUriToTransformResult = addUriToTransformResult;
+    try {
+        await next();
+    } catch (error) {
+        await ctx.uriDataset.remove({});
+        await ctx.publishedDataset.remove({});
+        throw error;
+    }
 };
+
+app.use(tryPublish);
+
+app.use(doPublish);
+
+export default app;
--- src/api/controller/api/publish.spec.js
+++ src/api/controller/api/publish.spec.js
@@ -1,61 +1,180 @@
 /* eslint max-len: off */
 import expect, { createSpy } from 'expect';
-import { publishMiddleware as publish } from './publish';
+
+import {
+    doPublish,
+    tryPublish,
+    tranformAllDocument,
+    addTransformResultToDoc,
+    addUriToTransformResult,
+} from './publish';
+import getDocumentTransformer from '../../../common/getDocumentTransformer';
 
 describe('publish', () => {
-    const dataset = [{ foo: 'foo1', bar: 'bar1' }, { foo: 'foo2', bar: 'bar2' }];
-    const fields = ['field1', 'field2'];
-    const transformDocument = createSpy().andReturn(Promise.resolve('transformedDocument'));
+    describe('doPublish', () => {
+        const fields = ['uri', 'field1', 'field2'].map(name => ({ name }));
 
-    const ctx = {
-        dataset: {
-            count: createSpy().andReturn(Promise.resolve(201)),
-            findLimitFromSkip: createSpy().andReturn(dataset),
-        },
-        ezMasterConfig: { naan: '55555' },
-        field: {
-            insertMany: createSpy(),
-            findAll: createSpy().andReturn(fields),
-        },
-        getDocumentTransformer: createSpy().andReturn(transformDocument),
-        publishedDataset: {
-            insertMany: createSpy(),
-        },
-        redirect: createSpy(),
-    };
+        const ctx = {
+            dataset: {
+                count: createSpy().andReturn(Promise.resolve('count')),
+                findLimitFromSkip: 'dataset.findLimitFromSkip()',
+            },
+            uriDataset: {
+                insertBatch: 'uriDataset.insertBatch()',
+                findLimitFromSkip: 'uriDataset.findLimitFromSkip()',
+            },
+            field: {
+                findAll: createSpy().andReturn(fields),
+            },
+            getDocumentTransformer: createSpy().andReturn('transformDocument()'),
+            addUriToTransformResult: createSpy().andReturn('transformDocumentAndKeepUri()'),
+            addTransformResultToDoc: createSpy().andReturn('addUri()'),
+            publishedDataset: {
+                insertBatch: 'publishedDataset.insertBatch()',
+            },
+            tranformAllDocument: createSpy(),
+            redirect: createSpy(),
+        };
 
-    beforeEach(async () => {
-        await publish(ctx);
+        before(async () => {
+            await doPublish(ctx);
+        });
+
+        it('should get the total number of items in the original dataset', () => {
+            expect(ctx.dataset.count).toHaveBeenCalledWith({});
+        });
+
+        it('should get the columns', () => {
+            expect(ctx.field.findAll).toHaveBeenCalled();
+        });
+
+        it('should call getDocumentTransformer to get the uri transformers', () => {
+            expect(ctx.getDocumentTransformer).toHaveBeenCalledWith({ env: 'node', dataset: ctx.dataset }, [fields[0]]);
+        });
+
+        it('should call ctx.addTransformResultToDoc with transformDocument', () => {
+            expect(ctx.addTransformResultToDoc).toHaveBeenCalledWith('transformDocument()');
+        });
+
+        it('should call ctx.tranformAllDocument', () => {
+            expect(ctx.tranformAllDocument).toHaveBeenCalledWith('count', 'dataset.findLimitFromSkip()', 'uriDataset.insertBatch()', 'addUri()');
+        });
+
+        it('should call getDocumentTransformer with all other fields', () => {
+            expect(ctx.getDocumentTransformer).toHaveBeenCalledWith({ env: 'node', dataset: ctx.uriDataset }, fields.slice(1));
+        });
+
+        it('should call ctx.addUriToTransformResult with transformDocument', () => {
+            expect(ctx.addUriToTransformResult).toHaveBeenCalledWith('transformDocument()');
+        });
+
+        it('should call ctx.tranformAllDocument', () => {
+            expect(ctx.tranformAllDocument).toHaveBeenCalledWith('count', 'uriDataset.findLimitFromSkip()', 'publishedDataset.insertBatch()', 'transformDocumentAndKeepUri()');
+        });
+
+
+        it('should redirect to the publication route', () => {
+            expect(ctx.redirect).toHaveBeenCalledWith('/api/publication');
+        });
     });
 
-    it('should get the total number of items in the original dataset', () => {
-        expect(ctx.dataset.count).toHaveBeenCalledWith({});
+    describe('tryPublish', () => {
+        it('should provide ctx with needed dependency', async () => {
+            const ctx = {};
+            const next = createSpy();
+            await tryPublish(ctx, next);
+
+            expect(ctx).toEqual({
+                tranformAllDocument,
+                getDocumentTransformer,
+                addTransformResultToDoc,
+                addUriToTransformResult,
+            });
+        });
+
+        it('should remove uriDataset and publishedDataset if next fail', (done) => {
+            const ctx = {
+                uriDataset: {
+                    remove: createSpy(),
+                },
+                publishedDataset: {
+                    remove: createSpy(),
+                },
+            };
+            const error = new Error('Boom');
+            const next = createSpy().andReturn(Promise.reject(error));
+            tryPublish(ctx, next)
+            .then(() => {
+                throw new Error('tryPublish promise should have been rejected');
+            })
+            .catch((e) => {
+                expect(e).toEqual(error);
+                expect(ctx.uriDataset.remove).toHaveBeenCalled();
+                expect(ctx.publishedDataset.remove).toHaveBeenCalled();
+                done();
+            })
+            .catch(done);
+        });
     });
 
-    it('should get the columns', () => {
-        expect(ctx.field.findAll).toHaveBeenCalled();
+    describe('tranformAllDocument', () => {
+        const dataset = [{ foo: 'foo1', bar: 'bar1' }, { foo: 'foo2', bar: 'bar2' }];
+        const count = 201;
+        const findLimitFromSkip = createSpy().andReturn(dataset);
+        const insertBatch = createSpy();
+        const transformDocument = createSpy().andReturn(Promise.resolve('transformedDocument'));
+
+        before(async () => {
+            await tranformAllDocument(count, findLimitFromSkip, insertBatch, transformDocument);
+        });
+        it('should load items from the original dataset and insert them in the publishedDataset by page of 100', () => {
+            expect(findLimitFromSkip).toHaveBeenCalledWith(100, 0);
+            expect(findLimitFromSkip).toHaveBeenCalledWith(100, 200);
+            expect(insertBatch).toHaveBeenCalledWith(['transformedDocument', 'transformedDocument']);
+        });
+
+        it('should call transform document with each document in dataset', () => {
+            expect(transformDocument).toHaveBeenCalledWith(dataset[0], 0, dataset);
+            expect(transformDocument).toHaveBeenCalledWith(dataset[1], 1, dataset);
+        });
+
+        it('should insert all transformedDocument', () => {
+            expect(insertBatch).toHaveBeenCalledWith(['transformedDocument', 'transformedDocument']);
+        });
     });
 
-    it('should get the transformers', () => {
-        expect(ctx.getDocumentTransformer).toHaveBeenCalledWith({ env: 'node', dataset: ctx.dataset }, fields);
+    describe('addTransformResultToDoc', () => {
+        it('should add transform result to doc omitting doc._id', async () => {
+            const transform = createSpy().andReturn({ transformed: 'data' });
+            const doc = {
+                _id: 'id',
+                data: 'value',
+            };
+
+            expect(await addTransformResultToDoc(transform)(doc)).toEqual({
+                data: 'value',
+                transformed: 'data',
+            });
+
+            expect(transform).toHaveBeenCalledWith(doc);
+        });
     });
 
-    it('should load items from the original dataset and insert them in the publishedDataset by page of 100', () => {
-        expect(ctx.dataset.findLimitFromSkip).toHaveBeenCalledWith(100, 0);
-        expect(ctx.dataset.findLimitFromSkip).toHaveBeenCalledWith(100, 200);
-        expect(ctx.publishedDataset.insertMany).toHaveBeenCalledWith(['transformedDocument', 'transformedDocument']);
-    });
+    describe('addUriToTransformResult', () => {
+        it('should add doc.uri to transform result', async () => {
+            const transform = createSpy().andReturn({ transformed: 'data' });
+            const doc = {
+                _id: 'id',
+                uri: 'uri',
+                data: 'value',
+            };
 
-    it('should call transform document with each document in dataset', () => {
-        expect(transformDocument).toHaveBeenCalledWith(dataset[0], 0, dataset);
-        expect(transformDocument).toHaveBeenCalledWith(dataset[1], 1, dataset);
-    });
+            expect(await addUriToTransformResult(transform)(doc)).toEqual({
+                uri: 'uri',
+                transformed: 'data',
+            });
 
-    it('should insert all transformedDocument', () => {
-        expect(ctx.publishedDataset.insertMany).toHaveBeenCalledWith(['transformedDocument', 'transformedDocument']);
-    });
-
-    it('should redirect to the publication route', () => {
-        expect(ctx.redirect).toHaveBeenCalledWith('/api/publication');
+            expect(transform).toHaveBeenCalledWith(doc);
+        });
     });
 });
