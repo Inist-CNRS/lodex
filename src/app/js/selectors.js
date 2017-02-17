724642c5a45c74e872ec8a0bda873e002bacd482
djhi <gildas.garcia@gmail.com>
2017-02-17 12:53:46 +0100

    [RFR] Global selectors

---
+++ src/app/js/selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+import { fromPublication as localFromPublication } from './publication';
+
+export const getProps = (state, props) => props;
+
+export const createGlobalSelector = (getLocalState, selector) => createSelector(
+    getLocalState,
+    getProps,
+    (localState, props) => selector(localState, props),
+);
+
+export const createGlobalSelectors = (getLocalState, selectors) => Object.keys(selectors).reduce((result, key) => ({
+    ...result,
+    [key]: createGlobalSelector(getLocalState, selectors[key]),
+}), {});
+
+const getPublicationState = state => state.publication;
+
+export const fromPublication = createGlobalSelectors(getPublicationState, localFromPublication);
