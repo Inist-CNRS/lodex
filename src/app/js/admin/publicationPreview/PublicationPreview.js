ac8c0b977691f2a458c1eb5d34af730a64ae6c41
gildas <gildas.garcia@gmail.com>
2017-02-21 09:53:44 +0100

    [RFR] Admin Ergonomics - Move button in headers
    [Trello card](https://trello.com/c/yDw7ffgB)
    
    - [x] Move button upload inside the parsing box header
    - [x] Move the add_column button inside the publication preview box header
    - [x] Move the publish button inside the publication box header

--- src/app/js/admin/publicationPreview/PublicationPreview.js
+++ src/app/js/admin/publicationPreview/PublicationPreview.js
@@ -1,35 +1,67 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import compose from 'recompose/compose';
 import translate from 'redux-polyglot/translate';
 import { CardHeader, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 import PublicationExcerpt from './PublicationExcerpt';
 
-import { editField } from '../fields';
+import { addField, editField } from '../fields';
 import { polyglot as polyglotPropTypes, field as fieldPropTypes } from '../../propTypes';
 import { fromFields, fromPublicationPreview } from '../selectors';
 import Card from '../../lib/Card';
 
-export const PublicationPreviewComponent = ({ columns, lines, editColumn, p: polyglot }) => (
-    <Card initiallyExpanded className="publication-preview">
-        <CardHeader
-            actAsExpander
-            showExpandableButton
-            title={polyglot.t('publication_preview')}
-        />
+const styles = {
+    title: {
+        height: '36px',
+        lineHeight: '36px',
+    },
+    button: {
+        float: 'right',
+        marginRight: '2rem',
+    },
+};
 
-        <CardText expandable>
-            <PublicationExcerpt
-                columns={columns}
-                lines={lines}
-                onHeaderClick={editColumn}
-            />
-        </CardText>
-    </Card>
-);
+export class PublicationPreviewComponent extends Component {
+    handleAddColumnClick = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.props.addColumn();
+    }
+
+    render() {
+        const { columns, lines, editColumn, p: polyglot } = this.props;
+
+        return (
+            <Card initiallyExpanded className="publication-preview">
+                <CardHeader
+                    showExpandableButton
+                    title={polyglot.t('publication_preview')}
+                    titleStyle={styles.title}
+                >
+                    <FlatButton
+                        className="add-column"
+                        label={polyglot.t('add_column')}
+                        onClick={this.handleAddColumnClick}
+                        style={styles.button}
+                    />
+                </CardHeader>
+
+                <CardText expandable>
+                    <PublicationExcerpt
+                        columns={columns}
+                        lines={lines}
+                        onHeaderClick={editColumn}
+                    />
+                </CardText>
+            </Card>
+        );
+    }
+}
 
 PublicationPreviewComponent.propTypes = {
+    addColumn: PropTypes.func.isRequired,
     columns: PropTypes.arrayOf(fieldPropTypes).isRequired,
     lines: PropTypes.arrayOf(PropTypes.object).isRequired,
     p: polyglotPropTypes.isRequired,
@@ -42,6 +74,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
+    addColumn: addField,
     editColumn: editField,
 };
 
