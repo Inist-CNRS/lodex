b457f55a34caf16726dd7e1c653b7cf517062a05
ThieryMichel <thiery@marmelab.com>
2017-02-23 12:07:51 +0100

    detect loader by file extension

--- src/api/loaders/index.js
+++ src/api/loaders/index.js
@@ -2,6 +2,6 @@ import parseCsv from './parseCsv'; // eslint-disable-line
 
 
 export default {
-    'text/csv': parseCsv,
-    'text/tab-separated-values': parseCsv,
+    csv: parseCsv,
+    tsv: parseCsv,
 };
