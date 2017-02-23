f61586b63f84d99accab64cef2f3aa80a33df31d
gildas <gildas.garcia@gmail.com>
2017-02-23 10:01:05 +0100

    [RFR] Public Layout Customization

--- src/api/controller/front/dev.js
+++ src/api/controller/front/dev.js
@@ -9,9 +9,6 @@ app.use(koaWebpack({
     config: webpackConfig,
     dev: {
         publicPath: webpackConfig.output.publicPath,
-        headers: {
-            'Content-Type': 'text/html; charset=utf-8',
-        },
         stats: {
             colors: true,
         },
