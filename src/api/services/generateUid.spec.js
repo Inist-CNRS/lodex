97cfb10273e0aa513ff0329def1af4a743297f7c
ThieryMichel <thiery@marmelab.com>
2017-02-23 10:28:52 +0100

    add generateUid service

---
+++ src/api/services/generateUid.spec.js
@@ -0,0 +1,16 @@
+import expect from 'expect';
+
+import generateUid from './generateUid';
+
+describe('generateUid', () => {
+    it('should generate random 4 char uid', async () => {
+        const uid = await generateUid();
+        expect(uid).toMatch(/^[A-za-z0-9]{4}$/);
+    });
+
+    it('should generate different uid on eac call', async () => {
+        const uid1 = await generateUid();
+        const uid2 = await generateUid();
+        expect(uid1).toNotEqual(uid2);
+    });
+});
