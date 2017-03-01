f91dfc531b23cf7b3b138b92096e0e10d70e10a3
ThieryMichel <thiery@marmelab.com>
2017-03-01 10:16:31 +0100

    allow to display composite field

--- src/app/js/public/CompositeProperty.js
+++ src/app/js/public/CompositeProperty.js
@@ -1,5 +1,6 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import FlatButton from 'material-ui/FlatButton';
 
 import {
     fromPublication,
@@ -8,41 +9,67 @@ import {
     field as fieldPropTypes,
 } from '../propTypes';
 import Format from './Format';
+import Property from './Property';
 
-const PropertyComponent = ({
-    field,
-    compositeFields,
-    fields,
-    resource,
-}) => (
-    <div>
-        {compositeFields.map((f, index) => (
-            <span>
-                <Format
-                    className="property_value"
-                    field={f}
-                    fields={fields}
-                    resource={resource}
+class CompositePropertyComponent extends Component {
+    static propTypes = {
+        field: fieldPropTypes.isRequired,
+        fields: PropTypes.arrayOf(fieldPropTypes).isRequired,
+        compositeFields: PropTypes.arrayOf(fieldPropTypes).isRequired,
+        resource: PropTypes.shape({}).isRequired,
+    }
+
+    static defaultProps = {
+        className: null,
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showCompositeField: false,
+        };
+    }
+
+    render() {
+        const {
+            field,
+            compositeFields,
+            fields,
+            resource,
+        } = this.props;
+
+        return (
+            <div>
+                {compositeFields.map((f, index) => (
+                    <span>
+                        <Format
+                            className="property_value"
+                            field={f}
+                            fields={fields}
+                            resource={resource}
+                        />
+                        {
+                            index !== compositeFields.length - 1 ?
+                                <span>{field.composedOf.separator}</span> : null
+                        }
+                    </span>
+                ))}
+                <FlatButton
+                    onClick={() => this.setState({ showCompositeField: !this.state.showCompositeField })}
+                    label="detail"
                 />
-                {
-                    index !== compositeFields.length - 1 ?
-                        <span>{field.composedOf.separator}</span> : null
-                }
-            </span>
-        ))}
-    </div>
-);
-
-PropertyComponent.propTypes = {
-    field: fieldPropTypes.isRequired,
-    fields: PropTypes.arrayOf(fieldPropTypes).isRequired,
-    compositeFields: PropTypes.arrayOf(fieldPropTypes).isRequired,
-    resource: PropTypes.shape({}).isRequired,
-};
-
-PropertyComponent.defaultProps = {
-    className: null,
-};
+                { this.state.showCompositeField ? compositeFields.map(f => (
+                    <Property
+                        field={f}
+                        fields={fields}
+                        resource={resource}
+                    />
+                )) : null}
+            </div>
+        );
+    }
+}
 
 const mapStateToProps = (state, { field, resource }) => ({
     resource,
@@ -50,6 +77,6 @@ const mapStateToProps = (state, { field, resource }) => ({
     compositeFields: fromPublication.getCompositeFields(state, field),
 });
 
-const Property = connect(mapStateToProps)(PropertyComponent);
+const CompositeProperty = connect(mapStateToProps)(CompositePropertyComponent);
 
-export default Property;
+export default CompositeProperty;
