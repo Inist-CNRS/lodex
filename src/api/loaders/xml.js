3b8509227c81bd4e75579dcd64d3a1ef97c2eb1b
Nicolas Thouvenin <nthouvenin@gmail.com>
2017-03-08 12:24:53 +0100

    lint

--- src/api/loaders/xml.js
+++ src/api/loaders/xml.js
@@ -5,16 +5,19 @@ ezs.use(ezsBasics);
 
 export default config => stream =>
     stream
-        .pipe(ezs('XMLParse', { separator: [
-          '/RDF/*', 
-          '/rdf:RDF/*',
-          '/modsCollection/mods',
-          '/teiCorpus/TEI',
-          '/root/*',
-          '/struct/*',
-          '/rss/channel/item',
-          '/feed/entry',
-          '/rows/row',
-        ], ...config }))
+        .pipe(ezs('XMLParse', {
+            separator: [
+                '/RDF/*',
+                '/rdf:RDF/*',
+                '/modsCollection/mods',
+                '/teiCorpus/TEI',
+                '/root/*',
+                '/struct/*',
+                '/rss/channel/item',
+                '/feed/entry',
+                '/rows/row',
+            ],
+            ...config,
+        }))
         .pipe(ezs('flatten', { separator: '/' }))
         .pipe(ezs('standardize'));
