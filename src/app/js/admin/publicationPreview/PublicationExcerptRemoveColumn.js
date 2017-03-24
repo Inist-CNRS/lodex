165adb9141e61ded7b60850dfef91c3c30089569
gildas <gildas.garcia@gmail.com>
2017-03-24 10:55:03 +0100

    [RFR] Disallow URI removal

--- src/app/js/admin/publicationPreview/PublicationExcerptRemoveColumn.js
+++ src/app/js/admin/publicationPreview/PublicationExcerptRemoveColumn.js
@@ -9,15 +9,20 @@ import FlatButton from 'material-ui/FlatButton';
 
 import { removeField } from '../fields';
 import { polyglot as polyglotPropTypes, field as fieldPropTypes } from '../../propTypes';
+import getFieldClassName from '../../lib/getFieldClassName';
 
-export const PublicationExcerptRemoveColumnComponent = ({ removeColumn, field: { label }, p: polyglot }) => (
+export const PublicationExcerptRemoveColumnComponent = ({ removeColumn, field: { label = '', name }, p: polyglot }) => (
     <TableRowColumn>
-        <FlatButton
-            className={`btn-excerpt-remove-column btn-excerpt-remove-column-${label.toLowerCase().replace(/\s/g, '_')}`}
-            label={polyglot.t('remove_from_publication')}
-            onClick={removeColumn}
-            primary
-        />
+        {name !== 'uri'
+        ? (
+            <FlatButton
+                className={`btn-excerpt-remove-column btn-excerpt-remove-column-${getFieldClassName({ label })}`}
+                label={polyglot.t('remove_from_publication')}
+                onClick={removeColumn}
+                primary
+            />
+        )
+        : null}
     </TableRowColumn>
 );
 
