edb88bd9cef923095914e47c09b32931fcff8851
ThieryMichel <thiery@marmelab.com>
2017-04-03 11:41:09 +0200

    allow to override mongo config from config.json

--- config/production-dist.js
+++ config/production-dist.js
@@ -1,9 +1,26 @@
-module.exports = {
-    port: 3000,
-    mongo: {
+import config from '../config.json';
+
+const getMongoConfig = () => {
+    const { mongo = {} } = config;
+
+    const defaultConfig = {
         host: process.env.EZMASTER_MONGODB_HOST_PORT,
         dbName: process.env.EZMASTER_TECHNICAL_NAME,
-    },
+    };
+
+    if (!mongo) {
+        return defaultConfig;
+    }
+
+    return {
+        host: mongo.host || defaultConfig.host,
+        dbName: mongo.dbName || defaultConfig.dbName,
+    };
+};
+
+module.exports = {
+    port: 3000,
+    mongo: getMongoConfig(),
     auth: {
         cookieSecret: 'cookie',
         headerSecret: 'header',
