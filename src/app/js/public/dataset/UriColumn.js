7773579652dbaaa54492ff013ce306d1d70607e2
gildas <gildas.garcia@gmail.com>
2017-04-04 11:25:27 +0200

    [RFR] Fix URI with VALUE function

--- src/app/js/public/dataset/UriColumn.js
+++ src/app/js/public/dataset/UriColumn.js
@@ -5,10 +5,18 @@ import { TableRowColumn } from 'material-ui/Table';
 import { field as fieldPropTypes } from '../../propTypes';
 import { getResourceUri } from '../../../../common/uris';
 
+const getHumanUri = (uri) => {
+    if (uri.startsWith('uid:/')) {
+        return `uid:/${decodeURIComponent(uri.substr(4))}`;
+    }
+
+    return uri;
+};
+
 const UriColumn = ({ column, resource }) => (
     <TableRowColumn className={classnames('dataset-column', `dataset-${column.name}`)}>
         <Link to={getResourceUri(resource, `${window.location.protocol}//${window.location.host}`)}>
-            {resource[column.name]}
+            {getHumanUri(resource[column.name])}
         </Link>
     </TableRowColumn>
 );
