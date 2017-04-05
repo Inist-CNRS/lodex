238bc19dceb5f53c2fec3b352e86d29279943f74
ThieryMichel <thiery@marmelab.com>
2017-04-05 17:38:57 +0200

    refactor ensureIsUnique to work on multiple field

--- src/api/models/ensureIsUnique.js
+++ src/api/models/ensureIsUnique.js
@@ -1,10 +1,24 @@
 import memoize from 'lodash.memoize';
 
+export const ensureConcatenationIsUnique = collection => async (fieldNames) => {
+    const $concat = fieldNames.map(v => `$${v}`);
+    const { distinct } = await collection.aggregate([
+        { $project: { uri: { $concat } } },
+        { $group: { _id: null, uris: { $addToSet: '$uri' } } },
+        { $project: { distinct: { $size: '$uris' } } },
+    ]);
+
+    return distinct;
+};
+
 const ensure = collection => async (fieldName) => {
     const count = await collection.count();
-    const distinct = await collection.distinct(fieldName);
+    const distinct = Array.isArray(fieldName) ?
+            await ensureConcatenationIsUnique(fieldName)
+        :
+            await collection.distinct(fieldName).length;
 
-    return distinct.length === count;
+    return distinct === count;
 };
 
 export default collection => memoize(ensure(collection));
