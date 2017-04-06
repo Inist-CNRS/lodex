8136dcba4a4008934f908755b8f8e56654ed7213
François Parmentier <francois.parmentier@gmail.com>
2017-04-06 17:39:51 +0200

    Add test for email format

---
+++ src/app/js/formats/email/Component.spec.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow } from 'enzyme';
+
+import EmailView from './Component';
+
+describe('<EmailView />', () => {
+    it('should render', () => {
+        const resource = { foo: 'firstname.lastname@example.com' };
+        const field = { name: 'foo', label: 'label' };
+        const fields = [];
+        const wrapper = shallow(<EmailView resource={resource} field={field} fields={fields} />);
+        expect(wrapper.find('Link').length).toEqual(1);
+        expect(wrapper.prop('to')).toEqual('mailto:firstname.lastname@example.com');
+    });
+});
