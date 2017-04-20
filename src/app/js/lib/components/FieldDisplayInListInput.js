748f5349f8061277db48f18bc1e5fdd5a0a7f76f
gildas <gildas.garcia@gmail.com>
2017-04-20 14:40:43 +0200

    review missing files

--- src/app/js/lib/components/FieldDisplayInListInput.js
+++ src/app/js/lib/components/FieldDisplayInListInput.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import translate from 'redux-polyglot/translate';
-import { Field } from 'redux-form';
 
 import FormCheckboxField from './FormCheckboxField';
-import { polyglot as polyglotPropTypes } from '../../propTypes';
+import FieldInput from './FieldInput';
 
-export const FieldDisplayInResourceInputComponent = ({ p: polyglot }) => (
-    <Field
-        name="display_in_list"
-        component={FormCheckboxField}
-        label={polyglot.t('field_display_in_list')}
-    />
-);
-
-FieldDisplayInResourceInputComponent.propTypes = {
-    p: polyglotPropTypes.isRequired,
-};
-
-export default translate(FieldDisplayInResourceInputComponent);
+export default () => <FieldInput
+    name="display_in_list"
+    component={FormCheckboxField}
+    labelKey="field_display_in_list"
+/>;
