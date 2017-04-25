6d5d02b51ba53985981192738708cb0379c4aa42
ThieryMichel <thiery@marmelab.com>
2017-04-25 10:14:58 +0200

    make dataset column bold and black

--- src/app/js/admin/preview/ExcerptHeader.js
+++ src/app/js/admin/preview/ExcerptHeader.js
@@ -2,17 +2,27 @@ import React from 'react';
 import compose from 'recompose/compose';
 import translate from 'redux-polyglot/translate';
 import { connect } from 'react-redux';
+import memoize from 'lodash.memoize';
+
 import { fromFields } from '../selectors';
 import { polyglot as polyglotPropTypes, field as fieldPropTypes } from '../../propTypes';
 import getFieldClassName from '../../lib/getFieldClassName';
 
+const getStyle = memoize(field =>
+    (field.cover === 'dataset' ? ({
+        fontWeight: 'bold',
+        color: 'black',
+    }) : null));
+
 const ExcerptHeaderComponent = ({
     completedField,
-    field: { name, label },
+    field,
     p: polyglot,
 }) => (
-    <div>
-        {label || name}
+    <div
+        style={getStyle(field)}
+    >
+        {field.label || field.name}
         {completedField &&
             <div className={`completes_${getFieldClassName(completedField)}`}>
                 {polyglot.t('completes_field_X', { field: completedField.label })}
