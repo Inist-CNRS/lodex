0bb8d48e5e1a8c224c7f475162aad462dbb1ca7c
Nicolas Thouvenin <nthouvenin@gmail.com>
2017-04-27 17:19:34 +0200

    use default admin component

--- src/app/js/formats/trello-timeline/index.js
+++ src/app/js/formats/trello-timeline/index.js
@@ -1,9 +1,8 @@
 import Component from './Component';
-import AdminComponent from '../DefaultAdminComponentWithLabel';
 import DefaultFormat from '../DefaultFormat';
 
 export default {
     Component,
-    AdminComponent,
+    AdminComponent: DefaultFormat.AdminComponent,
     EditionComponent: DefaultFormat.EditionComponent,
 };
