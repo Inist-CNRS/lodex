59872d15ef8b206be52ee4cae46d6097ced310e9
ThieryMichel <thiery@marmelab.com>
2017-04-27 10:01:45 +0200

    review

--- src/api/exporters/exportWidgetResource.js
+++ src/api/exporters/exportWidgetResource.js
@@ -1,44 +1,33 @@
 import { html } from 'common-tags';
 
-const renderField = (fields, resource) => (fieldName) => {
-    const field = fields.find(f => f.name === fieldName);
 
-    return html`
-        <dt class="title">
-            <div class="language">
-                ${field.language}
-            </div>
-            <div class="name">
-                ${field.label}
-            </div>
-            <div class="scheme">
-                ${field.scheme}
-            </div>
-        </dt>
-        <dd class="description">
-            <div class="value">
-                ${resource[fieldName]}
-            </div>
-        </dd>
-    `;
-};
+const renderField = (field, value) => html`
+    <dt class="title">
+        <div class="language">
+            ${field.language}
+        </div>
+        <div class="name">
+            ${field.label}
+        </div>
+        <div class="scheme">
+            ${field.scheme}
+        </div>
+    </dt>
+    <dd class="description">
+        <div class="value">
+            ${value}
+        </div>
+    </dd>`;
 
-const renderOneResource = (fields, requestedFields) => resource =>
-    Object.keys(resource)
-        .filter(k => k !== 'publicationDate' && (requestedFields.includes(k)))
-        .map(renderField(fields, resource))
+const renderOneResource = (fieldsByName, requestedFields) => resource =>
+    requestedFields
+        .map(name => renderField(fieldsByName[name], resource[name]))
         .join('');
 
 const renderResourceInTable = displayedFields => resource =>
     html`<tr>${displayedFields.map(name => `<td>${resource[name]}</td>`)}</tr>`;
 
-const renderResources = (fields, displayedFields, resources) => {
-    const fieldsByName = fields.reduce((acc, field) => ({
-        ...acc,
-        [field.name]: field,
-    }), {});
-
-    return html `<table class="table">
+const renderResources = (fieldsByName, displayedFields, resources) => html `<table class="table">
         <thead>
             <tr>
                 ${displayedFields.map(name => fieldsByName[name].label).map(label => `<th>${label}</th>`)}
@@ -48,18 +37,17 @@ const renderResources = (fields, displayedFields, resources) => {
             ${resources.map(renderResourceInTable(displayedFields))}
         </tbody>
     </table>`;
-};
 
 const getLastVersion = resource => ({
     uri: resource.uri,
     ...resource.versions[resource.versions.length - 1],
 });
 
-const getResourcesHtml = (fields, requestedFields, resources) =>
+const getResourcesHtml = (fieldsByName, requestedFields, resources) =>
     (Array.isArray(resources) ?
-        renderResources(fields, requestedFields, resources.map(getLastVersion))
+        renderResources(fieldsByName, requestedFields, resources.map(getLastVersion))
     :
-        renderOneResource(fields, requestedFields)(getLastVersion(resources)));
+        renderOneResource(fieldsByName, requestedFields)(getLastVersion(resources)));
 
 const getPaginationHtml = (page, perPage, total, displayedFields) => {
     if (!total || perPage >= total) {
@@ -94,8 +82,13 @@ const getPaginationHtml = (page, perPage, total, displayedFields) => {
 };
 
 function exporter(config, fields, resources, requestedFields, page, perPage, total) {
+    const fieldsByName = fields.reduce((acc, field) => ({
+        ...acc,
+        [field.name]: field,
+    }), {});
     const displayedFields = requestedFields.length ? requestedFields : fields.map(({ name }) => name);
-    const resourcesHtml = getResourcesHtml(fields, displayedFields, resources);
+    const resourcesHtml = getResourcesHtml(fieldsByName, displayedFields, resources);
+
     return html`
         <!DOCTYPE html>
         <html>
