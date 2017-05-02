70e3af81473f07b2b4309b59f7cbaf63865540f5
Gildas Garcia <gildas.garcia@gmail.com>
2017-05-02 11:50:00 +0200

    [RFR] Fix column headers with long texts (#311)
    * [RFR] Fix column headers with long texts
    [Trello card](https://trello.com/c/hBdhXPmj)
    ! [screen](http://i.imgur.com/MYmltP7.png)
    
    * fix unit tests

---
+++ src/app/js/admin/parsing/ParsingExcerptHeaderColumn.js
@@ -0,0 +1,37 @@
+import React, { PropTypes } from 'react';
+import pure from 'recompose/pure';
+import { TableHeaderColumn } from 'material-ui/Table';
+import { isLongText, getShortText } from '../../lib/longTexts';
+
+const styles = {
+    col: {
+        position: 'relative',
+        minWidth: '10rem',
+        overflow: 'visible',
+        height: 'auto',
+    },
+};
+
+export const ParsingExcerptHeaderColumnComponent = ({ style, column }) => (
+    isLongText(column)
+    ? (
+        <TableHeaderColumn style={Object.assign(styles.col, style)} title={column}>
+            {getShortText(column)}
+        </TableHeaderColumn>
+    )
+    : (
+        <TableHeaderColumn style={styles.col}>
+            {column}
+        </TableHeaderColumn>)
+);
+
+ParsingExcerptHeaderColumnComponent.propTypes = {
+    style: PropTypes.object, // eslint-disable-line
+    column: PropTypes.string.isRequired,
+};
+
+ParsingExcerptHeaderColumnComponent.defaultProps = {
+    style: null,
+};
+
+export default pure(ParsingExcerptHeaderColumnComponent);
