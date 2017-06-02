f1dcdc513f6c1d23dccdc0a56f1a6ae9c748e134
Nicolas Thouvenin <nthouvenin@gmail.com>
2017-06-02 09:03:45 +0200

    add min export

---
+++ src/api/exporters/exportMin.js
@@ -0,0 +1,20 @@
+import ezs from 'ezs';
+import ezsBasics from 'ezs-basics';
+import ezsLocals from './ezsLocals';
+
+ezs.use(ezsBasics);
+ezs.use(ezsLocals);
+
+const exporter = (config, fields, characteristics, stream) =>
+    stream
+        .pipe(ezs('filterVersions'))
+        .pipe(ezs('filterContributions', { fields }))
+        .pipe(ezs('MinimalObject', { fields }))
+        .pipe(ezs('jsonify'));
+
+exporter.extension = 'js';
+exporter.mimeType = 'application/json';
+exporter.type = 'file';
+exporter.label = 'min';
+
+export default exporter;
