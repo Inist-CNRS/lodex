e209d0ad5bcae0e45fef9d0c30cae72910c02629
Nicolas Thouvenin <nthouvenin@gmail.com>
2017-06-04 23:53:05 +0200

    fetch and display

---
+++ src/app/js/formats/lodex-resource/index.js
@@ -0,0 +1,9 @@
+import Component from './Component';
+
+import DefaultFormat from '../DefaultFormat';
+
+export default {
+    Component,
+    AdminComponent: DefaultFormat.AdminComponent,
+    EditionComponent: DefaultFormat.EditionComponent,
+};
