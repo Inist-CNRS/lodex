7cbbaaa5f01b71a691f1a126bf60254b85466e3e
QuanticPotatoes <jeremy.dieuze@viacesi.fr>
2017-06-13 15:50:10 +0200

    change default value searchable to true

--- src/app/js/fields/index.js
+++ src/app/js/fields/index.js
@@ -88,7 +88,7 @@ const getDefaultField = (name, index) => ({
     name: 'new',
     display_in_list: true,
     display_in_resource: true,
-    searchable: false,
+    searchable: true,
     transformers: name ? [{
         operation: 'COLUMN',
         args: [{
