26d1c9668a54113baf19570a42632a702db135fa
QuanticPotatoes <jeremy.dieuze@viacesi.fr>
2017-07-05 18:09:39 +0200

    unique blank nodes URI  fix #390

--- src/api/exporters/ezsLocals/linkDataset.js
+++ src/api/exporters/ezsLocals/linkDataset.js
@@ -1,10 +1,20 @@
+function blankNodeSpecifier(data) {
+    Object.keys(data).map((e) => {
+        if (e !== '@context' && typeof data[e] === 'object') {
+            data[e] = { '@id': `${data['@id']}/${e}/${Math.floor(new Date())}`, ...data[e] };
+        }
+        return 0;
+    });
+    return data;
+}
+
 module.exports = function linkDataset(data, feed) {
     const uri = this.getParam('uri');
     const scheme = this.getParam('scheme', 'http://purl.org/dc/terms/isPartOf');
 
     if (uri && data && data['@context']) {
         feed.send({
-            ...data,
+            ...blankNodeSpecifier(data),
             dataset: uri,
             '@context': {
                 ...data['@context'],
@@ -14,16 +24,6 @@ module.exports = function linkDataset(data, feed) {
             },
         });
 
-        console.log({
-            ...data,
-            dataset: uri,
-            '@context': {
-                ...data['@context'],
-                dataset: {
-                    '@id': scheme,
-                },
-            },
-        });
         return;
     }
 
