a06b1a47206903cc6df9a18d9950724121e2ad8d
QuanticPotatoes <jeremy.dieuze@viacesi.fr>
2017-08-17 17:35:16 +0200

    :sparkles: add atom feed

---
+++ src/api/exporters/exportAtom.js
@@ -0,0 +1,23 @@
+import ezs from 'ezs';
+import Feed from 'feed';
+import ezsBasics from 'ezs-basics';
+import ezsLocals from '../statements';
+
+ezs.use(ezsBasics);
+ezs.use(ezsLocals);
+
+const exporter = (config, fields, characteristics, stream) => {
+    const atomFeed = new Feed({});
+    return stream
+        .pipe(ezs('filterVersions'))
+        .pipe(ezs('filterContributions', { fields }))
+        .pipe(ezs('convertToAtom', { fields, atomFeed }));
+};
+
+exporter.extension = 'atom';
+exporter.mimeType = 'application/atom+xml';
+exporter.type = 'file';
+exporter.label = 'atom';
+
+export default exporter;
+
