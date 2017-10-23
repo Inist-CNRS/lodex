17e3b5b8e2dadace24b40b5d3ddd08f7e2a1c72f
Nicolas Thouvenin <nthouvenin@gmail.com>
2017-10-23 23:12:47 +0200

    add bar chart

---
+++ src/app/js/formats/global-bar-chart/index.js
@@ -0,0 +1,10 @@
+import Component from './Component';
+import AdminComponent from './AdminComponent';
+import DefaultFormat from '../DefaultFormat';
+
+export default {
+    Component,
+    ListComponent: DefaultFormat.ListComponent,
+    AdminComponent,
+    EditionComponent: DefaultFormat.EditionComponent,
+};
