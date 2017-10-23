170e9bc9cc6b9aaabd3612635b9ed1579ea5c37f
François Parmentier <francois.parmentier@gmail.com>
2017-10-23 11:11:00 +0200

    :bug: Add a default value to uri

--- src/api/statements/JSONLDObject/mergeClasses.js
+++ src/api/statements/JSONLDObject/mergeClasses.js
@@ -1,3 +1,4 @@
+import { host } from 'config';
 import getFieldContext from './getFieldContext';
 import getUri from './getUri';
 import formatData from './formatData';
@@ -7,7 +8,7 @@ const mergeClasses = (output, field, data) => {
     const classes = field.classes;
     const fieldContext = getFieldContext(field);
     const property = {
-        '@id': `${getUri(data.uri)}/classes/${propertyName}`,
+        '@id': `${getUri(data.uri || host.concat('/'))}/classes/${propertyName}`,
         '@type': classes,
         label: formatData(data, propertyName),
     };
