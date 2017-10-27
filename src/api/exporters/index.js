b64e7812ffe60559a3e83fd1f66007199dd5c846
Nicolas Thouvenin <nthouvenin@gmail.com>
2017-10-27 22:46:47 +0200

    min & raw are standard formats, so we should use routines

--- src/api/exporters/index.js
+++ src/api/exporters/index.js
@@ -1,7 +1,5 @@
 import csv from './exportCsv';
-import raw from './exportRaw';
 import tsv from './exportTsv';
-import min from './exportMin';
 import nquads from './exportNQuads';
 import turtle from './exportTurtle';
 import atom from './exportAtom';
@@ -18,9 +16,7 @@ export default {
     extendednquadscompressed,
     jsonld,
     jsonldcompacted,
-    min,
     nquads,
-    raw,
     tsv,
     turtle,
     widget,
