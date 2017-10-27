9479781371d3f849d32db8c3423d4b29a5f7d029
François Parmentier <francois.parmentier@gmail.com>
2017-10-27 17:58:14 +0200

    :rotating_light: Make test pass

--- src/api/exporters/exportExtendedNquads.spec.js
+++ src/api/exporters/exportExtendedNquads.spec.js
@@ -32,16 +32,22 @@ describe.only('exportExtendedNquads', () => {
                 labels: '',
                 linked: 'language.raw',
                 context: {
-                    'language.raw': 'language',
+                    'language.raw': 'dcterms:language',
                     link: 'skos:inScheme',
                     doi: 'bibo:doi',
                     'fulltext[0].uri': 'istex:accessURL',
+                    'fulltext[1].uri': 'istex:accessURL',
+                    'fulltext[2].uri': 'istex:accessURL',
+                    'fulltext[3].uri': 'istex:accessURL',
                 },
             },
         }, [{ // fields
             cover: 'collection',
             scheme: 'https://data.istex.fr/ontology/istex#query',
             name: 'istexQuery',
+            format: {
+                name: 'istex',
+            },
         }, {
             cover: 'collection',
             scheme: 'http://purl.org/dc/terms/language',
@@ -49,22 +55,33 @@ describe.only('exportExtendedNquads', () => {
         }],
         null, // characteristics
         from([{ // stream
-            uri: 'http://uri/1',
+            uri: 'http://uri/lang/pal',
             language: 'pal',
             istexQuery: 'language:pal',
         }]),
         ).pipe(ezs((data, feed) => {
             if (data !== null) {
                 outputString += data;
-            } else {
-                try {
-                    expect(outputString).toEqual('\n');
-                } catch (e) {
-                    return done(e);
-                }
-                return done();
+                return feed.end();
             }
-            return feed.end();
+            try {
+                expect(outputString).toEqual([
+                    '<https://api.istex.fr/document/08CBCE8A1AEA8857CACD7A2D47D1085BBD47F38F> <http://purl.org/dc/terms/language> <http://uri/lang/pal> .',
+                    '<https://api.istex.fr/document/08CBCE8A1AEA8857CACD7A2D47D1085BBD47F38F> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/bibo/Document> .',
+                    '<https://api.istex.fr/document/08CBCE8A1AEA8857CACD7A2D47D1085BBD47F38F> <https://data.istex.fr/ontology/istex#accessURL> <https://api.istex.fr/document/08CBCE8A1AEA8857CACD7A2D47D1085BBD47F38F/fulltext/pdf> .',
+                    '<https://api.istex.fr/document/08CBCE8A1AEA8857CACD7A2D47D1085BBD47F38F> <https://data.istex.fr/ontology/istex#accessURL> <https://api.istex.fr/document/08CBCE8A1AEA8857CACD7A2D47D1085BBD47F38F/fulltext/tiff> .',
+                    '<https://api.istex.fr/document/08CBCE8A1AEA8857CACD7A2D47D1085BBD47F38F> <https://data.istex.fr/ontology/istex#accessURL> <https://api.istex.fr/document/08CBCE8A1AEA8857CACD7A2D47D1085BBD47F38F/fulltext/zip> .',
+                    '<https://api.istex.fr/document/EDC4EAC977D4BEB4018117A4EF048013019F6691> <http://purl.org/dc/terms/language> <http://uri/lang/pal> .',
+                    '<https://api.istex.fr/document/EDC4EAC977D4BEB4018117A4EF048013019F6691> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://purl.org/ontology/bibo/Document> .',
+                    '<https://api.istex.fr/document/EDC4EAC977D4BEB4018117A4EF048013019F6691> <https://data.istex.fr/ontology/istex#accessURL> <https://api.istex.fr/document/EDC4EAC977D4BEB4018117A4EF048013019F6691/fulltext/pdf> .',
+                    '<https://api.istex.fr/document/EDC4EAC977D4BEB4018117A4EF048013019F6691> <https://data.istex.fr/ontology/istex#accessURL> <https://api.istex.fr/document/EDC4EAC977D4BEB4018117A4EF048013019F6691/fulltext/tiff> .',
+                    '<https://api.istex.fr/document/EDC4EAC977D4BEB4018117A4EF048013019F6691> <https://data.istex.fr/ontology/istex#accessURL> <https://api.istex.fr/document/EDC4EAC977D4BEB4018117A4EF048013019F6691/fulltext/zip> .',
+                    '',
+                ].join('\n'));
+            } catch (e) {
+                return done(e);
+            }
+            return done();
         }));
     });
 });
