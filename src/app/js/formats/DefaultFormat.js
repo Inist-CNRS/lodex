43b6cfe9b1e5fe03bdcdb28d86cd3bae0d3ff3b9
ThieryMichel <thiery@marmelab.com>
2017-11-20 17:50:10 +0100

    add DefaultEdition component able to properly handle array

--- src/app/js/formats/DefaultFormat.js
+++ src/app/js/formats/DefaultFormat.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import memoize from 'lodash.memoize';
 import PropTypes from 'prop-types';
 
-import { field as fieldPropTypes } from '../propTypes';
+import {
+    field as fieldPropTypes,
+    formField as formFieldPropTypes,
+} from '../propTypes';
 import FormTextField from '../lib/components/FormTextField';
 import { isLongText, getShortText } from '../lib/longTexts';
 import { REJECTED } from '../../../common/propositionStatus';
@@ -48,11 +51,34 @@ DefaultView.defaultProps = {
     shrink: false,
 };
 
+class DefaultEditon extends Component {
+    onchangeArray = (_, value) =>
+        this.props.input.onChange(_, value.split(','));
+
+    render() {
+        const { value } = this.props.input;
+        if (Array.isArray(value)) {
+            const updatedProps = {
+                ...this.props,
+                input: {
+                    ...this.props.input,
+                    value: value.join(','),
+                    onChange: this.onchangeArray,
+                },
+            };
+            return <FormTextField {...updatedProps} />;
+        }
+        return <FormTextField {...this.props} />;
+    }
+}
+
+DefaultEditon.propTypes = formFieldPropTypes;
+
 const Empty = () => <span />;
 
 export default {
     Component: DefaultView,
     ListComponent: DefaultView,
     AdminComponent: Empty,
-    EditionComponent: FormTextField,
+    EditionComponent: DefaultEditon,
 };
