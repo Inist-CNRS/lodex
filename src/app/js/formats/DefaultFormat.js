74f58c0d924370b575f65e5fd109701190168df6
ThieryMichel <thiery@marmelab.com>
2017-11-20 17:13:49 +0100

    allow DefaultVIew to properly handle array of values

--- src/app/js/formats/DefaultFormat.js
+++ src/app/js/formats/DefaultFormat.js
@@ -1,5 +1,6 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import memoize from 'lodash.memoize';
+import PropTypes from 'prop-types';
 
 import { field as fieldPropTypes } from '../propTypes';
 import FormTextField from '../lib/components/FormTextField';
@@ -7,21 +8,31 @@ import { isLongText, getShortText } from '../lib/longTexts';
 import { REJECTED } from '../../../common/propositionStatus';
 
 const styles = {
-    text: memoize(status => Object.assign({
-        fontSize: '1.5rem',
-        textDecoration: status === REJECTED ? 'line-through' : 'none',
-    })),
+    text: memoize(status =>
+        Object.assign({
+            fontSize: '1.5rem',
+            textDecoration: status === REJECTED ? 'line-through' : 'none',
+        }),
+    ),
 };
 
-const DefaultView = ({ className, resource, field, fieldStatus, shrink }) => (
-    <span style={styles.text(fieldStatus)}>
-        {
-            shrink && isLongText(resource[field.name])
-                ? <span className={className}>{getShortText(resource[field.name])}</span>
-                : <span className={className}>{resource[field.name]}</span>
-        }
-    </span>
-);
+const DefaultView = ({ className, resource, field, fieldStatus, shrink }) => {
+    let value = resource[field.name];
+
+    if (Array.isArray(value)) {
+        value = value.join(', ');
+    }
+
+    return (
+        <span style={styles.text(fieldStatus)}>
+            {shrink && isLongText(value) ? (
+                <span className={className}>{getShortText(value)}</span>
+            ) : (
+                <span className={className}>{value}</span>
+            )}
+        </span>
+    );
+};
 
 DefaultView.propTypes = {
     className: PropTypes.string,
