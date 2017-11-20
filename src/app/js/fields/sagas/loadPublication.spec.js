aa2b15a47a3e06334509aaa45784069dda872f69
ThieryMichel <thiery@marmelab.com>
2017-11-20 12:03:30 +0100

    fix test for loadPublication saga

--- src/app/js/fields/sagas/loadPublication.spec.js
+++ src/app/js/fields/sagas/loadPublication.spec.js
@@ -2,35 +2,65 @@ import expect from 'expect';
 import { call, put, select } from 'redux-saga/effects';
 
 import {
+    loadPublication,
     loadPublicationError,
     loadPublicationSuccess,
 } from '../';
-import { fromUser } from '../../sharedSelectors';
+import { fromUser, fromFields } from '../../sharedSelectors';
 import fetchSaga from '../../lib/sagas/fetchSaga';
 
 import { handleLoadPublicationRequest } from './loadPublication';
 
 describe('publication saga', () => {
     describe('handleLoadPublicationRequest', () => {
-        const saga = handleLoadPublicationRequest();
+        it('should select getNbFields', () => {
+            const saga = handleLoadPublicationRequest();
+            expect(saga.next().value).toEqual(select(fromFields.getNbFields));
+        });
+
+        it('should end if nbFields is more than 0', () => {
+            const saga = handleLoadPublicationRequest();
+            saga.next();
+            expect(saga.next(10).done).toBe(true);
+        });
+
+        it('should put loadPublication', () => {
+            const saga = handleLoadPublicationRequest();
+            saga.next();
+            expect(saga.next(0).value).toEqual(put(loadPublication()));
+        });
 
         it('should select getLoadPublicationRequest', () => {
+            const saga = handleLoadPublicationRequest();
+            saga.next();
+            saga.next(0);
             expect(saga.next().value).toEqual(select(fromUser.getLoadPublicationRequest));
         });
 
         it('should call fetchPublication with the request', () => {
+            const saga = handleLoadPublicationRequest();
+            saga.next();
+            saga.next(0);
+            saga.next();
             expect(saga.next('request').value).toEqual(call(fetchSaga, 'request'));
         });
 
         it('should put loadPublicationSuccess action', () => {
+            const saga = handleLoadPublicationRequest();
+            saga.next();
+            saga.next(0);
+            saga.next();
+            saga.next('request');
             expect(saga.next({ response: 'foo' }).value).toEqual(put(loadPublicationSuccess('foo')));
         });
 
         it('should put loadPublicationError action with error if any', () => {
-            const failedSaga = handleLoadPublicationRequest();
-            failedSaga.next();
-            failedSaga.next();
-            expect(failedSaga.next({ error: 'foo' }).value)
+            const saga = handleLoadPublicationRequest();
+            saga.next();
+            saga.next(0);
+            saga.next();
+            saga.next();
+            expect(saga.next({ error: 'foo' }).value)
                 .toEqual(put(loadPublicationError('foo')));
         });
     });
