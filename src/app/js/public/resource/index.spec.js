0272d091f49689b62659581991770cc5727c4b1f
ThieryMichel <thiery@marmelab.com>
2017-11-22 09:32:16 +0100

    CS on resource/index.spec.js

--- src/app/js/public/resource/index.spec.js
+++ src/app/js/public/resource/index.spec.js
@@ -27,7 +27,11 @@ import reducer, {
     createResourceSuccess,
     fromResource,
 } from './index';
-import { PROPOSED, VALIDATED, REJECTED } from '../../../../common/propositionStatus';
+import {
+    PROPOSED,
+    VALIDATED,
+    REJECTED,
+} from '../../../../common/propositionStatus';
 
 describe('resourceReducer', () => {
     it('should initialize with correct state', () => {
@@ -36,12 +40,15 @@ describe('resourceReducer', () => {
     });
 
     it('should handle LOAD_RESOURCE_SUCCESS', () => {
-        const state = reducer({
-            key: 'value',
-        }, {
-            type: LOAD_RESOURCE_SUCCESS,
-            payload: 'resource',
-        });
+        const state = reducer(
+            {
+                key: 'value',
+            },
+            {
+                type: LOAD_RESOURCE_SUCCESS,
+                payload: 'resource',
+            },
+        );
         expect(state).toEqual({
             key: 'value',
             resource: 'resource',
@@ -52,12 +59,15 @@ describe('resourceReducer', () => {
     });
 
     it('should handle LOAD_RESOURCE_ERROR', () => {
-        const state = reducer({
-            key: 'value',
-        }, {
-            type: LOAD_RESOURCE_ERROR,
-            payload: { message: 'error' },
-        });
+        const state = reducer(
+            {
+                key: 'value',
+            },
+            {
+                type: LOAD_RESOURCE_ERROR,
+                payload: { message: 'error' },
+            },
+        );
         expect(state).toEqual({
             key: 'value',
             error: 'error',
@@ -67,9 +77,12 @@ describe('resourceReducer', () => {
     });
 
     it('should handle LOAD_RESOURCE', () => {
-        const state = reducer({
-            key: 'value',
-        }, { type: LOAD_RESOURCE });
+        const state = reducer(
+            {
+                key: 'value',
+            },
+            { type: LOAD_RESOURCE },
+        );
         expect(state).toEqual({
             key: 'value',
             error: null,
@@ -79,14 +92,13 @@ describe('resourceReducer', () => {
     });
 
     it('should handle SAVE_RESOURCE, HIDE_RESOURCE and ADD_FIELD_TO_RESOURCE', () => {
-        [
-            SAVE_RESOURCE,
-            HIDE_RESOURCE,
-            ADD_FIELD_TO_RESOURCE,
-        ].forEach((type) => {
-            const state = reducer({
-                key: 'value',
-            }, { type });
+        [SAVE_RESOURCE, HIDE_RESOURCE, ADD_FIELD_TO_RESOURCE].forEach(type => {
+            const state = reducer(
+                {
+                    key: 'value',
+                },
+                { type },
+            );
             expect(state).toEqual({
                 key: 'value',
                 error: null,
@@ -97,34 +109,32 @@ describe('resourceReducer', () => {
 
     it('should handle SAVE_RESOURCE_SUCCESS', () => {
         const state = { data: 'value', resource: 'resource' };
-        expect(reducer(state, saveResourceSuccess('new resource')))
-            .toEqual({
-                data: 'value',
-                resource: 'new resource',
-                error: null,
-                saving: false,
-            });
+        expect(reducer(state, saveResourceSuccess('new resource'))).toEqual({
+            data: 'value',
+            resource: 'new resource',
+            error: null,
+            saving: false,
+        });
     });
 
     it('should handle SAVE_RESOURCE_SUCCESS with no resource change', () => {
         const state = { data: 'value', resource: 'resource' };
-        expect(reducer(state, saveResourceSuccess()))
-            .toEqual({
-                data: 'value',
-                resource: 'resource',
-                error: null,
-                saving: false,
-            });
+        expect(reducer(state, saveResourceSuccess())).toEqual({
+            data: 'value',
+            resource: 'resource',
+            error: null,
+            saving: false,
+        });
     });
 
     it('should handle HIDE_RESOURCE_SUCCESS, ADD_FIELD_TO_RESOURCE_SUCCESS', () => {
-        [
-            HIDE_RESOURCE_SUCCESS,
-            ADD_FIELD_TO_RESOURCE_SUCCESS,
-        ].forEach((type) => {
-            const state = reducer({
-                key: 'value',
-            }, { type });
+        [HIDE_RESOURCE_SUCCESS, ADD_FIELD_TO_RESOURCE_SUCCESS].forEach(type => {
+            const state = reducer(
+                {
+                    key: 'value',
+                },
+                { type },
+            );
             expect(state).toEqual({
                 key: 'value',
                 error: null,
@@ -139,10 +149,13 @@ describe('resourceReducer', () => {
             SAVE_RESOURCE_ERROR,
             HIDE_RESOURCE_ERROR,
             ADD_FIELD_TO_RESOURCE_ERROR,
-        ].forEach((type) => {
-            const state = reducer({
-                key: 'value',
-            }, { type, payload: { message: 'boom' } });
+        ].forEach(type => {
+            const state = reducer(
+                {
+                    key: 'value',
+                },
+                { type, payload: { message: 'boom' } },
+            );
             expect(state).toEqual({
                 key: 'value',
                 error: 'boom',
@@ -163,18 +176,26 @@ describe('resourceReducer', () => {
             },
         };
 
-        expect(reducer(state, changeFieldStatus({ field: 'target', status: 'new status' })))
-            .toEqual({
-                moderating: true,
-                resource: {
-                    data: 'value',
-                    contributions: [
-                        { fieldName: 'field', status: 'status', other: 'data' },
-                        { fieldName: 'target', status: 'new status', other: 'data' },
-                        { fieldName: 'miss', status: 'status', other: 'data' },
-                    ],
-                },
-            });
+        expect(
+            reducer(
+                state,
+                changeFieldStatus({ field: 'target', status: 'new status' }),
+            ),
+        ).toEqual({
+            moderating: true,
+            resource: {
+                data: 'value',
+                contributions: [
+                    { fieldName: 'field', status: 'status', other: 'data' },
+                    {
+                        fieldName: 'target',
+                        status: 'new status',
+                        other: 'data',
+                    },
+                    { fieldName: 'miss', status: 'status', other: 'data' },
+                ],
+            },
+        });
     });
 
     it('should handle CHANGE_FIELD_STATUS_SUCCESS', () => {
@@ -182,12 +203,11 @@ describe('resourceReducer', () => {
             data: 'value',
         };
 
-        expect(reducer(state, changeFieldStatusSuccess()))
-            .toEqual({
-                data: 'value',
-                error: null,
-                moderating: false,
-            });
+        expect(reducer(state, changeFieldStatusSuccess())).toEqual({
+            data: 'value',
+            error: null,
+            moderating: false,
+        });
     });
 
     it('should handle CHANGE_FIELD_STATUS_ERROR', () => {
@@ -196,7 +216,11 @@ describe('resourceReducer', () => {
             resource: {
                 contributions: [
                     { fieldName: 'field', status: 'status', other: 'data' },
-                    { fieldName: 'target', status: 'updated status', other: 'data' },
+                    {
+                        fieldName: 'target',
+                        status: 'updated status',
+                        other: 'data',
+                    },
                     { fieldName: 'miss', status: 'status', other: 'data' },
                 ],
             },
@@ -208,19 +232,22 @@ describe('resourceReducer', () => {
             prevStatus: 'previous status',
         });
 
-        expect(reducer(state, action))
-            .toEqual({
-                data: 'value',
-                error: 'boom',
-                moderating: false,
-                resource: {
-                    contributions: [
-                        { fieldName: 'field', status: 'status', other: 'data' },
-                        { fieldName: 'target', status: 'previous status', other: 'data' },
-                        { fieldName: 'miss', status: 'status', other: 'data' },
-                    ],
-                },
-            });
+        expect(reducer(state, action)).toEqual({
+            data: 'value',
+            error: 'boom',
+            moderating: false,
+            resource: {
+                contributions: [
+                    { fieldName: 'field', status: 'status', other: 'data' },
+                    {
+                        fieldName: 'target',
+                        status: 'previous status',
+                        other: 'data',
+                    },
+                    { fieldName: 'miss', status: 'status', other: 'data' },
+                ],
+            },
+        });
     });
 
     it('should handle SELECT_VERSION action', () => {
@@ -228,11 +255,10 @@ describe('resourceReducer', () => {
             data: 'value',
         };
 
-        expect(reducer(state, selectVersion('version')))
-            .toEqual({
-                data: 'value',
-                selectedVersion: 'version',
-            });
+        expect(reducer(state, selectVersion('version'))).toEqual({
+            data: 'value',
+            selectedVersion: 'version',
+        });
     });
 
     it('should handle ADD_FIELD_TO_RESOURCE_OPEN action', () => {
@@ -240,12 +266,11 @@ describe('resourceReducer', () => {
             data: 'value',
         };
 
-        expect(reducer(state, addFieldToResourceOpen()))
-            .toEqual({
-                data: 'value',
-                error: null,
-                addingField: true,
-            });
+        expect(reducer(state, addFieldToResourceOpen())).toEqual({
+            data: 'value',
+            error: null,
+            addingField: true,
+        });
     });
 
     it('should handle ADD_FIELD_TO_RESOURCE_CANCEL action', () => {
@@ -253,12 +278,11 @@ describe('resourceReducer', () => {
             data: 'value',
         };
 
-        expect(reducer(state, addFieldToResourceCancel()))
-            .toEqual({
-                data: 'value',
-                error: null,
-                addingField: false,
-            });
+        expect(reducer(state, addFieldToResourceCancel())).toEqual({
+            data: 'value',
+            error: null,
+            addingField: false,
+        });
     });
 
     it('should handle HIDE_RESOURCE_OPEN action', () => {
@@ -266,12 +290,11 @@ describe('resourceReducer', () => {
             data: 'value',
         };
 
-        expect(reducer(state, hideResourceOpen()))
-            .toEqual({
-                data: 'value',
-                error: null,
-                hiding: true,
-            });
+        expect(reducer(state, hideResourceOpen())).toEqual({
+            data: 'value',
+            error: null,
+            hiding: true,
+        });
     });
 
     it('should handle HIDE_RESOURCE_CANCEL action', () => {
@@ -279,12 +302,11 @@ describe('resourceReducer', () => {
             data: 'value',
         };
 
-        expect(reducer(state, hideResourceCancel()))
-            .toEqual({
-                data: 'value',
-                error: null,
-                hiding: false,
-            });
+        expect(reducer(state, hideResourceCancel())).toEqual({
+            data: 'value',
+            error: null,
+            hiding: false,
+        });
     });
 
     it('should handle CREATE_RESOURCE_OPEN action', () => {
@@ -292,12 +314,11 @@ describe('resourceReducer', () => {
             data: 'value',
         };
 
-        expect(reducer(state, createResourceOpen()))
-            .toEqual({
-                data: 'value',
-                error: null,
-                isCreating: true,
-            });
+        expect(reducer(state, createResourceOpen())).toEqual({
+            data: 'value',
+            error: null,
+            isCreating: true,
+        });
     });
 
     it('should handle CREATE_RESOURCE_CANCEL action', () => {
@@ -305,12 +326,11 @@ describe('resourceReducer', () => {
             data: 'value',
         };
 
-        expect(reducer(state, createResourceCancel()))
-            .toEqual({
-                data: 'value',
-                error: null,
-                isCreating: false,
-            });
+        expect(reducer(state, createResourceCancel())).toEqual({
+            data: 'value',
+            error: null,
+            isCreating: false,
+        });
     });
 
     it('should handle CREATE_RESOURCE_SUCCESS action', () => {
@@ -318,13 +338,12 @@ describe('resourceReducer', () => {
             data: 'value',
         };
 
-        expect(reducer(state, createResourceSuccess()))
-            .toEqual({
-                data: 'value',
-                error: null,
-                isCreating: false,
-                saving: false,
-            });
+        expect(reducer(state, createResourceSuccess())).toEqual({
+            data: 'value',
+            error: null,
+            isCreating: false,
+            saving: false,
+        });
     });
 
     describe('selector', () => {
@@ -335,14 +354,22 @@ describe('resourceReducer', () => {
                         contributions: [
                             { fieldName: 'validatedField', status: VALIDATED },
                             { fieldName: 'proposedField', status: PROPOSED },
-                            { fieldName: 'othervalidatedField', status: VALIDATED },
-                            { fieldName: 'otherProposedField', status: PROPOSED },
+                            {
+                                fieldName: 'othervalidatedField',
+                                status: VALIDATED,
+                            },
+                            {
+                                fieldName: 'otherProposedField',
+                                status: PROPOSED,
+                            },
                             { fieldName: 'rejectedField', status: REJECTED },
                         ],
                     },
                 };
-                expect(fromResource.getResourceProposedFields(state))
-                    .toEqual(['proposedField', 'otherProposedField']);
+                expect(fromResource.getResourceProposedFields(state)).toEqual([
+                    'proposedField',
+                    'otherProposedField',
+                ]);
             });
         });
 
@@ -351,15 +378,29 @@ describe('resourceReducer', () => {
                 const state = {
                     resource: {
                         contributions: [
-                            { fieldName: 'field1', contributor: { name: 'contributor1' } },
-                            { fieldName: 'field2', contributor: { name: 'contributor2' } },
-                            { fieldName: 'field3', contributor: { name: 'contributor3' } },
-                            { fieldName: 'field4', contributor: { name: 'contributor4' } },
+                            {
+                                fieldName: 'field1',
+                                contributor: { name: 'contributor1' },
+                            },
+                            {
+                                fieldName: 'field2',
+                                contributor: { name: 'contributor2' },
+                            },
+                            {
+                                fieldName: 'field3',
+                                contributor: { name: 'contributor3' },
+                            },
+                            {
+                                fieldName: 'field4',
+                                contributor: { name: 'contributor4' },
+                            },
                         ],
                     },
                 };
 
-                expect(fromResource.getResourceContributorsCatalog(state)).toEqual({
+                expect(
+                    fromResource.getResourceContributorsCatalog(state),
+                ).toEqual({
                     field1: 'contributor1',
                     field2: 'contributor2',
                     field3: 'contributor3',
