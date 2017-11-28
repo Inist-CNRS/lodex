a8feb3e8a82c708e87628af245e0734564e53972
ThieryMichel <thiery@marmelab.com>
2017-11-28 17:45:41 +0100

    better style for GraphItemContainer

--- src/app/js/public/graph/GraphItemContainer.js
+++ src/app/js/public/graph/GraphItemContainer.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, CardHeader, CardMedia } from 'material-ui/Card';
+import { Card, CardTitle, CardMedia } from 'material-ui/Card';
 import { Link } from 'react-router';
+import { grey200 } from 'material-ui/styles/colors';
+import Forward from 'material-ui/svg-icons/content/forward';
 
 const styles = {
     media: { minHeight: 200, margin: '10px 0px' },
+    label: {
+        flexGrow: 2,
+        fontWeight: 'bold',
+        textDecoration: 'none',
+        fontSize: '2rem',
+    },
+    title: {
+        backgroundColor: grey200,
+        textDecoration: 'none',
+    },
+    icon: {
+        float: 'right',
+    },
 };
 
 const Graph = ({ link, label, children }) => (
     <Link to={link}>
         <Card>
-            <CardMedia
-                style={styles.media}
-                overlay={<CardHeader title={label} />}
-            >
-                {children}
-            </CardMedia>
+            <CardTitle
+                style={styles.title}
+                title={
+                    <span style={styles.label}>
+                        {label}
+                        <Forward style={styles.icon} />
+                    </span>
+                }
+            />
+            <CardMedia style={styles.media}>{children}</CardMedia>
         </Card>
     </Link>
 );
