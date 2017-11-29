de216c959d53a68bec90b72490700455cdf94fa8
François Parmentier <francois.parmentier@gmail.com>
2017-11-29 14:45:48 +0100

    :bulb: Document loader

--- src/api/loaders/tsv-double-quotes.js
+++ src/api/loaders/tsv-double-quotes.js
@@ -3,6 +3,11 @@ import ezsBasics from 'ezs-basics';
 
 ezs.use(ezsBasics);
 
+/**
+ * Loader parsing CSV, with tabulation as separator, and double quote (") as
+ * escape character (so, it's a TSV loader, using " to escape cells with special
+ * characters, such as \n, \t, ...)
+ */
 export default config => stream =>
     stream
         .pipe(
