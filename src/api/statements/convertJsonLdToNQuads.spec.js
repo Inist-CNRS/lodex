ddbcd5701a7e0eefdfbdda8d1ccf2b7c8fa5185b
François Parmentier <francois.parmentier@gmail.com>
2017-12-06 17:53:40 +0100

    :fire: Remove useless comments and .only
    :)

--- src/api/statements/convertJsonLdToNQuads.spec.js
+++ src/api/statements/convertJsonLdToNQuads.spec.js
@@ -1,71 +1,3 @@
-// import path from 'path';
-// import fs from 'fs';
-// import request from 'request';
-// import ezs from 'ezs';
-// import from from 'from';
-// import sinon from 'sinon';
-// import { expect } from 'chai';
-// import testAll from './testAll';
-
-// const dataTest = require('./fixture.data.json');
-// const dataNquads = fs.readFileSync(
-//     path.resolve(__dirname, './fixture.data.nq'),
-//     'utf8',
-// );
-// const ezsLocals = require('.');
-
-// const config = {
-//     istexQuery: {
-//         labels: '',
-//         linked: 'language',
-//         context: {
-//             language: 'http://purl.org/ontology/dc/language',
-//             doi: 'http://purl.org/ontology/bibo/doi',
-//         },
-//     },
-// };
-
-// ezs.use(ezsLocals);
-// describe('conversion to extended Nquads', () => {
-//     let sandbox;
-//     beforeEach(() => {
-//         sandbox = sinon.sandbox.create();
-//         const s = sandbox.stub(request, 'get');
-//         s.onFirstCall().yields(null, { statusCode: 200 }, dataTest[0]);
-//         s.onSecondCall().yields(null, { statusCode: 200 }, dataTest[1]);
-//     });
-
-//     afterEach(() => {
-//         sandbox.restore();
-//     });
-
-//     it('should return nquads from the dataset', done => {
-//         /* Fake URL */
-//         const stream = from([
-//             {
-//                 lodex: { uri: 'https://lodex-uri.fr/URI' },
-//                 content: 'https://api-v5.istex.fr/document/?q=language:test',
-//             },
-//         ])
-//             .pipe(ezs('scroll'))
-//             .pipe(ezs('convertToExtendedJsonLd', { config }));
-//         testAll(
-//             stream,
-//             data => {
-//                 const lines = data.split('\n');
-//                 expect(dataNquads.includes(lines[0]));
-//                 expect(dataNquads.includes(lines[1]));
-//                 expect(dataNquads.includes(lines[2]));
-//                 expect(dataNquads.includes(lines[3]));
-//                 expect(dataNquads.includes(lines[4]));
-//                 expect(dataNquads.includes(lines[5]));
-//                 expect(dataNquads.includes(lines[6]));
-//             },
-//             done,
-//         );
-//     });
-// });
-
 import expect from 'expect';
 import ezs from 'ezs';
 import from from 'from';
@@ -74,7 +6,7 @@ import testOne from './testOne';
 const statements = require('.');
 ezs.use(statements);
 
-describe.only('convertJSonLdToNquads', () => {
+describe('convertJSonLdToNquads', () => {
     it('should convert a correct JSON-LD into NQuads', done => {
         // see https://json-ld.org/playground/ for the Person example
         const stream = from([
