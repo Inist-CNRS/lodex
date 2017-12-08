2d203ca921d48596807b6193cc43aec1cef188f2
ThieryMichel <thiery@marmelab.com>
2017-12-08 15:17:48 +0100

    code review

--- src/app/js/lib/components/ExportableComponent.js
+++ src/app/js/lib/components/ExportableComponent.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import FlatButton from 'material-ui/FlatButton';
 import { CardTitle, CardActions } from 'material-ui/Card';
-import { grey500 } from 'material-ui/styles/colors';
+import { grey500, red500 } from 'material-ui/styles/colors';
 
 import convertHtmlToPng from '../convertHtmlToPng';
 
@@ -19,28 +18,53 @@ const styles = {
         fontSize: '2rem',
         textDecoration: 'none',
     },
+    error: {
+        color: red500,
+        alignSelf: 'flex-end',
+    },
 };
 
 class ExportableComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { error: null };
+    }
+
     exportElement = () => {
-        const element = ReactDOM.findDOMNode(this).childNodes[1]; //eslint-disable-line
-        convertHtmlToPng(element).then(uri => {
-            const link = document.createElement('a');
-            link.download = this.props.label;
-            link.href = uri;
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        });
+        this.setState({ error: null });
+        convertHtmlToPng(this.element)
+            .then(uri => {
+                const link = document.createElement('a');
+                link.download = this.props.label;
+                link.href = uri;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            })
+            .catch(error => {
+                this.setState({
+                    error:
+                        'We encountered an error while trying to export this chart.',
+                });
+                console.error(error.message);
+            });
     };
 
     render() {
         const { children, label } = this.props;
+        const { error } = this.state;
         return (
             <div>
                 <CardTitle title={<span style={styles.label}>{label}</span>} />
-                {children}
+                <div
+                    ref={element => {
+                        this.element = element;
+                    }}
+                >
+                    {children}
+                </div>
                 <CardActions style={styles.actions}>
+                    {error && <p style={styles.error}>{error}</p>}
                     <FlatButton
                         primary
                         style={styles.button}
