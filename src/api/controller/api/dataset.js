b01cbc0022c22184ea2c6f27eda803bee6e23727
ThieryMichel <thiery@marmelab.com>
2017-12-11 11:33:12 +0100

    clear publishedFacet when clearing dataset to avoid duplicate on republish

--- src/api/controller/api/dataset.js
+++ src/api/controller/api/dataset.js
@@ -7,6 +7,7 @@ export const clearDataset = async ctx => {
     try {
         await ctx.publishedDataset.remove({});
         await ctx.publishedCharacteristic.remove({});
+        await ctx.publishedFacet.remove({});
         await ctx.field.remove({});
         await ctx.uriDataset.remove({});
         await ctx.dataset.remove({});
