ecd2bad36bc9abfa80f919622b640bd4918d1d70
ThieryMichel <thiery@marmelab.com>
2017-12-13 12:13:50 +0100

    add injectData helper in format and use it for chart

--- src/app/js/formats/distributionChart/global-bar-chart/index.js
+++ src/app/js/formats/distributionChart/global-bar-chart/index.js
@@ -1,9 +1,10 @@
 import Component from './BarChartView';
 import AdminComponent from '../AdminComponent';
 import DefaultFormat from '../../DefaultFormat';
+import injectData from '../../injectData';
 
 export default {
-    Component,
+    Component: injectData(Component),
     ListComponent: DefaultFormat.ListComponent,
     AdminComponent,
     EditionComponent: DefaultFormat.EditionComponent,
--- src/app/js/formats/distributionChart/global-radar-chart/index.js
+++ src/app/js/formats/distributionChart/global-radar-chart/index.js
@@ -1,9 +1,10 @@
 import Component from './RadarChartView';
 import AdminComponent from '../AdminComponent';
 import DefaultFormat from '../../DefaultFormat';
+import injectData from '../../injectData';
 
 export default {
-    Component,
+    Component: injectData(Component),
     ListComponent: DefaultFormat.ListComponent,
     AdminComponent,
     EditionComponent: DefaultFormat.EditionComponent,
