888f6f7992552c13c8666b2ae02d1a2510338150
ThieryMichel <thiery@marmelab.com>
2017-12-13 11:26:35 +0100

    handle no data on graphe format View

--- src/app/js/formats/distributionChart/global-pie-chart/PieChartView.js
+++ src/app/js/formats/distributionChart/global-pie-chart/PieChartView.js
@@ -3,37 +3,50 @@ import PropTypes from 'prop-types';
 import translate from 'redux-polyglot/translate';
 import { ResponsiveContainer, PieChart, Pie, Cell, Legend } from 'recharts';
 
-import { field as fieldPropTypes } from '../../../propTypes';
+import {
+    field as fieldPropTypes,
+    polyglot as polyglotPropTypes,
+} from '../../../propTypes';
 import CustomizedLabel from './CustomizedLabel';
 
-const PieChartView = ({ chartData, colorSet }) => (
-    <ResponsiveContainer className="lodex-chart" width="100%" height={300}>
-        <PieChart>
-            <Legend verticalAlign="middle" layout="vertical" align="right" />
-            <Pie
-                cx={155}
-                data={chartData}
-                fill="#8884d8"
-                outerRadius="63%"
-                labelLine
-                label={CustomizedLabel}
-            >
-                {chartData.map((entry, index) => (
-                    <Cell
-                        key={String(index).concat('_cell_pie')}
-                        fill={colorSet[index % colorSet.length]}
-                    />
-                ))}
-            </Pie>
-        </PieChart>
-    </ResponsiveContainer>
-);
+const PieChartView = ({ chartData, colorSet, p: polyglot }) => {
+    if (!chartData) {
+        return <p>{polyglot.t('no_data')}</p>;
+    }
+    return (
+        <ResponsiveContainer className="lodex-chart" width="100%" height={300}>
+            <PieChart>
+                <Legend
+                    verticalAlign="middle"
+                    layout="vertical"
+                    align="right"
+                />
+                <Pie
+                    cx={155}
+                    data={chartData}
+                    fill="#8884d8"
+                    outerRadius="63%"
+                    labelLine
+                    label={CustomizedLabel}
+                >
+                    {chartData.map((entry, index) => (
+                        <Cell
+                            key={String(index).concat('_cell_pie')}
+                            fill={colorSet[index % colorSet.length]}
+                        />
+                    ))}
+                </Pie>
+            </PieChart>
+        </ResponsiveContainer>
+    );
+};
 
 PieChartView.propTypes = {
     field: fieldPropTypes.isRequired,
     resource: PropTypes.object.isRequired,
     chartData: PropTypes.array.isRequired,
     colorSet: PropTypes.arrayOf(PropTypes.string),
+    p: polyglotPropTypes,
 };
 
 PieChartView.defaultProps = {
