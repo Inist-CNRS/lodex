59fd084ceafe618837df9e1c0e6cd866bf15cdd5
ThieryMichel <thiery@marmelab.com>
2017-12-15 16:57:22 +0100

    bootstrap cartography format

---
+++ src/app/js/formats/cartography/CartographyView.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CartographyView = () => <p>Cartography place holder</p>;
+
+export default CartographyView;
---
+++ src/app/js/formats/cartography/index.js
@@ -0,0 +1,9 @@
+import Component from './CartographyView';
+import AdminComponent from '../DefaultAdminComponentWithLabel';
+import DefaultFormat from '../DefaultFormat';
+
+export default {
+    Component,
+    AdminComponent,
+    EditionComponent: DefaultFormat.EditionComponent,
+};
