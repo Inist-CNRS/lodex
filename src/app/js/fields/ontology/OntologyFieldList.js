2fe7df21b36c5994b384f337d2daeea99be59e11
ThieryMichel <thiery@marmelab.com>
2018-01-05 15:24:12 +0100

    use collection to separate dataset fiel from document field, and use field position directly rather than index

--- src/app/js/fields/ontology/OntologyFieldList.js
+++ src/app/js/fields/ontology/OntologyFieldList.js
@@ -3,16 +3,19 @@ import { SortableContainer } from 'react-sortable-hoc';
 import { TableBody } from 'material-ui/Table';
 
 import OntologyField from './OntologyField';
+import { COVER_DATASET } from '../../../../common/cover';
 
 const OntologyFieldList = SortableContainer(({ items }) => (
     <TableBody>
-        {items.map((field, index) => (
+        {items.map(field => (
             <OntologyField
                 key={field.name}
                 field={field}
-                index={index}
-                disabled={index === 0}
-                sortIndex={index}
+                index={field.position}
+                collection={
+                    field.cover === COVER_DATASET ? 'dataset' : 'document'
+                }
+                disabled={field.position === 0}
             />
         ))}
     </TableBody>
