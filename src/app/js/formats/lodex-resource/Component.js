c7dbef288a63dfbbf4c3d4ab1e5444a32ad6caca
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-01-08 21:49:41 +0100

    e fetch-with-proxy

--- src/app/js/formats/lodex-resource/Component.js
+++ src/app/js/formats/lodex-resource/Component.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import fetch from 'omni-fetch';
+import fetch from 'fetch-with-proxy';
 import translate from 'redux-polyglot/translate';
 import url from 'url';
 import MQS from 'mongodb-querystring';
