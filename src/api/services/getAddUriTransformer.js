2e459e1ec18ab437ba1e5dc67eb6504385088f24
ThieryMichel <thiery@marmelab.com>
2018-01-11 09:54:40 +0100

    add comment

--- src/api/services/getAddUriTransformer.js
+++ src/api/services/getAddUriTransformer.js
@@ -2,9 +2,10 @@ import omit from 'lodash.omit';
 
 import getDocumentTransformer from './getDocumentTransformer';
 
+// take a doc remove its _id and add its uri
 export const addUriToDoc = getUriDoc => async doc => ({
-    ...omit(doc, ['_id']),
-    ...(await getUriDoc(doc)),
+    ...omit(doc, ['_id']), // we return the
+    ...(await getUriDoc(doc)), // getUriDoc return { uri: uri value }
 });
 
 export default (fetchLineBy, uriCol) => {
