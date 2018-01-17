86d56eb99c9d227f9f3e9e56b09030b00036326c
ThieryMichel <thiery@marmelab.com>
2018-01-17 16:00:01 +0100

    remove all label validation/restriction

--- src/app/js/fields/FieldLabelInput.js
+++ src/app/js/fields/FieldLabelInput.js
@@ -3,19 +3,11 @@ import PropTypes from 'prop-types';
 import compose from 'recompose/compose';
 import { connect } from 'react-redux';
 import translate from 'redux-polyglot/translate';
-import memoize from 'lodash.memoize';
 import get from 'lodash.get';
 
 import FormTextField from '../lib/components/FormTextField';
 import FieldInput from '../lib/components/FieldInput';
 import { fromFields } from '../sharedSelectors';
-import {
-    field as fieldPropTypes,
-    polyglot as polyglotPropTypes,
-} from '../propTypes';
-
-const required = polyglot => value =>
-    value ? undefined : polyglot.t('required');
 
 export const uniqueField = (fields, polyglot) => (value, _, props) => {
     // retrieve previous label of the edited field if any (either in props.field of props.fieldToAdd)
@@ -32,30 +24,21 @@ export const uniqueField = (fields, polyglot) => (value, _, props) => {
         : undefined;
 };
 
-const getValidation = memoize((fields, polyglot) => [
-    required(polyglot),
-    uniqueField(fields, polyglot),
-]);
-
-export const FieldLabelInputComponent = ({ fields, disabled, p: polyglot }) => (
+export const FieldLabelInputComponent = ({ disabled }) => (
     <FieldInput
         name="label"
         component={FormTextField}
         labelKey="fieldLabel"
         fullWidth
         disabled={disabled}
-        validate={getValidation(fields, polyglot)}
     />
 );
 
 FieldLabelInputComponent.propTypes = {
-    fields: PropTypes.arrayOf(fieldPropTypes).isRequired,
     disabled: PropTypes.bool,
-    p: polyglotPropTypes.isRequired,
 };
 
 FieldLabelInputComponent.defaultProps = {
-    validate: false,
     isNewField: false,
     disabled: false,
 };
