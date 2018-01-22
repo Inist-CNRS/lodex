907db826ee1c5a6aabfa13128fdbedad686889f5
ThieryMichel <thiery@marmelab.com>
2018-01-22 14:52:08 +0100

    display Composite and linked field inside GraphLink to emphasize semantic relation

--- src/app/js/public/graph/GraphPage.js
+++ src/app/js/public/graph/GraphPage.js
@@ -66,6 +66,7 @@ const GraphPage = ({ graphField, resource }) => (
                     <PropertyLinkedFields
                         fieldName={graphField.name}
                         resource={resource}
+                        parents={[]}
                     />
                 </Card>
             )}
