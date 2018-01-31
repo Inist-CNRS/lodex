590df56065353bf9e7a09377650bba1f2287e7e9
ThieryMichel <thiery@marmelab.com>
2018-01-31 16:53:55 +0100

    readd timestamp in log

--- src/api/services/logger.js
+++ src/api/services/logger.js
@@ -1,6 +1,6 @@
 import winston from 'winston';
 
 export default new winston.Logger({
-    transports: [new winston.transports.Console()],
+    transports: [new winston.transports.Console({ timestamp: true })],
     level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
 });
