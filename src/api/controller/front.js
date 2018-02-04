d3fc88d4e673a2c6630e1410dca62196346c69fb
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-02-04 21:00:08 +0100

    to avoid to lock /login if userAuth is defined

--- src/api/controller/front.js
+++ src/api/controller/front.js
@@ -200,7 +200,7 @@ if (config.userAuth) {
     );
 
     app.use(async (ctx, next) => {
-        if (!ctx.state.cookie && ctx.request.url !== '/login') {
+        if (!ctx.state.cookie && ctx.request.url !== '/login' && ctx.request.url !== '/index.js') {
             ctx.redirect('/login');
             return;
         }
