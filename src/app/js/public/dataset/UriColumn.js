86dc20fda5986af5e2b9dc8f3bae65ad164d187b
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-03-07 23:47:34 +0100

    use flat button instead of link

--- src/app/js/public/dataset/UriColumn.js
+++ src/app/js/public/dataset/UriColumn.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Link } from 'react-router';
 import { TableRowColumn } from 'material-ui/Table';
+import FlatButton from 'material-ui/FlatButton';
+import RightIcon from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
 
 import { field as fieldPropTypes } from '../../propTypes';
 import { getResourceUri } from '../../../../common/uris';
@@ -11,7 +13,12 @@ const UriColumn = ({ column, resource, indice }) => (
     <TableRowColumn
         className={classnames('dataset-column', `dataset-${column.name}`)}
     >
-        <Link to={getResourceUri(resource)}> #{indice} </Link>
+        <FlatButton
+            labelPosition="after"
+            label={indice}
+            containerElement={<Link to={getResourceUri(resource)} />}
+            icon={<RightIcon />}
+        />
     </TableRowColumn>
 );
 
