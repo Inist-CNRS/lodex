ac1e769952e6523dfc6a8393c948c58ddb38b499
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-03-18 22:43:35 +0100

    sort keys

--- src/api/statements/fixFlatten.js
+++ src/api/statements/fixFlatten.js
@@ -4,20 +4,24 @@ export default function fixFlatten(data, feed) {
         return;
     }
     const obj = {};
-    Object.keys(data).forEach(key => {
-        const val = Array.isArray(data[key]) ? data[key].join(';') : data[key];
-        if (val.length > 0) {
-            if (key.match(/\/[0-9]+$/)) {
-                const newkey = key.replace(/(\/[0-9]+$)/, '');
-                if (obj[newkey]) {
-                    obj[newkey] += ';' + val;
+    Object.keys(data)
+        .sort((x, y) => x.localeCompare(y))
+        .forEach(key => {
+            const val = Array.isArray(data[key])
+                ? data[key].join(';')
+                : data[key];
+            if (val.length > 0) {
+                if (key.match(/\/[0-9]+$/)) {
+                    const newkey = key.replace(/(\/[0-9]+$)/, '');
+                    if (obj[newkey]) {
+                        obj[newkey] += ';' + val;
+                    } else {
+                        obj[newkey] = val;
+                    }
                 } else {
-                    obj[newkey] = val;
+                    obj[key] = val;
                 }
-            } else {
-                obj[key] = val;
             }
-        }
-    });
+        });
     feed.send(obj);
 }
