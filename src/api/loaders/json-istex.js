6f544e5bd4be7e18b86d7bace504a7c5423248f8
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-03-27 23:03:46 +0200

    now fixFlatten will work

--- src/api/loaders/json-istex.js
+++ src/api/loaders/json-istex.js
@@ -5,11 +5,9 @@ import ezsLocals from '../statements';
 ezs.use(ezsBasics);
 ezs.use(ezsLocals);
 
-ezs.use(ezsBasics);
-
 export default config => stream =>
     stream
         .pipe(ezs('JSONParse', { separator: 'hits.*', ...config }))
-        .pipe(ezs('flatten'))
+        .pipe(ezs('flatten', { safe: false }))
         .pipe(ezs('fixFlatten'))
         .pipe(ezs('standardize'));
