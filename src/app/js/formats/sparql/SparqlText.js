92e5a5abba8db68a32d395bc88e92a5bf61f2c47
chaBiselx <cha.biselx@gmail.com>
2018-04-20 13:38:42 +0200

    requete axios

--- src/app/js/formats/sparql/SparqlText.js
+++ src/app/js/formats/sparql/SparqlText.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { field as fieldPropTypes } from '../../propTypes';
-//import getLabel from '../shared/getLabel';
+import axios from 'axios';
 
-const sparqlText = ({ className /*, field, resource*/ }) => {
-    //const value = resource[field.name];
+const sparqlText = async ({ className, field, resource }) => {
+    const Tvalue = resource[field.name]; // eslint-disable-line
     const value =
         'PREFIX bibo: <http://purl.org/ontology/bibo/> SELECT count(?doi) FROM <https://inist-category.data.istex.fr/notice/graph> WHERE { ?subject bibo:doi ?doi }';
-    const rawData = 'https://data.istex.fr/sparql/?query=' + value.trim();
+    const url = 'https://data.istex.fr/sparql/?query=' + value.trim();
+    const rawData = await getData(url);
+    console.log(rawData); // eslint-disable-line
+    //{rawData} <p> {rawData.head.vars[0]} </p>
+
     return (
-        <div>
-            <p className={className}> {rawData} </p>
+        <div className={className}>
+            <p>{url}</p>
         </div>
     );
+};
+
+const getData = async url => {
+    let data;
+    await axios
+        .get(url)
+        .then(function(response) {
+            data = response.data;
+        })
+        .catch(function(error) {
+            data = error;
+        });
+    return data;
 };
 
 sparqlText.propTypes = {
