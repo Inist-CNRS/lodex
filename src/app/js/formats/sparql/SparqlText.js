0a083837eb7bde19914b5dbda9281708119ed40d
chaBiselx <cha.biselx@gmail.com>
2018-04-20 08:36:28 +0200

    Base propre

--- src/app/js/formats/sparql/SparqlText.js
+++ src/app/js/formats/sparql/SparqlText.js
@@ -1,56 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { field as fieldPropTypes } from '../../propTypes';
-import getLabel from '../shared/getLabel';
+//import getLabel from '../shared/getLabel';
 
-const LinkView = ({
-    className,
-    linkedResource,
-    resource,
-    field,
-    fields,
-    type,
-    sparql,
-}) => {
-    const label = getLabel(
-        field,
-        linkedResource,
-        resource,
-        fields,
-        type,
-        sparql,
-    );
-
-    if (Array.isArray(resource[field.name])) {
-        const links = resource[field.name];
-
-        return (
-            <div>
-                <h1> {links} </h1>
-            </div>
-        );
-    }
-
-    const link = resource[field.name];
+const sparqlText = ({ className, field, resource }) => {
+    const value = resource[field.name];
+    const rawData = value;
+    //console.log(value);
     return (
-        <a className={className} href={`${link}`}>
-            {label}
-        </a>
+        <div>
+            <p className={className}> {rawData} </p>
+        </div>
     );
 };
 
-LinkView.propTypes = {
+sparqlText.propTypes = {
     className: PropTypes.string,
     field: fieldPropTypes.isRequired,
-    fields: PropTypes.arrayOf(fieldPropTypes).isRequired,
-    linkedResource: PropTypes.object,
     resource: PropTypes.object.isRequired,
-    type: PropTypes.string.isRequired,
-    sparql: PropTypes.string.isRequired,
 };
 
-LinkView.defaultProps = {
+sparqlText.defaultProps = {
     className: null,
 };
 
-export default LinkView;
+export default sparqlText;
