4cab33fccda67e36875c5e65021e6e2486101588
chaBiselx <cha.biselx@gmail.com>
2018-04-20 09:25:16 +0200

    Creation de l'URL

--- src/app/js/formats/sparql/SparqlText.js
+++ src/app/js/formats/sparql/SparqlText.js
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import { field as fieldPropTypes } from '../../propTypes';
 //import getLabel from '../shared/getLabel';
 
-const sparqlText = ({ className, field, resource }) => {
-    const value = resource[field.name];
-    const rawData = value;
-    //console.log(value);
+const sparqlText = ({ className /*, field, resource*/ }) => {
+    //const value = resource[field.name];
+    const value =
+        'PREFIX bibo: <http://purl.org/ontology/bibo/> SELECT count(?doi) FROM <https://inist-category.data.istex.fr/notice/graph> WHERE { ?subject bibo:doi ?doi }';
+    const rawData = 'https://data.istex.fr/sparql/?query=' + value.trim();
     return (
         <div>
             <p className={className}> {rawData} </p>
