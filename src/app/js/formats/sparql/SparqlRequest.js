903206ebbed5561d378931458d972827fa93b2d7
chaBiselx <cha.biselx@gmail.com>
2018-05-11 14:04:56 +0200

    ajout console log pour la maintenance (cross-origin ou pas)

--- src/app/js/formats/sparql/SparqlRequest.js
+++ src/app/js/formats/sparql/SparqlRequest.js
@@ -81,6 +81,10 @@ export default url => FormatView => {
                 error,
                 ...props
             } = this.props;
+
+            // for maintenance
+            console.log(error);//eslint-disable-line
+
             if (error) {
                 return (
                     <p style={styles.message}>{polyglot.t('sparql_error')}</p>
