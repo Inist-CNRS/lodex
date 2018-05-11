6070f80046ac34984dc26119c594401cf4e81acf
chaBiselx <cha.biselx@gmail.com>
2018-05-11 15:52:25 +0200

    credential test corrected

--- src/app/js/user/index.spec.js
+++ src/app/js/user/index.spec.js
@@ -79,7 +79,7 @@ describe('user reducer', () => {
             expect(result).toEqual({
                 url: 'url',
                 body: '{"data":"value"}',
-                credentials: 'same_origin',
+                credentials: 'same-origin',
                 headers: {
                     Accept: 'application/json',
                     'Content-Type': 'application/json',
