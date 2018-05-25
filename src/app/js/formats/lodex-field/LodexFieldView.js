59117e81ab4590a5842fbcfe2a816fa350af3558
chaBiselx <cha.biselx@gmail.com>
2018-05-25 10:28:29 +0200

    start to load label with value + routine

--- src/app/js/formats/lodex-field/LodexFieldView.js
+++ src/app/js/formats/lodex-field/LodexFieldView.js
@@ -1,55 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import translate from 'redux-polyglot/translate';
-import { StyleSheet, css } from 'aphrodite/no-important';
 import compose from 'recompose/compose';
-import { connect } from 'react-redux';
-import get from 'lodash.get';
 import URL from 'url';
 
 import { isURL } from '../../../../common/uris.js';
-import LodexResource from '../shared/LodexResource';
 import { field as fieldPropTypes } from '../../propTypes';
 import injectData from '../injectData';
 
-const LodexResourceView = props => {
-    const styles = StyleSheet.create({
-        wrapper: {
-            padding: '1.1em',
-            borderRadius: '3px',
-            background: 'white',
-            boxShadow: '0px 6px 6px rgba(170, 170, 170, 0.25)',
-        },
-    });
+export class LodexResourceView extends Component {
+    toto = label => {
+        const { className, formatData } = this.props;
+        return formatData.map((data, key) => {
+            if (label == data.id) {
+                return (
+                    <div key={key}>
+                        <div className={('lodex_field_label', className)}>
+                            {data.label} :
+                        </div>
+                        <div className={('lodex_field_value', className)}>
+                            {data.value}
+                        </div>
+                    </div>
+                );
+            } else {
+                return null;
+            }
+        });
+    };
 
-    return (
-        <div className={css(styles.wrapper)}>
-            <LodexResource {...props} />
-        </div>
-    );
-};
+    render() {
+        const { className, formatData, param } = this.props;
+        if (formatData == undefined) {
+            return <span> </span>;
+        }
+        let labelArray = param.labelArray.map(e => e.trim()); //clean string
+        return (
+            <div className={className}>
+                {labelArray.map(label => {
+                    return this.toto(label);
+                })}
+            </div>
+        );
+    }
+}
 
 LodexResourceView.propTypes = {
+    className: PropTypes.string,
+    formatData: PropTypes.arrayOf(PropTypes.object),
+    param: PropTypes.object,
     field: fieldPropTypes.isRequired,
     resource: PropTypes.object.isRequired,
 };
 
 LodexResourceView.defaultProps = {
     className: null,
-};
-
-const mapStateToProps = (state, { formatData = {} }) => {
-    const { id = '', url = '', title = 'n/a', summary = '' } = get(
-        formatData,
-        'items[0]',
-        {},
-    );
-    return {
-        id,
-        url,
-        title,
-        summary,
-    };
 };
 
 export default compose(
@@ -60,7 +65,6 @@ export default compose(
         if (!value) {
             return null;
         }
-        console.log(value); //eslint-disable-line
 
         if (isURL(value)) {
             const source = URL.parse(value);
@@ -70,17 +74,16 @@ export default compose(
                     protocol: source.protocol,
                     hostname: source.hostname,
                     slashes: source.slashes,
-                    pathname: '/api/run/syndication/', //remove
+                    port: source.port,
+                    pathname: '/api/run/json-document/',
                     search: `?$query[uri]=${encodeURIComponent(uri)}`,
                 };
-                console.log(URL.format(target)); //eslint-disable-line
                 return URL.format(target);
             }
             return value;
         }
-        return `/api/run/syndication/?$query[uri]=${encodeURIComponent(
-            resource[field.name], //remove
+        return `/api/run/json-document/?$query[uri]=${encodeURIComponent(
+            resource[field.name],
         )}`;
     }),
-    connect(mapStateToProps),
 )(LodexResourceView);
