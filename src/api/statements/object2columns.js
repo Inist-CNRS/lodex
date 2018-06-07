19c33e40b5f6bf63f5534fe31e2563c14a554901
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-06-07 15:04:30 +0200

    useless test

--- src/api/statements/object2columns.js
+++ src/api/statements/object2columns.js
@@ -7,7 +7,7 @@ export default function object2columns(data, feed) {
     Object.keys(data)
         .sort((x, y) => x.localeCompare(y))
         .forEach(key => {
-            if (Array.isArray(data[key]) || typeof data[key] === 'object') {
+            if (typeof data[key] === 'object') {
                 obj[key] = JSON.stringify(data[key]);
             } else {
                 obj[key] = data[key];
