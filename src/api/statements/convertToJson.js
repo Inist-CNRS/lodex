7026a59c2d92dc6ed4f452150927ae28e378dff9
chaBiselx <cha.biselx@gmail.com>
2018-06-13 14:23:27 +0200

    fix conert to json

--- src/api/statements/convertToJson.js
+++ src/api/statements/convertToJson.js
@@ -1,36 +1,51 @@
 module.exports = function convertToJson(input, output) {
-    // console.log('data', data); //resource
-    // console.log('feed', feed); //output
+    const fields = this.getParam('fields', {});
 
     if (!input) {
         return output.close();
     }
 
-    // const getLabel = input.reduce(
-    //     (data, field) => {
-    //         data[field.name] = field.label;
+    // const getLabel = () =>
+    //     fields.reduce(
+    //         (data, field) => {
+    //             data[field.name] = field.label;
     //
-    //         return data;
-    //     },
-    //     { uri: 'uri' },
-    // );
+    //             return data;
+    //         },
+    //         { uri: 'uri' },
+    //     );
 
-    // const getLang = input.reduce(
-    //     (data, field) => {
+    const getLabel = fields.reduce(
+        (data, field) => {
+            data[field.name] = field.label;
+
+            return data;
+        },
+        { uri: 'uri' },
+    );
+
+    // const getLang = () =>
+    //     fields.reduce((data, field) => {
     //         data[field.name] = field.language;
-    //
     //         return data;
-    //     },
-    //     { uri: 'uri' },
-    // );
+    //     });
+
+    const getLang = fields.reduce(
+        (data, field) => {
+            data[field.name] = field.language;
+
+            return data;
+        },
+        { uri: 'uri' },
+    );
 
     const changedFields = Object.entries(input)
         .filter(([name]) => name !== 'uri')
         .map(([name, value]) => ({
             name,
             value,
-            // label: getLabel[name],
-            // language: getLang[name],
+            label: getLabel[name],
+            language: getLang[name],
         }));
 
     const changedResource = {
@@ -38,7 +53,6 @@ module.exports = function convertToJson(input, output) {
         fields: changedFields,
     };
 
-    // console.log(changedResource);
     output.send(changedResource);
 
     return output.end();
