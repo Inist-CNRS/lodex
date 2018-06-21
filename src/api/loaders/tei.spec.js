a29383629117657c6db4afcd1c96f653d6eaa142
François Parmentier <francois.parmentier@gmail.com>
2018-06-21 15:34:16 +0200

    Add TEI loader test

---
+++ src/api/loaders/tei.spec.js
@@ -0,0 +1,20 @@
+import expect from 'expect';
+import ezs from 'ezs';
+import from from 'from';
+
+describe('tei.ini', () => {
+    it('should parse a TEI XML', done => {
+        const res = [];
+        from([
+            `<teiCorpus><TEI><any>value</any><any>other</any></TEI></teiCorpus>`,
+        ])
+            .pipe(ezs.fromFile(__dirname + '/tei.ini'))
+            .on('data', chunk => {
+                res.push(chunk);
+            })
+            .on('end', () => {
+                expect(res).toEqual([ { any: [ { $t: 'value' }, { $t: 'other' } ] } ]); // eslint-disable-line
+                done();
+            });
+    });
+});
