206ec88347142939e13ae5b46502ca46b34d24dc
chaBiselx <cha.biselx@gmail.com>
2018-06-21 15:19:04 +0200

    add test

--- src/common/transformers/SPLIT.spec.js
+++ src/common/transformers/SPLIT.spec.js
@@ -2,7 +2,7 @@ import expect from 'expect';
 
 import { split } from './SPLIT';
 
-describe('SPLIT', () => {
+describe.only('SPLIT', () => {
     it('should return splitted value', () => {
         expect(split('hello dear world', ' dear ')).toEqual(['hello', 'world']);
     });
@@ -15,5 +15,9 @@ describe('SPLIT', () => {
 
     it('should throw an error if value is not a string', () => {
         expect(() => split([], ' ')).toThrow('Invalid value: need a string');
+    });
+
+    it('should return without extra with space', () => {
+        expect(split('hello ; ; world', ';')).toEqual(['hello', 'world']);
     });
 });
