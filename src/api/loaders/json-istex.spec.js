9b53277c1a96766f50475cce0d1906f3338743a3
François Parmentier <francois.parmentier@gmail.com>
2018-06-21 17:29:56 +0200

    Add JSON-ISTEX loader test

---
+++ src/api/loaders/json-istex.spec.js
@@ -0,0 +1,20 @@
+import expect from 'expect';
+import ezs from 'ezs';
+import from from 'from';
+
+describe('json-istex.ini', () => {
+    it('should parse a JSON', done => {
+        const res = [];
+        const expected = [{ a: '1', b: '2' }];
+        const input = { hits: expected };
+        from([JSON.stringify(input)])
+            .pipe(ezs.fromFile(__dirname + '/json-istex.ini'))
+            .on('data', chunk => {
+                res.push(chunk);
+            })
+            .on('end', () => {
+                expect(res).toEqual(expected);
+                done();
+            });
+    });
+});
