b76d2d2cc07f23fbc64aa228187b9eb83667bd84
François Parmentier <francois.parmentier@gmail.com>
2018-06-21 16:07:58 +0200

    Add JSON-LODEX loader test

---
+++ src/api/loaders/json-lodex.spec.js
@@ -0,0 +1,20 @@
+import expect from 'expect';
+import ezs from 'ezs';
+import from from 'from';
+
+describe('json-lodex.ini', () => {
+    it('should parse a JSON', done => {
+        const res = [];
+        const expected = [{ a: '1', b: '2' }];
+        const input = { data: expected };
+        from([JSON.stringify(input)])
+            .pipe(ezs.fromFile(__dirname + '/json-lodex.ini'))
+            .on('data', chunk => {
+                res.push(chunk);
+            })
+            .on('end', () => {
+                expect(res).toEqual(expected);
+                done();
+            });
+    });
+});
