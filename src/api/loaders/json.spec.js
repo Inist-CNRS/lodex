9f0400a8ac1352e953f79a84a224baabc780a997
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-06-21 15:09:52 +0200

    fix tests

--- src/api/loaders/json.spec.js
+++ src/api/loaders/json.spec.js
@@ -5,14 +5,14 @@ import from from 'from';
 describe('json.ini', () => {
     it('should parse a JSON', done => {
         const res = [];
-        const expected = { a: '1', b: '2' };
+        const expected = [{ a: '1', b: '2' }];
         from([JSON.stringify(expected)])
             .pipe(ezs.fromFile(__dirname + '/json.ini'))
             .on('data', chunk => {
                 res.push(chunk);
             })
             .on('end', () => {
-                expect(res).toEqual([expected]);
+                expect(res).toEqual(expected);
                 done();
             });
     });
--- src/api/loaders/xml.spec.js
+++ src/api/loaders/xml.spec.js
@@ -2,16 +2,19 @@ import expect from 'expect';
 import ezs from 'ezs';
 import from from 'from';
 
-describe.only('xml.ini', () => {
+describe('xml.ini', () => {
     it('should parse a RDF XML', done => {
         const res = [];
-        from([`<RDF><test>value</test></RDF>`])
+        from([
+            `<root><row><f1>val1</f1><f2>val2</f2></row><row><f1>val1</f1><f2>val2</f2></row></root>`,
+        ])
             .pipe(ezs.fromFile(__dirname + '/xml.ini'))
             .on('data', chunk => {
                 res.push(chunk);
             })
             .on('end', () => {
-                expect(res).toEqual([{ '$t': 'value' }]); // eslint-disable-line
+                expect(res.length).toEqual(2); // eslint-disable-line
+                expect(res).toEqual([ { 'f1/$t': 'val1', 'f2/$t': 'val2' }, { 'f1/$t': 'val1', 'f2/$t': 'val2' } ]); // eslint-disable-line
                 done();
             });
     });
