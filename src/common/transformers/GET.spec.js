c28cd049c5257561b1efb73196a5e16ad6c2e5ad
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-10-01 12:24:14 +0200

    disable .only

--- src/common/transformers/GET.spec.js
+++ src/common/transformers/GET.spec.js
@@ -2,7 +2,7 @@ import expect from 'expect';
 
 import { get } from './GET';
 
-describe.only('GET', () => {
+describe('GET', () => {
     it("should return key's value from object", () => {
         expect(get({ a: { aa: 1 }, b: 2 }, 'a.aa')).toEqual(1);
     });
