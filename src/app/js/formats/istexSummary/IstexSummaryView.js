ec73da8038d3c004c0b39d1262d3923f2eb77538
ThieryMichel <thiery@marmelab.com>
2018-10-15 10:18:55 +0200

    add error message when field has no value

--- src/app/js/formats/istexSummary/IstexSummaryView.js
+++ src/app/js/formats/istexSummary/IstexSummaryView.js
@@ -2,12 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import get from 'lodash.get';
 import { StyleSheet, css } from 'aphrodite/no-important';
+import compose from 'recompose/compose';
+import translate from 'redux-polyglot/translate';
 
-import { field as fieldPropTypes } from '../../propTypes';
+import {
+    field as fieldPropTypes,
+    polyglot as polyglotPropTypes,
+} from '../../propTypes';
 import injectData from '../injectData';
 import classnames from 'classnames';
 import IstexYear from './IstexYear';
 import { ISTEX_API_URL } from '../../../../common/externals';
+import AdminOnlyAlert from '../../lib/components/AdminOnlyAlert';
 
 const styles = StyleSheet.create({
     text: {
@@ -24,6 +30,10 @@ const styles = StyleSheet.create({
 export const getYearUrl = ({ resource, field }) => {
     const value = resource[field.name];
 
+    if (!value) {
+        return null;
+    }
+
     return `${ISTEX_API_URL}/document/?q=(${encodeURIComponent(
         `host.issn="${value}"`,
     )})&facet=publicationDate[perYear]&size=0&output=*`;
@@ -34,15 +44,30 @@ export const getYear = formatData =>
         .sort((a, b) => a.keyAsString - b.keyAsString)
         .map(({ keyAsString }) => keyAsString);
 
-export const IstexSummaryView = ({ formatData, field, resource }) => (
-    <ul className={classnames('istex-year', css(styles.text))}>
-        {getYear(formatData).map(year => (
-            <li key={year} className={css(styles.li)}>
-                <IstexYear issn={resource[field.name]} year={year} />
-            </li>
-        ))}
-    </ul>
-);
+export const IstexSummaryView = ({
+    formatData,
+    field,
+    resource,
+    p: polyglot,
+}) => {
+    if (!resource[field.name]) {
+        return (
+            <AdminOnlyAlert>
+                {polyglot.t('fetch_format_no_value_error')}
+            </AdminOnlyAlert>
+        );
+    }
+
+    return (
+        <ul className={classnames('istex-year', css(styles.text))}>
+            {getYear(formatData).map(year => (
+                <li key={year} className={css(styles.li)}>
+                    <IstexYear issn={resource[field.name]} year={year} />
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 IstexSummaryView.propTypes = {
     fieldStatus: PropTypes.string,
@@ -50,6 +75,7 @@ IstexSummaryView.propTypes = {
     field: fieldPropTypes.isRequired,
     formatData: PropTypes.shape({}),
     error: PropTypes.string,
+    p: polyglotPropTypes.isRequired,
 };
 
 IstexSummaryView.defaultProps = {
@@ -60,4 +86,4 @@ IstexSummaryView.defaultProps = {
     error: null,
 };
 
-export default injectData(getYearUrl)(IstexSummaryView);
+export default compose(injectData(getYearUrl), translate)(IstexSummaryView);
