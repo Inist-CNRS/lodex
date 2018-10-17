a826f392c2801894648f1c401fc1ffee57a55e49
ThieryMichel <thiery@marmelab.com>
2018-10-17 10:47:01 +0200

    add searchedField config to IstexSummary format

---
+++ src/app/js/formats/istexSummary/IstexSummaryAdmin.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import translate from 'redux-polyglot/translate';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+import { polyglot as polyglotPropTypes } from '../../propTypes';
+import updateAdminArgs from '../shared/updateAdminArgs';
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        width: '200%',
+        justifyContent: 'space-between',
+    },
+    input: {
+        width: '100%',
+    },
+};
+
+export const defaultArgs = {
+    searchedField: 'host.issn',
+};
+
+const searchedFieldValues = [
+    'host.issn',
+    'host.eissn',
+    'host.isbn',
+    'host.eisbn',
+    'host.title',
+];
+
+class IstexSummaryAdmin extends Component {
+    static propTypes = {
+        args: PropTypes.shape({
+            searchedField: PropTypes.oneOf(),
+        }),
+        onChange: PropTypes.func.isRequired,
+        p: polyglotPropTypes.isRequired,
+    };
+
+    static defaultProps = {
+        args: defaultArgs,
+    };
+
+    setSearchedField = (event, index, searchedField) => {
+        updateAdminArgs('searchedField', searchedField, this.props);
+    };
+
+    render() {
+        const { p: polyglot, args: { searchedField } } = this.props;
+
+        return (
+            <div style={styles.container}>
+                <SelectField
+                    floatingLabelText={polyglot.t('searched_field')}
+                    onChange={this.setSearchedField}
+                    style={styles.input}
+                    value={searchedField}
+                >
+                    {searchedFieldValues.map(value => (
+                        <MenuItem
+                            key={value}
+                            value={value}
+                            primaryText={polyglot.t(value)}
+                        />
+                    ))}
+                </SelectField>
+            </div>
+        );
+    }
+}
+
+export default translate(IstexSummaryAdmin);
--- src/app/js/formats/istexSummary/index.js
+++ src/app/js/formats/istexSummary/index.js
@@ -1,9 +1,11 @@
 import Component from './IstexSummaryView';
 import ListComponent from './IstexSummaryList';
+import AdminComponent from './IstexSummaryAdmin';
 import DefaultFormat from '../DefaultFormat';
 
 export default {
     ...DefaultFormat,
     Component,
     ListComponent,
+    AdminComponent,
 };
