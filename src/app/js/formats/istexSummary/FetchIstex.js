d50bd5d29d7ee6928391aba236f5f90ed2e3619b
ThieryMichel <thiery@marmelab.com>
2018-10-18 17:44:05 +0200

    display year by decade if more than fifty year

---
+++ src/app/js/formats/istexSummary/FetchIstex.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import CircularProgress from 'material-ui/CircularProgress';
+
+import { polyglot as polyglotPropTypes } from '../../propTypes';
+import AdminOnlyAlert from '../../lib/components/AdminOnlyAlert';
+
+const circularProgress = (
+    <CircularProgress
+        size={20}
+        innerStyle={{
+            display: 'flex',
+            marginLeft: 8,
+        }}
+    />
+);
+
+class FetchIstex extends Component {
+    state = {
+        data: null,
+        error: null,
+        isLoading: false,
+    };
+
+    componentWillMount() {
+        this.setState({ isLoading: true }, () => {
+            this.props
+                .getData(this.props)
+                .then(data => {
+                    this.setState({
+                        data,
+                        isLoading: false,
+                        isOpen: true,
+                    });
+                })
+                .catch(error => {
+                    console.error(error);
+                    this.setState({ error: true });
+                });
+        });
+    }
+
+    render() {
+        const { polyglot, children } = this.props;
+        const { error, data, isLoading } = this.state;
+
+        if (error) {
+            return <AdminOnlyAlert>{polyglot.t('istex_error')}</AdminOnlyAlert>;
+        }
+
+        if (isLoading) {
+            return circularProgress;
+        }
+
+        return children({ ...this.props, data });
+    }
+}
+
+FetchIstex.propTypes = {
+    label: PropTypes.string.isRequired,
+    polyglot: polyglotPropTypes.isRequired,
+    getData: PropTypes.func.isRequired,
+    children: PropTypes.func.isRequired,
+};
+
+export default FetchIstex;
--- src/app/js/formats/istexSummary/IstexSummaryView.js
+++ src/app/js/formats/istexSummary/IstexSummaryView.js
@@ -9,7 +9,7 @@ import {
 } from '../../propTypes';
 import injectData from '../injectData';
 import InvalidFormat from '../InvalidFormat';
-import { getYearUrl, parseYearData } from './getIstexData';
+import { getYearUrl, parseYearData, getDecadeData } from './getIstexData';
 import { searchedFieldValues } from './IstexSummaryAdmin';
 import composeRenderProps from '../../lib/composeRenderProps';
 import IstexList from './IstexList';
@@ -17,6 +17,8 @@ import IssueFold from './IssueFold';
 import VolumeFold from './VolumeFold';
 import YearFold from './YearFold';
 import IstexItem from '../istex/IstexItem';
+import FetchIstex from './FetchIstex';
+import DecadeFold from './DecadeFold';
 
 const FoldList = props => <IstexList {...props} />;
 const IstexDocument = ({ item }) => <IstexItem {...item} />;
@@ -37,9 +39,38 @@ export const IstexSummaryView = ({
         );
     }
 
+    const data = parseYearData(formatData);
+
+    if (data.length > 50) {
+        return composeRenderProps(
+            <FetchIstex
+                data={data}
+                issn={resource[field.name]}
+                searchedField={searchedField}
+                getData={getDecadeData({
+                    issn: resource[field.name],
+                    searchedField: searchedField,
+                })}
+                polyglot={polyglot}
+            />,
+            [
+                FoldList,
+                DecadeFold,
+                FoldList,
+                YearFold,
+                FoldList,
+                VolumeFold,
+                FoldList,
+                IssueFold,
+                FoldList,
+                IstexDocument,
+            ],
+        );
+    }
+
     return composeRenderProps(
         <FoldList
-            data={parseYearData(formatData)}
+            data={data}
             issn={resource[field.name]}
             searchedField={searchedField}
             polyglot={polyglot}
