9551b934c4c963795b2bd29fa530a052c1d4d696
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-10-23 09:52:52 +0200

    to choose text color for title (#750)
    * to choose text color for title
    
    * simple basic
    
    * useless

--- src/app/js/formats/title/TitleAdmin.js
+++ src/app/js/formats/title/TitleAdmin.js
@@ -2,27 +2,34 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
 import translate from 'redux-polyglot/translate';
 import { polyglot as polyglotPropTypes } from '../../propTypes';
+import updateAdminArgs from '../shared/updateAdminArgs';
 
 const styles = {
     container: {
-        display: 'inline-flex',
+        display: 'flex',
+        flexWrap: 'wrap',
+        width: '200%',
+        justifyContent: 'space-between',
     },
     input: {
-        marginLeft: '1rem',
+        width: '100%',
     },
 };
 
 export const defaultArgs = {
     level: 1,
+    textColor: '#000',
 };
 
 class TitleAdmin extends Component {
     static propTypes = {
-        args: {
+        args: PropTypes.shape({
             level: PropTypes.number,
-        },
+            textColor: PropTypes.string,
+        }),
         onChange: PropTypes.func.isRequired,
         p: polyglotPropTypes.isRequired,
     };
@@ -35,8 +42,13 @@ class TitleAdmin extends Component {
         this.props.onChange({ level });
     };
 
+    setTextColor = (_, color) => {
+        updateAdminArgs('textColor', color, this.props);
+    };
+
     render() {
         const { p: polyglot, args: { level } } = this.props;
+        const { textColor } = this.props.args;
 
         return (
             <div style={styles.container}>
@@ -53,6 +65,12 @@ class TitleAdmin extends Component {
                     <MenuItem value={3} primaryText={polyglot.t('level3')} />
                     <MenuItem value={4} primaryText={polyglot.t('level4')} />
                 </SelectField>
+                <TextField
+                    floatingLabelText={polyglot.t('text_color')}
+                    style={styles.input}
+                    onChange={this.setTextColor}
+                    value={textColor}
+                />
             </div>
         );
     }
