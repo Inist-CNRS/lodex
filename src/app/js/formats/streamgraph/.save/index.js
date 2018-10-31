29ae98b1199585e8ec0a4ed26174263382a4cd49
youlot <rhernandez@epsilon-alcen.com>
2018-10-31 10:29:41 +0100

    delete saved files

--- src/app/js/formats/streamgraph/.save/index.js
+++
@@ -1,9 +0,0 @@
-import Component from './Streamgraph';
-import AdminComponent from './StreamAdmin';
-import DefaultFormat from '../DefaultFormat';
-
-export default {
-    Component,
-    AdminComponent,
-    EditionComponent: DefaultFormat.EditionComponent,
-};
