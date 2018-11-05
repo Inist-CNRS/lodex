6ac19657afdbf5bf66e893fef8a1ab3e445cf7c6
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-11-05 15:46:09 +0100

    fix test 2

--- src/app/js/formats/redirect/RedirectView.spec.js
+++ src/app/js/formats/redirect/RedirectView.spec.js
@@ -4,12 +4,20 @@ import { shallow } from 'enzyme';
 import RedirectView from './RedirectView';
 
 describe('<RedirectView />', () => {
-    it('should render', () => {
+    it('should render if url', () => {
+        const resource = { foo: 'http://example.com' };
+        const field = { name: 'foo' };
+        const wrapper = shallow(
+            <RedirectView resource={resource} field={field} />,
+        );
+        expect(wrapper.find('a').text()).toEqual('http://example.com');
+    });
+    it('should render nothing if not url', () => {
         const resource = { foo: 'Run you fools!' };
         const field = { name: 'foo' };
         const wrapper = shallow(
             <RedirectView resource={resource} field={field} />,
         );
-        expect(wrapper.find('a').text()).toEqual('Run you fools!');
+        expect(wrapper.contains('a')).toBe(false);
     });
 });
