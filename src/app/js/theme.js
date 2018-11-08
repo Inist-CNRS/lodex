2b016e1b290582a1404e5b0b1e88a4db8ba0de88
Kmaschta <kevin@marmelab.com>
2018-11-08 12:10:48 +0100

    Implement applied facets for search

--- src/app/js/theme.js
+++ src/app/js/theme.js
@@ -2,6 +2,11 @@ export default {
     green: {
         primary: '#7DBD42',
     },
-    purple: { primary: '#B22F90' },
-    orange: { primary: '#F48022' },
+    purple: {
+        primary: '#B22F90',
+    },
+    orange: {
+        primary: '#F48022',
+        secondary: 'rgba(244, 128, 34, .7)',
+    },
 };
