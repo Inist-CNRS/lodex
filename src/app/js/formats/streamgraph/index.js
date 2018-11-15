07c36728c78ee66b3a6b0532dbfb485cdba4aec8
youlot <rhernandez@epsilon-alcen.com>
2018-11-15 16:28:12 +0100

    add streamgraph icon

--- src/app/js/formats/streamgraph/index.js
+++ src/app/js/formats/streamgraph/index.js
@@ -1,7 +1,9 @@
 import Component from './Streamgraph';
 import DefaultFormat from '../DefaultFormat';
+import Icon from './StreamgraphIcon';
 
 export default {
     ...DefaultFormat,
     Component,
+    Icon,
 };
