753fcc65d5d0c62496af354f91758aee7307bfe6
ThieryMichel <thiery@marmelab.com>
2018-11-16 10:10:32 +0100

    code review

--- src/app/js/formats/istexSummary/FetchFold.js
+++ src/app/js/formats/istexSummary/FetchFold.js
@@ -10,6 +10,7 @@ import get from 'lodash.get';
 
 import { polyglot as polyglotPropTypes } from '../../propTypes';
 import AdminOnlyAlert from '../../lib/components/AdminOnlyAlert';
+import SkipFold from './SkipFold';
 
 const styles = StyleSheet.create({
     li: {
@@ -46,55 +47,6 @@ const circularProgress = (
         }}
     />
 );
-
-class SkipFold extends Component {
-    state = {
-        data: null,
-        error: null,
-        isLoading: true,
-    };
-
-    UNSAFE_componentWillMount() {
-        this.props
-            .getData(this.props)
-            .then(data => {
-                this.setState({
-                    data,
-                    isLoading: false,
-                    isOpen: true,
-                });
-            })
-            .catch(error => {
-                console.error(error);
-                this.setState({ error: true, isLoading: false });
-            });
-    }
-
-    render() {
-        const { polyglot } = this.props;
-        const { data, isLoading, error } = this.state;
-
-        if (error) {
-            return <AdminOnlyAlert>{polyglot.t('istex_error')}</AdminOnlyAlert>;
-        }
-
-        if (isLoading) {
-            return circularProgress;
-        }
-
-        if (!data.hits.length) {
-            return null;
-        }
-
-        return this.props.children({ ...this.props, data });
-    }
-}
-
-SkipFold.propTypes = {
-    children: PropTypes.func.isRequired,
-    polyglot: polyglotPropTypes.isRequired,
-    getData: PropTypes.func.isRequired,
-};
 
 class FetchFold extends Component {
     state = {
