4e4b2aa14a67c574afcc45652b06327df989350f
ThieryMichel <thiery@marmelab.com>
2018-11-19 16:16:59 +0100

    better list format to display one input by value

--- src/app/js/formats/list/EditionComponent.js
+++ src/app/js/formats/list/EditionComponent.js
@@ -1,51 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Field } from 'redux-form';
+import { FieldArray, Field } from 'redux-form';
+import IconButton from 'material-ui/IconButton';
+import ActionDeleteIcon from 'material-ui/svg-icons/action/delete';
+import ActionAddIcon from 'material-ui/svg-icons/content/add';
+import translate from 'redux-polyglot/translate';
+import { StyleSheet, css } from 'aphrodite/no-important';
 
-import FormTextField from '../../lib/components/FormTextField';
+import { getEditionComponent } from '../';
+import {
+    field as fieldPropTypes,
+    polyglot as polyglotPropTypes,
+} from '../../propTypes';
 
-const format = v => {
-    try {
-        return v && v.join(';');
-    } catch (error) {
-        return error;
+const styles = StyleSheet.create({
+    item: {
+        display: 'flex',
+    },
+    input: {
+        flex: 'auto',
+    },
+    add: {
+        float: 'right',
+    },
+});
+
+const getSubFormat = args => ({
+    args: args.subFormatOptions,
+    name: args.subFormat,
+});
+
+class EditionComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.ItemComponent = getEditionComponent(
+            getSubFormat(props.field.format.args),
+        );
     }
-};
-const normalize = v => v && v.split(';');
 
-const SafeFormTextField = ({ input, label, polyglot, ...props }) => {
-    if (input.value instanceof Error) {
-        return <p>{polyglot.t('bad_format', { label })}</p>;
+    renderList = ({ fields }) => {
+        const { p: polyglot } = this.props;
+        return (
+            <div>
+                {fields.map((value, index) => (
+                    <div key={value} className={css(styles.item)}>
+                        <Field
+                            className={css(styles.input)}
+                            name={value}
+                            fullwidth
+                            component={this.ItemComponent}
+                        />
+
+                        <IconButton
+                            tooltip={polyglot.t('remove')}
+                            onClick={() => fields.remove(index)}
+                        >
+                            <ActionDeleteIcon />
+                        </IconButton>
+                    </div>
+                ))}
+                <IconButton
+                    className={css(styles.add)}
+                    tooltip={polyglot.t('add')}
+                    onClick={() => fields.push()}
+                >
+                    <ActionAddIcon />
+                </IconButton>
+            </div>
+        );
+    };
+
+    render() {
+        const { label, input } = this.props;
+
+        return (
+            <FieldArray
+                name={input.name}
+                component={this.renderList}
+                disabled={name === 'uri'}
+                label={label}
+                fullWidth
+            />
+        );
     }
-    return (
-        <FormTextField
-            {...props}
-            input={input}
-            label={label}
-            polyglot={polyglot}
-        />
-    );
-};
-
-const EditionComponent = ({ name, disabled, label, fullWidth, ...input }) => (
-    <Field
-        key={name}
-        name={name}
-        component={SafeFormTextField}
-        disabled={name === 'uri'}
-        label={label}
-        fullWidth
-        {...input}
-        format={format}
-        normalize={normalize}
-    />
-);
+}
 
 EditionComponent.propTypes = {
-    name: PropTypes.string.isRequired,
-    disabled: PropTypes.bool,
+    input: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+    }),
+    field: fieldPropTypes.isRequired,
+    p: polyglotPropTypes.isRequired,
     label: PropTypes.string.isRequired,
-    fullWidth: PropTypes.bool, // eslint-disable-line
+    fullWidth: PropTypes.bool,
 };
 
 EditionComponent.defaultProps = {
@@ -54,4 +101,4 @@ EditionComponent.defaultProps = {
 
 EditionComponent.isReduxFormReady = true;
 
-export default EditionComponent;
+export default translate(EditionComponent);
