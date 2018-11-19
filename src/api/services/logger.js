932b68cd204527d7f83638ef596ee24d4bba1847
Kmaschta <kevin@marmelab.com>
2018-11-19 17:56:13 +0100

    Upgrade config & winston

--- src/api/services/logger.js
+++ src/api/services/logger.js
@@ -1,9 +1,15 @@
-import winston from 'winston';
+import winston, { createLogger, format } from 'winston';
 import config from 'config';
+import { MESSAGE } from 'triple-beam';
 
-export default new winston.Logger({
-    transports: config.logger.disabled
-        ? []
-        : [new winston.transports.Console({ timestamp: true })],
+export default createLogger({
     level: process.env.NODE_ENV === 'development' ? 'debug' : 'info',
+    transports: [new winston.transports.Console()],
+    format: format.combine(
+        format.timestamp(),
+        format.colorize(),
+        format.simple(),
+        format.printf(info => `${info.timestamp} ${info[MESSAGE]}`),
+    ),
+    silent: config.logger.disabled,
 });
