5042e7329d4f6306e09431894debb58a7a99a227
François Parmentier <francois.parmentier@gmail.com>
2018-12-07 17:30:48 +0100

    :fire: Remove all duplicated statements (ezs-lodex)

--- src/api/statements/index.js
+++ src/api/statements/index.js
@@ -1,16 +1,5 @@
-/* eslint-disable  */
-import filterVersions from './filterVersions';
-import filterContributions from './filterContributions';
-import useFieldNames from './useFieldNames';
-import linkDataset from './linkDataset';
-import JSONLDCompacter from './JSONLDCompacter';
-import JSONLDString from './JSONLDString';
-import JSONLDObject from './JSONLDObject';
 import extractIstexQuery from './extractIstexQuery';
 import scroll from './scroll';
-import convertJsonLdToNQuads from './convertJsonLdToNQuads';
-import convertToJson from './convertToJson';
-import convertToExtendedJsonLd from './convertToExtendedJsonLd';
 import LodexContext from './LodexContext';
 import LodexConfig from './LodexConfig';
 import LodexRunQuery from './LodexRunQuery';
@@ -19,22 +8,10 @@ import LodexParseQuery from './LodexParseQuery';
 import LodexReduceQuery from './LodexReduceQuery';
 import LodexOutput from './LodexOutput';
 import LodexDocuments from './LodexDocuments';
-import fixFlatten from './fixFlatten';
-import objects2columns from './objects2columns';
 
 export default {
-    filterVersions,
-    filterContributions,
-    useFieldNames,
-    linkDataset,
-    JSONLDCompacter,
-    JSONLDString,
-    JSONLDObject,
     extractIstexQuery,
     scroll,
-    convertJsonLdToNQuads,
-    convertToJson,
-    convertToExtendedJsonLd,
     LodexContext,
     LodexConfig,
     LodexParseQuery,
@@ -43,6 +20,4 @@ export default {
     LodexSetField,
     LodexOutput,
     LodexDocuments,
-    fixFlatten,
-    objects2columns,
 };
