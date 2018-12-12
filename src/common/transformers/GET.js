09fdabcc0558476b7af35c7de46339c7be030088
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-12-12 11:53:39 +0100

    adding support for (lilou) multi paths

--- src/common/transformers/GET.js
+++ src/common/transformers/GET.js
@@ -1,8 +1,11 @@
 import _get from 'lodash.get';
+import _flatten from 'lodash.flatten';
 import { transformerWithArg } from './transformer';
 
 export const get = (value, path) => {
-    return _get(value, path, '');
+    const keys = path.split(';');
+    const values = _flatten(keys.map(key => _get(value, key, '')));
+    return values.length === 1 ? values[0] : values;
 };
 
 const transformation = (_, args) => value =>
--- src/common/transformers/GET.spec.js
+++ src/common/transformers/GET.spec.js
@@ -1,7 +1,24 @@
 import { get } from './GET';
 
-describe('GET', () => {
-    it("should return key's value from object", () => {
+describe.only('GET', () => {
+    it("should return key's value from object (number)", () => {
         expect(get({ a: { aa: 1 }, b: 2 }, 'a.aa')).toEqual(1);
     });
+
+    it("should return key's value from object (string)", () => {
+        expect(get({ a: { aa: '1' }, b: 2 }, 'a.aa')).toEqual('1');
+    });
+
+    it("should return all key's value from object", () => {
+        expect(get({ a: { aa: 1, bb: 1 }, b: 2 }, 'a.aa;a.bb')).toEqual([1, 1]);
+    });
+
+    it('should return nothing with empty path', () => {
+        expect(get({ a: { aa: 1, bb: 1 }, b: 2 }, '')).toEqual('');
+    });
+
+    it('should return nothing with empty paths', () => {
+        expect(get({ a: { aa: 1, bb: 1 }, b: 2 }, ';')).toEqual(['', '']);
+    });
+
 });
