1b31ada34a6c3223959ac9171a42aeccaef6c6d5
Nicolas Thouvenin <nthouvenin@gmail.com>
2018-12-12 12:10:04 +0100

    added new  tests

--- src/common/transformers/GET.spec.js
+++ src/common/transformers/GET.spec.js
@@ -9,7 +9,7 @@ describe('GET', () => {
         expect(get({ a: { aa: '1' }, b: 2 }, 'a.aa')).toEqual('1');
     });
 
-    it("should return all key's value from object", () => {
+    it("should return all key's values from object", () => {
         expect(get({ a: { aa: 1, bb: 1 }, b: 2 }, 'a.aa;a.bb')).toEqual([1, 1]);
     });
 
@@ -17,8 +17,23 @@ describe('GET', () => {
         expect(get({ a: { aa: 1, bb: 1 }, b: 2 }, '')).toEqual('');
     });
 
+    it("should return all mixed key's values (ordered)", () => {
+        expect(get({ a: { aa: 1, bb: [2, 3] }, b: 4 }, 'a.aa;a.bb')).toEqual([
+            1,
+            2,
+            3,
+        ]);
+    });
+
+    it("should return all mixed key's values (unordered)", () => {
+        expect(get({ a: { aa: 1, bb: [2, 3] }, b: 4 }, 'a.bb;a.aa')).toEqual([
+            2,
+            3,
+            1,
+        ]);
+    });
+
     it('should return nothing with empty paths', () => {
         expect(get({ a: { aa: 1, bb: 1 }, b: 2 }, ';')).toEqual(['', '']);
     });
-
 });
