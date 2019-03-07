5cb1fc78f6a92f434c490621435489d4f0ae3ea3
Nicolas Thouvenin <nthouvenin@gmail.com>
2019-03-07 16:23:55 +0100

    avoid maxSize=null

--- src/api/controller/api/run.js
+++ src/api/controller/api/run.js
@@ -63,7 +63,7 @@ export const runRoutine = async (ctx, routineCalled, field1, field2) => {
     }`;
     // context is the intput for LodexReduceQuery & LodexRunQuery & LodexDocuments
     const context = {
-        limit: maxSize,
+        limit: Number(maxSize) || 200,
         skip,
         sort: {
             [order]: dir === 'asc' ? 1 : -1,
