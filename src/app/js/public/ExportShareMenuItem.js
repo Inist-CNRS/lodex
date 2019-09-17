8aeabd77553196f94aaf1ece84009deb174438e5
Luwangel <adrien.amoros@gmail.com>
2019-09-17 18:06:51 +0200

    Fix warnings in react-router-dom

--- src/app/js/public/ExportShareMenuItem.js
+++ src/app/js/public/ExportShareMenuItem.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import translate from 'redux-polyglot/translate';
 import compose from 'recompose/compose';
 import { connect } from 'react-redux';
-import withRouter from 'react-router/withRouter';
+import { withRouter } from 'react-router';
 
 import { polyglot as polyglotPropTypes } from '../propTypes';
 import ButtonWithDialog from '../lib/components/ButtonWithDialog';
