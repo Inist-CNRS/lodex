c5429aa32d601035ba7b8bcf20f914ead1d437c8
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2019-09-23 16:17:33 +0200

    Due to menu, increase bottom padding page size

--- src/app/js/public/App.js
+++ src/app/js/public/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const styles = {
+    body: {
+        paddingBottom: 80,
+    },
+};
+
 export const AppComponent = ({ children }) => (
-    <div className="body">{children}</div>
+    <div style={styles.body}>
+        <div className="body">{children}</div>
+    </div>
 );
 
 AppComponent.propTypes = {
