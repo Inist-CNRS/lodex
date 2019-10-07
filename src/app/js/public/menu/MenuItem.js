e514c8318d0a817fb776936fad0cd77086d55600
Luwangel <adrien.amoros@gmail.com>
2019-10-07 14:59:54 +0200

    Display an alert in the console when using the old role resources

--- src/app/js/public/menu/MenuItem.js
+++ src/app/js/public/menu/MenuItem.js
@@ -222,7 +222,6 @@ const MenuItem = ({
                 />
             );
         }
-
         case 'custom': {
             const { link } = config;
             if (!link) {
@@ -260,11 +259,21 @@ const MenuItem = ({
                 </NavLink>
             );
         }
+        case 'resources': {
+            console.error(
+                `The role ${
+                    config.role
+                } has been removed in lodex v11.0.2 (https://github.com/Inist-CNRS/lodex/releases/tag/v11.0.1). Menu item: ${JSON.stringify(
+                    config,
+                )} will be ignored.`,
+            );
+            return null;
+        }
         default:
             console.error(
-                `Unknow role: ${config.role} menu item: ${JSON.stringify(
+                `Unknow role: ${config.role}. Menu item: ${JSON.stringify(
                     config,
-                )} will be ignored`,
+                )} will be ignored.`,
             );
             return null;
     }
