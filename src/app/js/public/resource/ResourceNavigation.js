158f05c693c00c99f0e7f22d987aa5d37372de7a
Luwangel <adrien.amoros@gmail.com>
2019-10-09 16:42:58 +0200

    Remove swiper in Resource Navigation

--- src/app/js/public/resource/ResourceNavigation.js
+++ src/app/js/public/resource/ResourceNavigation.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import compose from 'recompose/compose';
@@ -6,7 +6,6 @@ import { withRouter } from 'react-router-dom';
 
 import stylesToClassname from '../../lib/stylesToClassName';
 import NavButton, { NEXT, PREV } from '../../lib/components/NavButton';
-import useSwiper, { SWIPE_LEFT, SWIPE_RIGHT } from '../../lib/hooks/useSwiper';
 
 const styles = stylesToClassname(
     {
@@ -43,16 +42,6 @@ const ResourceNavigation = ({ history, prevResource, nextResource }) => {
     const nextLocation = buildLocationFromResource(nextResource);
 
     const navigate = location => history.push(location);
-
-    const direction = useSwiper();
-
-    useEffect(() => {
-        if (direction === SWIPE_LEFT && prevLocation) {
-            navigate(prevLocation);
-        } else if (direction === SWIPE_RIGHT && nextLocation) {
-            navigate(nextLocation);
-        }
-    }, [direction]);
 
     return (
         <Fragment>
