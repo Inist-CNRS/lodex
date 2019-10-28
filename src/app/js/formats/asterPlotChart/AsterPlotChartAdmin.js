21dc859edd33bb6fb7327f31c76a2aafd290cdf9
Luwangel <adrien.amoros@gmail.com>
2019-10-28 17:44:34 +0100

    Create AsterPlotChartAdmin view

---
+++ src/app/js/formats/asterPlotChart/AsterPlotChartAdmin.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import translate from 'redux-polyglot/translate';
+
+import { polyglot as polyglotPropTypes } from '../../propTypes';
+import updateAdminArgs from '../shared/updateAdminArgs';
+import RoutineParamsAdmin from '../shared/RoutineParamsAdmin';
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        width: '200%',
+        justifyContent: 'space-between',
+    },
+};
+
+export const defaultArgs = {
+    params: {
+        maxSize: 20,
+        maxValue: 100,
+        minValue: 50,
+        orderBy: 'value/asc',
+    },
+};
+
+const AsterPlotChartAdmin = ({
+    p: polyglot,
+    args,
+    showMaxSize,
+    showMaxValue,
+    showMinValue,
+    showOrderBy,
+}) => {
+    const setParams = params => {
+        updateAdminArgs('params', params, { args });
+    };
+
+    return (
+        <div style={styles.container}>
+            <RoutineParamsAdmin
+                params={args.params}
+                onChange={setParams}
+                polyglot={polyglot}
+                showMaxSize={showMaxSize}
+                showMaxValue={showMaxValue}
+                showMinValue={showMinValue}
+                showOrderBy={showOrderBy}
+            />
+        </div>
+    );
+};
+
+AsterPlotChartAdmin.propTypes = {
+    args: PropTypes.shape({
+        params: PropTypes.shape({
+            maxSize: PropTypes.number,
+            maxValue: PropTypes.number,
+            minValue: PropTypes.number,
+            orderBy: PropTypes.string,
+        }),
+    }),
+    onChange: PropTypes.func.isRequired,
+    p: polyglotPropTypes.isRequired,
+    showMaxSize: PropTypes.bool,
+    showMaxValue: PropTypes.bool,
+    showMinValue: PropTypes.bool,
+    showOrderBy: PropTypes.bool,
+};
+
+AsterPlotChartAdmin.defaultProps = {
+    args: defaultArgs,
+    showMaxSize: true,
+    showMaxValue: true,
+    showMinValue: true,
+    showOrderBy: true,
+};
+
+export default translate(AsterPlotChartAdmin);
