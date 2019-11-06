55c951c7e0664ce6bf9013c5ba8fd58455b84565
Luwangel <adrien.amoros@gmail.com>
2019-11-06 11:07:23 +0100

    Apply review

--- src/app/js/public/dataset/AppliedDatasetFacet.js
+++ src/app/js/public/dataset/AppliedDatasetFacet.js
@@ -1,25 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import compose from 'recompose/compose';
 import withHandlers from 'recompose/withHandlers';
 
-import { field as fieldPropTypes } from '../../propTypes';
 import AppliedFacet from '../facet/AppliedFacet';
 import { fromFields } from '../../sharedSelectors';
 import { fromDataset } from '../selectors';
 import { facetActions } from '.';
-
-export const AppliedDatasetFacetComponent = props => (
-    <AppliedFacet {...props} />
-);
-
-AppliedDatasetFacetComponent.propTypes = {
-    value: PropTypes.arrayOf(PropTypes.string).isRequired,
-    field: fieldPropTypes.isRequired,
-    onRequestDelete: PropTypes.func.isRequired,
-    inverted: PropTypes.bool.isRequired,
-};
 
 const mapStateToProps = (state, { name }) => ({
     field: fromFields.getFieldByName(state, name),
@@ -36,4 +22,4 @@ export default compose(
     withHandlers({
         onRequestDelete: ({ name, onClearFacet }) => () => onClearFacet(name),
     }),
-)(AppliedDatasetFacetComponent);
+)(AppliedFacet);
--- src/app/js/public/search/AppliedSearchFacet.js
+++ src/app/js/public/search/AppliedSearchFacet.js
@@ -1,23 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import compose from 'recompose/compose';
 import withHandlers from 'recompose/withHandlers';
 
-import { field as fieldPropTypes } from '../../propTypes';
 import AppliedFacet from '../facet/AppliedFacet';
 import { fromFields } from '../../sharedSelectors';
 import { fromSearch } from '../selectors';
 import { facetActions } from './reducer';
-
-export const AppliedSearchFacetComponent = props => <AppliedFacet {...props} />;
-
-AppliedSearchFacetComponent.propTypes = {
-    value: PropTypes.arrayOf(PropTypes.string).isRequired,
-    field: fieldPropTypes.isRequired,
-    onRequestDelete: PropTypes.func.isRequired,
-    inverted: PropTypes.bool.isRequired,
-};
 
 const mapStateToProps = (state, { name }) => ({
     field: fromFields.getFieldByName(state, name),
@@ -34,4 +22,4 @@ export default compose(
     withHandlers({
         onRequestDelete: ({ name, onClearFacet }) => () => onClearFacet(name),
     }),
-)(AppliedSearchFacetComponent);
+)(AppliedFacet);
