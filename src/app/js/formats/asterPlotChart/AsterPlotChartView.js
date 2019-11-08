c1e74d8ec8f93eab187a868accea23111725c35b
Luwangel <adrien.amoros@gmail.com>
2019-11-08 14:16:46 +0100

    Navigate between resources in the aster plot chart

--- src/app/js/formats/asterPlotChart/AsterPlotChartView.js
+++ src/app/js/formats/asterPlotChart/AsterPlotChartView.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import compose from 'recompose/compose';
 import get from 'lodash.get';
-import { getShortText } from '../../lib/longTexts';
 
-import injectData from '../injectData';
+import { getShortText } from '../../lib/longTexts';
 import stylesToClassname from '../../lib/stylesToClassName';
+import injectData from '../injectData';
 import AsterPlot from './AsterPlot';
 
 const sortByKey = (key = '') => (dataA, dataB) => {
@@ -24,17 +25,24 @@ function getRandomValue(decimals = 2) {
     return parseFloat(Math.random() * 100).toFixed(decimals);
 }
 
-const prepareData = (data = []) =>
+const prepareData = (data = [], history) =>
     data
         .map(d => {
             const title = getShortText(d['target-title']);
 
             const value = getRandomValue(2);
             const label = `<div>${title} <br/><br/> ${value}% similarity</div>`;
+            const onClick = () => {
+                history.push({
+                    pathname: `/${d.target}`,
+                    state: {},
+                });
+            };
 
             return {
                 label,
                 value,
+                onClick,
             };
         })
         .sort(sortByKey('index'));
@@ -58,13 +66,17 @@ const AsterPlotChartView = ({ data }) => {
 
 AsterPlotChartView.propTypes = {
     data: PropTypes.array.isRequired,
+    history: PropTypes.shape({
+        push: PropTypes.func.isRequired,
+    }),
 };
 
-const mapStateToProps = (_, { formatData }) => ({
-    data: prepareData(formatData),
+const mapStateToProps = (_, { formatData, history }) => ({
+    data: prepareData(formatData, history),
 });
 
 export default compose(
+    withRouter,
     injectData(),
     connect(mapStateToProps),
 )(AsterPlotChartView);
