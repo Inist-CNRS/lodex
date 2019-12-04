7f23f02f1c970efc39ba4e5c10cf9622dcdb1d2d
Luwangel <adrien.amoros@gmail.com>
2019-12-04 10:14:17 +0100

    Use useInView in DatasetCharacteristicItem

--- src/app/js/characteristic/DatasetCharacteristicItem.js
+++ src/app/js/characteristic/DatasetCharacteristicItem.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { useInView } from 'react-intersection-observer';
+import moment from 'moment';
 
 import Property from '../public/Property';
 import { fromFields, fromCharacteristic } from '../sharedSelectors';
@@ -10,7 +12,53 @@ export const DatasetCharacteristicItemComponent = ({
     resource,
     field,
     style,
-}) => <Property resource={resource} field={field} style={style} />;
+}) => {
+    const [logged, setLogged] = useState(false);
+
+    useEffect(() => {
+        if (!logged) {
+            console.log(`There are ${Object.keys(resource).length}  fields`);
+            setLogged(true);
+        }
+    }, [resource]);
+
+    const [ref, inView] = useInView({
+        triggerOnce: true,
+        rootMargin: '0px',
+    });
+
+    useEffect(() => {
+        if (inView) {
+            console.log(
+                `${field.name} displayed at ${moment(new Date()).format(
+                    'hh:mm:ss',
+                )}`,
+            );
+        }
+    }, [inView]);
+
+    return (
+        <span
+            ref={ref}
+            style={
+                !inView
+                    ? {
+                          display: 'block',
+                          minHeight: '300px',
+                          width: '100%',
+                      }
+                    : {
+                          height: '100%',
+                          width: '100%',
+                      }
+            }
+        >
+            {inView && (
+                <Property resource={resource} field={field} style={style} />
+            )}
+        </span>
+    );
+};
 
 DatasetCharacteristicItemComponent.propTypes = {
     resource: PropTypes.shape({
