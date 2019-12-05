f4057cab50d84336808f46e516df5373db158ad1
Luwangel <adrien.amoros@gmail.com>
2019-12-05 14:07:16 +0100

    Clean the integration of the useInView hook

--- src/app/js/characteristic/DatasetCharacteristicItem.js
+++ src/app/js/characteristic/DatasetCharacteristicItem.js
@@ -1,62 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { useInView } from 'react-intersection-observer';
-import moment from 'moment';
 
 import Property from '../public/Property';
 import { fromFields, fromCharacteristic } from '../sharedSelectors';
 import { field as fieldPropTypes } from '../propTypes';
+import stylesToClassname from '../lib/stylesToClassName';
+
+const styles = stylesToClassname(
+    {
+        loading: {
+            height: '500px !important',
+            width: '100% !important',
+        },
+        loaded: {
+            height: '0px !important',
+            width: '0px !important',
+        },
+    },
+    'dataset-characteristic-item',
+);
 
 export const DatasetCharacteristicItemComponent = ({
     resource,
     field,
     style,
 }) => {
-    const [logged, setLogged] = useState(false);
-
-    useEffect(() => {
-        if (!logged) {
-            console.log(`There are ${Object.keys(resource).length}  fields`);
-            setLogged(true);
-        }
-    }, [resource]);
-
     const [ref, inView] = useInView({
         triggerOnce: true,
-        rootMargin: '0px',
+        rootMargin: '50px 0px',
     });
 
-    useEffect(() => {
-        if (inView) {
-            console.log(
-                `${field.name} displayed at ${moment(new Date()).format(
-                    'hh:mm:ss',
-                )}`,
-            );
-        }
-    }, [inView]);
-
     return (
-        <span
-            ref={ref}
-            style={
-                !inView
-                    ? {
-                          display: 'block',
-                          minHeight: '300px',
-                          width: '100%',
-                      }
-                    : {
-                          height: '100%',
-                          width: '100%',
-                      }
-            }
-        >
+        <Fragment>
+            <div
+                ref={ref}
+                className={inView ? styles.loaded : styles.loading}
+            />
             {inView && (
                 <Property resource={resource} field={field} style={style} />
             )}
-        </span>
+        </Fragment>
     );
 };
 
