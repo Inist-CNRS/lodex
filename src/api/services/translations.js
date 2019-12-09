674f0a86e7aa685d1560200681fb8ff3a905f00f
Luwangel <adrien.amoros@gmail.com>
2019-12-09 12:02:29 +0100

    Rewrite translations file

--- src/api/services/translations.js
+++ src/api/services/translations.js
@@ -2,29 +2,20 @@ const fs = require('fs');
 const { resolve } = require('path');
 const CSV = require('csv-string');
 
-let lastCacheTimestamp = null;
-let translations = {};
-
 const getFileUpdatedDate = path => {
     const stats = fs.statSync(path);
     return stats.mtime;
 };
 
-const shouldUpdateCache = (filePutInCacheDate, fileLastUpdatedDate) =>
-    fileLastUpdatedDate > filePutInCacheDate;
+const shouldUpdateTranslations = (fileCacheTime, fileLastUpdatedTime) =>
+    fileLastUpdatedTime > fileCacheTime;
 
-const getLanguageTranslations = (language, index) => {
-    const path = resolve(__dirname, './custom/translations.tsv');
-    if (!fs.existsSync(path)) {
-        console.error('The translation file is missing.');
-        return {};
-    }
+const parseTranslationsFile = path => {
+    const tsv = fs.readFileSync(path, 'utf8');
+    const csv = CSV.parse(tsv, `\t`, '"');
 
-    const lastUpdateTimestamp = getFileUpdatedDate(path);
-
-    if (shouldUpdateCache(lastCacheTimestamp, lastUpdateTimestamp)) {
-        const tsv = fs.readFileSync(path, 'utf8');
-        const csv = CSV.parse(tsv, `\t`, '"');
+    const translations = {};
+    ['english', 'french'].forEach((language, index) => {
         translations[language] = csv.reduce(
             (acc, line) => ({
                 ...acc,
@@ -32,22 +23,42 @@ const getLanguageTranslations = (language, index) => {
             }),
             {},
         );
-        lastCacheTimestamp = new Date().getTime();
-    }
-    return translations[language];
+    });
+    return translations;
 };
 
-const getTranslations = locale => {
+let lastCacheTimestamp = null;
+let cachedTranslations = {};
+
+const getTranslations = (filePath = './custom/translations.tsv') => {
+    const path = resolve(__dirname, filePath);
+    if (!fs.existsSync(path)) {
+        console.error('The translation file is missing.');
+        return {};
+    }
+
+    const lastUpdateTimestamp = getFileUpdatedDate(path);
+
+    if (shouldUpdateTranslations(lastCacheTimestamp, lastUpdateTimestamp)) {
+        cachedTranslations = parseTranslationsFile(path);
+        lastCacheTimestamp = new Date().getTime();
+    }
+    return cachedTranslations;
+};
+
+const getByLanguage = (locale = 'en') => {
+    const translations = getTranslations();
+
     if (typeof locale != 'string') {
         locale = 'en';
     }
+
     const language = locale.toLowerCase();
-    if (language.startsWith('fr')) {
-        return getLanguageTranslations('french', 2);
-    }
-    return getLanguageTranslations('english', 1);
+    return language.startsWith('fr')
+        ? translations.french
+        : translations.english;
 };
 
 module.exports = {
-    translate: locale => getTranslations(locale),
+    getByLanguage,
 };
