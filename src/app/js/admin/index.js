142b3d01ad20f2b044583dbb123ffe9eda757a74
Luwangel <adrien.amoros@gmail.com>
2019-12-09 16:05:37 +0100

    Initialize admin state the same way as the app

--- src/app/js/admin/index.js
+++ src/app/js/admin/index.js
@@ -3,18 +3,19 @@ import 'url-api-polyfill';
 
 import React from 'react';
 import { render } from 'react-dom';
+import { createHashHistory } from 'history';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import rootReducer from './reducers';
 import Routes from './Routes';
 import sagas from './sagas';
 import configureStore from '../configureStore';
-import { createHashHistory } from 'history';
-import { Provider } from 'react-redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import scrollToTop from '../lib/scrollToTop';
-import { ConnectedRouter } from 'connected-react-router';
-
+import phrasesFor from '../i18n/translations';
+import getLocale from '../../../common/getLocale';
 import theme from '../theme';
 
 const muiTheme = getMuiTheme({
@@ -26,8 +27,21 @@ const muiTheme = getMuiTheme({
     },
 });
 
+const locale = getLocale();
+const initialState = {
+    polyglot: {
+        locale,
+        phrases: phrasesFor(locale),
+    },
+};
+
 const history = createHashHistory();
-const store = configureStore(rootReducer, sagas, {}, history);
+const store = configureStore(
+    rootReducer,
+    sagas,
+    window.__PRELOADED_STATE__ || initialState,
+    history,
+);
 
 render(
     <Provider {...{ store }}>
