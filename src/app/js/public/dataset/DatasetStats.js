50e97104717d878fda85ce45fac59024a9c2af86
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2019-12-10 14:28:17 +0100

    Ease code reading

--- src/app/js/public/dataset/DatasetStats.js
+++ src/app/js/public/dataset/DatasetStats.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Stats from '../Stats';
 
 import { fromDataset } from '../selectors';
 
-export const StatsComponent = ({ nbResources, currentNbResources }) => (
-    <Stats currentNbResources={currentNbResources} nbResources={nbResources} />
-);
-
-StatsComponent.propTypes = {
-    nbResources: PropTypes.number.isRequired,
-    currentNbResources: PropTypes.number.isRequired,
-};
-
 const mapStateToProps = state => ({
     currentNbResources: fromDataset.getDatasetTotal(state),
     nbResources: fromDataset.getDatasetFullTotal(state),
 });
 
-export default connect(mapStateToProps)(StatsComponent);
+export default connect(mapStateToProps)(Stats);
--- src/app/js/public/search/SearchStats.js
+++ src/app/js/public/search/SearchStats.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Stats from '../Stats';
 
 import { fromSearch } from '../selectors';
 
-export const StatsComponent = ({ nbResources, currentNbResources }) => (
-    <Stats currentNbResources={currentNbResources} nbResources={nbResources} />
-);
-
-StatsComponent.propTypes = {
-    nbResources: PropTypes.number.isRequired,
-    currentNbResources: PropTypes.number.isRequired,
-};
-
 const mapStateToProps = state => ({
     currentNbResources: fromSearch.getDatasetTotal(state),
     nbResources: fromSearch.getDatasetFullTotal(state),
 });
 
-export default connect(mapStateToProps)(StatsComponent);
+export default connect(mapStateToProps)(Stats);
