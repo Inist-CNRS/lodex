9342e3b0ef143c7c5fa64f2b1a6b958747fe4ce4
Luwangel <adrien.amoros@gmail.com>
2019-12-11 09:41:58 +0100

    Allow null in the VALUE transformer

--- src/common/transformers/VALUE.js
+++ src/common/transformers/VALUE.js
@@ -1,8 +1,10 @@
+import isUndefinedOrEmpty from '../lib/isUndefinedOrEmpty';
+
 const transformation = (_, args) => () =>
     new Promise((resolve, reject) => {
         const valueArg = args.find(a => a.name === 'value');
 
-        if (!valueArg || valueArg.value == undefined) {
+        if (!valueArg || isUndefinedOrEmpty(valueArg.value)) {
             reject(new Error('Invalid Argument for VALUE transformation'));
             return;
         }
