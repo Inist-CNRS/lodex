ec3e80d39cdad4d9b54028990379ae31af5697b7
Luwangel <adrien.amoros@gmail.com>
2019-12-16 15:47:26 +0100

    Refact colorUtils

--- src/app/js/formats/colorUtils.js
+++ src/app/js/formats/colorUtils.js
@@ -6,12 +6,10 @@ export const MULTICHROMATIC_DEFAULT_COLORSET =
 export const MULTICHROMATIC_DEFAULT_COLORSET_STREAMGRAPH =
     '#e6194b #3cb44b #ffe119 #4363d8 #f58231 #911eb4 #46f0f0 #f032e6 #bcf60c #fabebe #008080 #e6beff #9a6324 #fffac8 #800000 #aaffc3 #808000 #ffd8b1 #000075 #808080 #ffffff #000000';
 
-export const isValidColor = colorInput => {
-    return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(colorInput) ? true : false;
-};
+export const isValidColor = colorInput =>
+    /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(colorInput);
 
 export const getColor = (colorSet, index) => {
-    return isValidColor(colorSet[index % colorSet.length])
-        ? colorSet[index % colorSet.length]
-        : 'black';
+    const color = colorSet[index % colorSet.length];
+    return isValidColor(color) ? color : 'black';
 };
