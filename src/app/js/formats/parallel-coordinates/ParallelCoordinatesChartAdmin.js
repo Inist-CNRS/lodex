d1c4ebf7235b7e15e642d7cf6ddf7439b8a3ab99
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2020-01-02 14:46:04 +0100

    Configure chart

---
+++ src/app/js/formats/parallel-coordinates/ParallelCoordinatesChartAdmin.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import translate from 'redux-polyglot/translate';
+
+import { polyglot as polyglotPropTypes } from '../../propTypes';
+import updateAdminArgs from '../shared/updateAdminArgs';
+import RoutineParamsAdmin from '../shared/RoutineParamsAdmin';
+import ColorPickerParamsAdmin from '../shared/ColorPickerParamsAdmin';
+import { MULTICHROMATIC_DEFAULT_COLORSET } from '../colorUtils';
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        width: '200%',
+        justifyContent: 'space-between',
+    },
+};
+
+const ParallelCoordinatesChartAdmin = ({
+    p: polyglot,
+    args,
+    onChange,
+    showMaxSize,
+    showMaxValue,
+    showMinValue,
+}) => {
+    const setParams = params => {
+        updateAdminArgs('params', Object.assign({}, args.params, params), {
+            args,
+            onChange,
+        });
+    };
+
+    const setColors = colors => {
+        updateAdminArgs('colors', colors, {
+            args,
+            onChange,
+        });
+    };
+
+    return (
+        <div style={styles.container}>
+            <RoutineParamsAdmin
+                params={args.params}
+                onChange={setParams}
+                polyglot={polyglot}
+                showMaxSize={showMaxSize}
+                showMaxValue={showMaxValue}
+                showMinValue={showMinValue}
+            />
+            <ColorPickerParamsAdmin
+                colors={args.colors}
+                onChange={setColors}
+                polyglot={polyglot}
+            />
+        </div>
+    );
+};
+
+ParallelCoordinatesChartAdmin.propTypes = {
+    args: PropTypes.shape({
+        params: PropTypes.shape({
+            maxSize: PropTypes.number,
+            maxValue: PropTypes.number,
+            minValue: PropTypes.number,
+        }),
+        colors: PropTypes.string,
+    }),
+    onChange: PropTypes.func.isRequired,
+    p: polyglotPropTypes.isRequired,
+    showMaxSize: PropTypes.bool,
+    showMaxValue: PropTypes.bool,
+    showMinValue: PropTypes.bool,
+};
+
+export const defaultArgs = {
+    params: {
+        maxSize: 20,
+        maxValue: 100,
+        minValue: 0,
+    },
+    colors: MULTICHROMATIC_DEFAULT_COLORSET,
+};
+
+ParallelCoordinatesChartAdmin.defaultProps = {
+    args: defaultArgs,
+    showMaxSize: true,
+    showMaxValue: true,
+    showMinValue: true,
+};
+
+export default translate(ParallelCoordinatesChartAdmin);
