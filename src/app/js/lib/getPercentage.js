e5ebb558392cc31652dd83336174b088c6481480
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2020-01-07 10:30:51 +0100

    Fix tests

--- src/app/js/lib/getPercentage.js
+++ src/app/js/lib/getPercentage.js
@@ -1,5 +1,3 @@
-import get from 'lodash.get';
-
 export function getPercentValue(value, decimals = 0) {
     const parsedValue = parseFloat(value);
     if (isNaN(parsedValue) || parsedValue < 0 || parsedValue > 1) {
