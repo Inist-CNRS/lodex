ca8df3e426977b075358ab673ece44d4bcbd46af
Nicolas Thouvenin <nthouvenin@gmail.com>
2020-02-27 15:20:24 +0100

    use latest version of @ezs/transformers

--- src/api/services/getDocumentTransformer.js
+++ src/api/services/getDocumentTransformer.js
@@ -1,12 +1,10 @@
 import getDocumentTransformer from '../../common/getDocumentTransformer';
-import applyJbjStylesheet from './applyJbjStylesheet';
 
 export default (fetchLineBy, fields) =>
     getDocumentTransformer(
         {
             env: 'node',
             fetchLineBy,
-            applyJbjStylesheet,
         },
         fields,
     );
