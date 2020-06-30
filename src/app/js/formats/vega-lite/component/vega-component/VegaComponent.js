cdd52eafdb8bd003d2786f5a2f29beda053e9e51
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2020-06-30 16:00:02 +0200

    add actions if user is an admin

--- src/app/js/formats/vega-lite/component/vega-component/VegaComponent.js
+++ src/app/js/formats/vega-lite/component/vega-component/VegaComponent.js
@@ -1,15 +1,43 @@
 import { Vega } from 'react-vega';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { isAdmin } from '../../../../user';
 
-export default function CustomActionVega(props) {
-    let actions = {
-        export: {
-            svg: true,
-            png: true,
-        },
-        source: false,
-        compiled: false,
-        editor: false,
-    };
+function CustomActionVega(props) {
+    let actions;
+    if (isAdmin(props.user)) {
+        actions = {
+            export: {
+                svg: true,
+                png: true,
+            },
+            source: true,
+            compiled: true,
+            editor: true,
+        };
+    } else {
+        actions = {
+            export: {
+                svg: true,
+                png: true,
+            },
+            source: false,
+            compiled: false,
+            editor: false,
+        };
+    }
     return <Vega {...props} actions={actions} mode="vega" />;
 }
+
+CustomActionVega.propTypes = {
+    user: PropTypes.any,
+};
+
+const mapStateToProps = state => {
+    return {
+        user: state.user,
+    };
+};
+
+export default connect(mapStateToProps)(CustomActionVega);
--- src/app/js/formats/vega-lite/component/vega-component/VegaLiteComponent.js
+++ src/app/js/formats/vega-lite/component/vega-component/VegaLiteComponent.js
@@ -1,15 +1,43 @@
 import { Vega } from 'react-vega';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { isAdmin } from '../../../../user';
 
-export default function CustomActionVegaLite(props) {
-    let actions = {
-        export: {
-            svg: true,
-            png: true,
-        },
-        source: false,
-        compiled: false,
-        editor: false,
-    };
+function CustomActionVegaLite(props) {
+    let actions;
+    if (isAdmin(props.user)) {
+        actions = {
+            export: {
+                svg: true,
+                png: true,
+            },
+            source: true,
+            compiled: true,
+            editor: true,
+        };
+    } else {
+        actions = {
+            export: {
+                svg: true,
+                png: true,
+            },
+            source: false,
+            compiled: false,
+            editor: false,
+        };
+    }
     return <Vega {...props} actions={actions} mode="vega-lite" />;
 }
+
+CustomActionVegaLite.propTypes = {
+    user: PropTypes.any,
+};
+
+const mapStateToProps = state => {
+    return {
+        user: state.user,
+    };
+};
+
+export default connect(mapStateToProps)(CustomActionVegaLite);
