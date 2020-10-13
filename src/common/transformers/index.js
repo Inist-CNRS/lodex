e1afc84c08159f7859c685620ee64cd3d521b1ea
jdemangeon <julien@marmelab.com>
2020-10-13 11:59:05 +0200

    #25: Correctif Import Ancien Modele

--- src/common/transformers/index.js
+++ src/common/transformers/index.js
@@ -6,12 +6,14 @@ transformersMetas.push(AUTOGENERATE_URI.getMetas());
 
 export default transformers;
 
+export const hasRegistredTransformer = operation =>
+    typeof transformers[operation] !== 'undefined';
+
 export const getTransformersMetas = type =>
     type ? transformersMetas.filter(m => m.type === type) : transformersMetas;
 
 export const getTransformerMetas = operation => {
     const transformer = transformers[operation];
-
     if (!transformer) return [];
 
     return transformer.getMetas();
