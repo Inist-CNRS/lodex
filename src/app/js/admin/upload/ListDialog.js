717ba036b422dbfdbd4fe3f7caebc1bd9652561e
JulienM <julienm@marmelab.com>
2020-11-24 10:51:40 +0100

    Add filter

--- src/app/js/admin/upload/ListDialog.js
+++ src/app/js/admin/upload/ListDialog.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import translate from 'redux-polyglot/translate';
 import classnames from 'classnames';
 import compose from 'recompose/compose';
 import {
     Button,
-    TextField,
+    Grid,
+    Box,
     List,
     ListItem,
     ListItemText,
     Dialog,
+    DialogTitle,
     DialogContent,
     DialogActions,
 } from '@material-ui/core';
+import FilterIcon from '@material-ui/icons/FilterList';
 import { makeStyles } from '@material-ui/core/styles';
 
 import theme from '../../theme';
@@ -24,6 +27,7 @@ const useStyles = makeStyles({
         '&:hover': {
             backgroundColor: theme.black.veryLight,
         },
+        borderBottom: `1px solid ${theme.black.light}`,
     },
     selectedItem: {
         backgroundColor: theme.green.secondary,
@@ -32,7 +36,8 @@ const useStyles = makeStyles({
         },
     },
     list: {
-        width: 1000,
+        width: 800,
+        height: '70vh',
     },
 });
 
@@ -46,6 +51,11 @@ export const ListDialogComponent = ({
     actions,
 }) => {
     const classes = useStyles();
+    const [filteredLoaders, setFilter] = useState(loaders.map(l => l.name));
+
+    useEffect(() => {
+        setFilter(loaders.map(l => l.name));
+    }, [loaders]);
 
     const changeValue = newValue => {
         setLoader(newValue);
@@ -54,6 +64,7 @@ export const ListDialogComponent = ({
 
     const loaderNames = loaders
         .map(loader => loader.name)
+        .filter(loader => filteredLoaders.includes(loader))
         .sort((x, y) => polyglot.t(x).localeCompare(polyglot.t(y)))
         .map(pn => (
             <ListItemComponent
@@ -68,6 +79,13 @@ export const ListDialogComponent = ({
 
     return (
         <Dialog open={open} onClose={handleClose} scroll="body" maxWidth="xl">
+            <DialogTitle>
+                <FilterComponent
+                    loaders={loaders}
+                    filter={filteredLoaders}
+                    setFilter={setFilter}
+                />
+            </DialogTitle>
             <DialogContent>
                 <List className={classnames(classes.list)}>
                     <ListItemComponent
@@ -101,7 +119,6 @@ const ListItemComponent = ({
     changeValue,
 }) => {
     const classes = useStyles();
-    console.log(value, selected);
     return (
         <ListItem
             value={value}
@@ -126,6 +143,65 @@ ListItemComponent.propTypes = {
     comment: PropTypes.string,
     title: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
+};
+
+const FilterComponent = ({ loaders, filter, setFilter }) => {
+    const allFilters = {
+        all: loaders.map(l => l.name),
+        csv: loaders.filter(l => l.name.startsWith('csv')).map(l => l.name),
+        tsv: loaders.filter(l => l.name.startsWith('tsv')).map(l => l.name),
+        XML: loaders
+            .filter(l =>
+                ['xml', 'rss', 'atom', 'mods', 'tei', 'skos'].includes(l.name),
+            )
+            .map(l => l.name),
+        JSON: loaders.filter(l => l.name.startsWith('json')).map(l => l.name),
+    };
+    allFilters.Other = allFilters.all.filter(
+        l =>
+            !allFilters.csv.includes(l) &&
+            !allFilters.tsv.includes(l) &&
+            !allFilters.XML.includes(l) &&
+            !allFilters.JSON.includes(l),
+    );
+
+    return (
+        <Grid
+            container={true}
+            direction="row"
+            style={{ width: '100%', marginBottom: 25 }}
+            justify="space-around"
+        >
+            <Box>
+                <FilterIcon fontSize="large" style={{ marginRight: 10 }} />
+            </Box>
+            {Object.keys(allFilters).map(key => (
+                <Box key={key}>
+                    <Button
+                        variant={
+                            allFilters[key].toString() === filter.toString()
+                                ? 'contained'
+                                : 'outlined'
+                        }
+                        color="primary"
+                        onClick={() => setFilter(allFilters[key])}
+                    >
+                        {key}
+                    </Button>
+                </Box>
+            ))}
+        </Grid>
+    );
+};
+
+FilterComponent.propTypes = {
+    filter: PropTypes.array,
+    setFilter: PropTypes.func.isRequired,
+    loaders: PropTypes.array.isRequired,
+};
+
+FilterComponent.defaultProps = {
+    loaders: [],
 };
 
 export default compose(translate)(ListDialogComponent);
