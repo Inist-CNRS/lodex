72de26ee1cc2eafbbed9aac59ddbba45f238a97b
jdemangeon <julien@marmelab.com>
2020-12-17 11:44:04 +0100

    Fix Updating Subresource In Redux

--- src/app/js/admin/publication/index.js
+++ src/app/js/admin/publication/index.js
@@ -3,11 +3,13 @@ import { createAction, handleActions } from 'redux-actions';
 export const LOAD_PUBLICATION = 'LOAD_PUBLICATION';
 export const LOAD_PUBLICATION_SUCCESS = 'LOAD_PUBLICATION_SUCCESS';
 export const LOAD_PUBLICATION_ERROR = 'LOAD_PUBLICATION_ERROR';
+export const COMPUTE_PUBLICATION = 'COMPUTE_PUBLICATION';
 export const SELECT_FIELD = 'SELECT_FIELD';
 
 export const loadPublication = createAction(LOAD_PUBLICATION);
 export const loadPublicationSuccess = createAction(LOAD_PUBLICATION_SUCCESS);
 export const loadPublicationError = createAction(LOAD_PUBLICATION_ERROR);
+export const computePublication = createAction(COMPUTE_PUBLICATION);
 
 export const selectField = createAction(SELECT_FIELD);
 
