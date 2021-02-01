b26c23e5da4ca27d869236dcbb0da769dbcf9301
JulienM <julienm@marmelab.com>
2021-02-01 10:13:56 +0100

    Best field control

--- src/app/js/formats/fieldClone/FieldCloneAdmin.js
+++ src/app/js/formats/fieldClone/FieldCloneAdmin.js
@@ -3,11 +3,18 @@ import PropTypes from 'prop-types';
 import compose from 'recompose/compose';
 import { connect } from 'react-redux';
 import translate from 'redux-polyglot/translate';
+import { useParams } from 'react-router';
 
 import { MenuItem, Select, FormControl, InputLabel } from '@material-ui/core';
 import { polyglot as polyglotPropTypes } from '../../propTypes';
 import { field as fieldPropTypes } from '../../propTypes';
 import { fromFields } from '../../sharedSelectors';
+import {
+    SCOPE_DATASET,
+    SCOPE_GRAPHIC,
+    SCOPE_DOCUMENT,
+    SCOPE_COLLECTION,
+} from '../../../../common/scope';
 
 const styles = {
     container: {
@@ -22,10 +29,13 @@ export const defaultArgs = {
 };
 
 const FieldCloneAdmin = ({ args, onChange, p: polyglot, fields }) => {
+    const { filter } = useParams();
     const setValue = e => {
         const newArgs = { value: e.target.value };
         onChange(newArgs);
     };
+
+    const filteredFields = fields.filter(f => isValidClonableField(f, filter));
 
     return (
         <div style={styles.container}>
@@ -39,7 +49,7 @@ const FieldCloneAdmin = ({ args, onChange, p: polyglot, fields }) => {
                     style={styles.input}
                     value={args.value}
                 >
-                    {fields.map(field => {
+                    {filteredFields.map(field => {
                         return (
                             <MenuItem value={field.name} key={field.name}>
                                 {field.name} - {field.label}
@@ -67,9 +77,30 @@ FieldCloneAdmin.defaultProps = {
 };
 
 const mapStateToProps = state => ({
-    fields: fromFields
-        .getFields(state)
-        .filter(f => !f.format || (f.format && f.format.name !== 'fieldClone')),
+    fields: fromFields.getFields(state),
 });
+
+export const isValidClonableField = (field, filter) => {
+    if (field.format && field.format.name === 'fieldClone') {
+        return false;
+    }
+
+    if (
+        filter === SCOPE_DATASET &&
+        field.scope !== SCOPE_DATASET &&
+        field.scope !== SCOPE_GRAPHIC
+    ) {
+        return false;
+    }
+
+    if (
+        filter === SCOPE_DOCUMENT &&
+        field.scope !== SCOPE_DOCUMENT &&
+        field.scope !== SCOPE_COLLECTION
+    ) {
+        return false;
+    }
+    return true;
+};
 
 export default compose(connect(mapStateToProps), translate)(FieldCloneAdmin);
