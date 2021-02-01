de1711823baab6c44fcf9f52233d36b49764b8e9
JulienM <julienm@marmelab.com>
2021-02-01 06:20:13 +0100

    Use list of fields

--- src/app/js/formats/fieldClone/FieldCloneAdmin.js
+++ src/app/js/formats/fieldClone/FieldCloneAdmin.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import compose from 'recompose/compose';
+import { connect } from 'react-redux';
 import translate from 'redux-polyglot/translate';
-import { TextField } from '@material-ui/core';
+
+import { MenuItem, Select, FormControl, InputLabel } from '@material-ui/core';
 import { polyglot as polyglotPropTypes } from '../../propTypes';
+import { field as fieldPropTypes } from '../../propTypes';
+import { fromFields } from '../../sharedSelectors';
 
 const styles = {
     container: {
@@ -16,7 +21,7 @@ export const defaultArgs = {
     value: '',
 };
 
-const FieldCloneAdmin = ({ args, onChange, p: polyglot }) => {
+const FieldCloneAdmin = ({ args, onChange, p: polyglot, fields }) => {
     const setValue = e => {
         const newArgs = { value: e.target.value };
         onChange(newArgs);
@@ -24,12 +29,25 @@ const FieldCloneAdmin = ({ args, onChange, p: polyglot }) => {
 
     return (
         <div style={styles.container}>
-            <TextField
-                label={polyglot.t('fieldclone_format_value')}
-                onChange={setValue}
-                style={styles.input}
-                value={args.value}
-            />
+            <FormControl fullWidth>
+                <InputLabel id="fieldclone-input-label">
+                    {polyglot.t('fieldclone_format_value')}
+                </InputLabel>
+                <Select
+                    labelId="fieldclone-input-label"
+                    onChange={setValue}
+                    style={styles.input}
+                    value={args.value}
+                >
+                    {fields.map(field => {
+                        return (
+                            <MenuItem value={field.name} key={field.name}>
+                                {field.name} - {field.label}
+                            </MenuItem>
+                        );
+                    })}
+                </Select>
+            </FormControl>
         </div>
     );
 };
@@ -40,10 +58,18 @@ FieldCloneAdmin.propTypes = {
     }),
     onChange: PropTypes.func.isRequired,
     p: polyglotPropTypes.isRequired,
+    fields: PropTypes.arrayOf(fieldPropTypes).isRequired,
 };
 
 FieldCloneAdmin.defaultProps = {
     args: defaultArgs,
+    fields: [],
 };
 
-export default translate(FieldCloneAdmin);
+const mapStateToProps = state => ({
+    fields: fromFields
+        .getFields(state)
+        .filter(f => f && f.format && f.format.name !== 'fieldClone'),
+});
+
+export default compose(connect(mapStateToProps), translate)(FieldCloneAdmin);
