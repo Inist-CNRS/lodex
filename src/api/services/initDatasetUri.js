1750f0bb7b2db6baa94c694be442a88cc9057ee5
JulienM <julienm@marmelab.com>
2021-02-03 15:52:31 +0100

    Add init URI in progress bar

--- src/api/services/initDatasetUri.js
+++ src/api/services/initDatasetUri.js
@@ -1,11 +1,13 @@
 import getDocumentTransformer from '../../common/getDocumentTransformer';
 import { URI_FIELD_NAME } from '../../common/uris';
+import progress from './progress';
+import { INITIALIZING_URI } from '../../common/progressStatus';
 
 const initDatasetUriFactory = ctx =>
     async function initDatasetUri() {
         const count = await ctx.dataset.countWithoutUri();
+        progress.start(INITIALIZING_URI, count);
         const fields = await ctx.field.findAll();
-
         const transformUri = getDocumentTransformer(
             ctx.dataset.findBy,
             fields.filter(f => f.name === URI_FIELD_NAME),
@@ -16,7 +18,6 @@ const initDatasetUriFactory = ctx =>
             const dataset = await ctx.dataset.findLimitFromSkip(1000, handled, {
                 uri: { $exists: false },
             });
-
             const transformedDataset = await Promise.all(
                 dataset.map(async item => {
                     const uriObject = await transformUri(item);
@@ -28,6 +29,7 @@ const initDatasetUriFactory = ctx =>
                 _id: item._id,
             }));
             handled += dataset.length;
+            progress.setProgress(handled);
         }
     };
 
