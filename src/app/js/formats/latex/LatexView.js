4001f7ef239aee8222fafeea6455aef1a7f574d4
Nicolas Thouvenin <nthouvenin@gmail.com>
2021-04-27 13:28:12 +0200

    render latex value with some default options

--- src/app/js/formats/latex/LatexView.js
+++ src/app/js/formats/latex/LatexView.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import katex from 'katex';
+import Helmet from 'react-helmet';
 import { field as fieldPropTypes } from '../../propTypes';
+import InvalidFormat from '../InvalidFormat';
 
 import stylesToClassname from '../../lib/stylesToClassName';
 
@@ -17,10 +19,39 @@ const styles = stylesToClassname(
 );
 
 const LatexView = ({ resource, field }) => {
-    const html = katex.renderToString(resource[field.name]);
+    const value = resource[field.name];
+
+    let html;
+    try {
+        html = katex.renderToString(resource[field.name], {
+            displayMode: false,
+            leqno: true,
+            fleqn: true,
+            throwOnError: false,
+            errorColor: '#cc0000',
+            strict: 'warn',
+            output: 'html',
+            trust: false,
+        });
+    } catch (e) {
+        return <InvalidFormat format={field.format} value={value} />;
+    }
+
     return (
         <div className={classnames('latex-container', styles.container)}>
-            {html}
+            <Helmet>
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/katex@0.13.3/dist/katex.min.css"
+                    integrity="sha384-ThssJ7YtjywV52Gj4JE/1SQEDoMEckXyhkFVwaf4nDSm5OBlXeedVYjuuUd0Yua+"
+                    crossOrigin="anonymous"
+                />
+            </Helmet>
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: html,
+                }}
+            />
         </div>
     );
 };
