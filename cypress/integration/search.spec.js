f2f199654adaba4eec5b274765838db692c6b5fa
habsaoui <mhabsaoui@gmail.com>
2021-05-18 12:30:52 +0200

    Fix e2e test

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -33,7 +33,7 @@ describe('Search', () => {
             cy.get('.export').click();
             cy.wait(300);
             cy.get('li[role="menuitem"]')
-                .should('have.length', 3)
+                .should('have.length', 4)
                 .contains('CSV')
                 .should('be.visible');
         });
