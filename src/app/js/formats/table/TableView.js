c5a2bad036afb590e9fd1739df27cd6c792f3989
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2021-06-04 15:47:06 +0200

    feat: First round of table pagination

--- src/app/js/formats/table/TableView.js
+++ src/app/js/formats/table/TableView.js
@@ -8,34 +8,37 @@ import {
     polyglot as polyglotPropTypes,
 } from '../../propTypes';
 import PropTypes from 'prop-types';
-import stylesToClassname from '../../lib/stylesToClassName';
 import { getResourceUri, isLocalURL } from '../../../../common/uris';
 import Link from '../../lib/components/Link';
-import { Table, TableBody, TableCell, TableRow } from '@material-ui/core';
-
-const styles = stylesToClassname(
-    {
-        elementExpectFirst: {
-            color: '#A1A1A4',
-        },
-    },
-    'table',
-);
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableFooter,
+    TablePagination,
+    TableRow,
+} from '@material-ui/core';
 
 class TableView extends Component {
-    static propTypes = {
-        field: fieldPropTypes.isRequired,
-        data: PropTypes.arrayOf(PropTypes.object).isRequired,
-        total: PropTypes.number.isRequired,
-        p: polyglotPropTypes.isRequired,
-    };
-
     constructor(props) {
         super(props);
+        this.onPageChange = this.onPageChange.bind(this);
+        this.state = {
+            rowsPerPage: props.pageSize,
+            page: 0,
+        };
+    }
+
+    onPageChange(event, newPage) {
+        this.setState({
+            rowsPerPage: this.state.rowsPerPage,
+            page: newPage,
+        });
     }
 
     render() {
-        const { data } = this.props;
+        const { data, pageSize, p } = this.props;
 
         const link = (id, url) => {
             if (isLocalURL(id))
@@ -44,21 +47,52 @@ class TableView extends Component {
         };
 
         return (
-            <Table>
-                <TableBody>
-                    {data.map((entry, index) => (
-                        <TableRow key={`${index}-table`}>
-                            <TableCell>{entry.title}</TableCell>
-                            <TableCell className={styles.elementExpectFirst}>
-                                {link(entry.id, entry.url)}
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+            <TableContainer>
+                <Table>
+                    <TableBody>
+                        {data
+                            .slice(
+                                this.state.page * this.state.rowsPerPage,
+                                this.state.page * this.state.rowsPerPage +
+                                    this.state.rowsPerPage,
+                            )
+                            .map((entry, index) => (
+                                <TableRow key={`${index}-table`}>
+                                    <TableCell>{entry.title}</TableCell>
+                                    <TableCell>
+                                        {link(entry.id, entry.url)}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                    </TableBody>
+                    <TableFooter>
+                        <TablePagination
+                            rowsPerPageOptions={[
+                                pageSize,
+                                pageSize * 2,
+                                pageSize * 3,
+                                { label: p.t('all'), value: -1 },
+                            ]}
+                            rowsPerPage={this.state.rowsPerPage}
+                            count={data.length}
+                            page={this.state.page}
+                            onChangePage={this.onPageChange}
+                            labelRowsPerPage={p.t('rows_per_page')}
+                        />
+                    </TableFooter>
+                </Table>
+            </TableContainer>
         );
     }
 }
+
+TableView.propTypes = {
+    field: fieldPropTypes.isRequired,
+    data: PropTypes.arrayOf(PropTypes.object).isRequired,
+    total: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequired,
+    p: polyglotPropTypes.isRequired,
+};
 
 const mapStateToProps = (_, { formatData, spaceWidth }) => {
     if (!formatData || !formatData.items) {
