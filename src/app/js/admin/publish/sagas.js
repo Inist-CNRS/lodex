d019aa831c65df325994f4d3e8d13c59900b7532
Anthony Rimet <anthony.rimet03@gmail.com>
2021-10-05 17:15:42 +0200

    17: Fix handlePublish function

--- src/app/js/admin/publish/sagas.js
+++ src/app/js/admin/publish/sagas.js
@@ -13,58 +13,62 @@ import fetchSaga from '../../lib/sagas/fetchSaga';
 import { FINISH_PROGRESS, ERROR_PROGRESS } from '../progress/reducer';
 
 export function* handlePublishRequest() {
-    const verifyUriRequest = yield select(fromUser.getVerifyUriRequest);
-    const verifyResponse = yield call(fetchSaga, verifyUriRequest);
+    try {
+        const verifyUriRequest = yield select(fromUser.getVerifyUriRequest);
+        const verifyResponse = yield call(fetchSaga, verifyUriRequest);
 
-    const {
-        error: verifyError,
-        response: { nbInvalidUri, nbInvalidSubresourceUriMap },
-    } = verifyResponse;
-
-    if (verifyError) {
-        yield put(publishError(verifyError));
-        return;
-    }
-
-    const countInvalidSubresourceUri = nbInvalidSubresourceUriMap
-        ? Object.keys(nbInvalidSubresourceUriMap).length
-        : 0;
-
-    const needWarn = nbInvalidUri > 0 || countInvalidSubresourceUri > 0;
-
-    if (needWarn) {
-        yield put(publishWarn({ nbInvalidUri, nbInvalidSubresourceUriMap }));
-        const { cancel } = yield race({
-            cancel: take(PUBLISH_CANCEL),
-            ok: take(PUBLISH_CONFIRM),
-        });
-
-        if (cancel) {
+        if (verifyResponse.error) {
+            yield put(publishError(verifyResponse.error));
             return;
         }
-    }
+        const {
+            response: { nbInvalidUri, nbInvalidSubresourceUriMap },
+        } = verifyResponse;
 
-    const request = yield select(fromUser.getPublishRequest);
-    const { error } = yield call(fetchSaga, request);
+        const countInvalidSubresourceUri = nbInvalidSubresourceUriMap
+            ? Object.keys(nbInvalidSubresourceUriMap).length
+            : 0;
 
-    if (error) {
-        yield put(publishError(error));
-        return;
-    } else if (needWarn) {
+        const needWarn = nbInvalidUri > 0 || countInvalidSubresourceUri > 0;
+
+        if (needWarn) {
+            yield put(
+                publishWarn({ nbInvalidUri, nbInvalidSubresourceUriMap }),
+            );
+            const { cancel } = yield race({
+                cancel: take(PUBLISH_CANCEL),
+                ok: take(PUBLISH_CONFIRM),
+            });
+
+            if (cancel) {
+                return;
+            }
+        }
+
+        const request = yield select(fromUser.getPublishRequest);
+        const { error } = yield call(fetchSaga, request);
+
+        if (error) {
+            yield put(publishError(error));
+            return;
+        } else if (needWarn) {
+            yield put(publishSuccess());
+        }
+
+        const { progressError } = yield race({
+            progressFinish: take(FINISH_PROGRESS),
+            progressError: take(ERROR_PROGRESS),
+        });
+
+        if (progressError) {
+            yield put(publishError(progressError.payload.error));
+            return;
+        }
+
         yield put(publishSuccess());
+    } catch (e) {
+        console.log(e);
     }
-
-    const { progressError } = yield race({
-        progressFinish: take(FINISH_PROGRESS),
-        progressError: take(ERROR_PROGRESS),
-    });
-
-    if (progressError) {
-        yield put(publishError(progressError.payload.error));
-        return;
-    }
-
-    yield put(publishSuccess());
 }
 
 export default function*() {
