1738114a709ef3b18c39b4bd53f563b96f69c9de
Anthony Rimet <anthony.rimet03@gmail.com>
2021-10-06 14:51:50 +0200

    6: add unit test for StepValue

---
+++ src/app/js/fields/wizard/StepValue.spec.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import StepValueValue from './StepValueValue';
+import StepValueColumn from './StepValueColumn';
+import StepValueConcat from './StepValueConcat';
+import StepValueSubresource from './StepValueSubresource';
+import StepValueSubresourceField from './StepValueSubresourceField';
+
+import { StepValueComponent as StepValue } from './StepValue';
+
+describe.only('StepValue', () => {
+    const defaultProps = {
+        isSubresourceField: false,
+        handleChange: jest.fn(),
+        filter: null,
+    };
+
+    it('should render StepValue with all values when is resource field', () => {
+        const wrapper = shallow(<StepValue {...defaultProps}/>);
+        expect(wrapper.find(StepValueValue)).toHaveLength(1);
+        expect(wrapper.find(StepValueColumn)).toHaveLength(1);
+        expect(wrapper.find(StepValueConcat)).toHaveLength(1);
+        expect(wrapper.find(StepValueSubresource)).toHaveLength(1);
+        expect(wrapper.find(StepValueSubresourceField)).toHaveLength(1);
+    });
+
+    it('should render StepValue with arbitrary and column values when is subresource field', () => {
+        const wrapper = shallow(<StepValue {...defaultProps} isSubresourceField={true}/>);
+        expect(wrapper.find(StepValueValue)).toHaveLength(1);
+        expect(wrapper.find(StepValueColumn)).toHaveLength(1);
+        expect(wrapper.find(StepValueConcat)).toHaveLength(0);
+        expect(wrapper.find(StepValueSubresource)).toHaveLength(0);
+        expect(wrapper.find(StepValueSubresourceField)).toHaveLength(0);
+    });
+});
