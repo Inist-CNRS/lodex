5d19d3430ba761d4a9c56112e8400776a4bf8501
Guillaume BILLEY <guilbill@gmail.com>
2021-11-10 15:39:02 +0100

    Avoid display clearPublishButton when publishing

--- src/app/js/admin/publish/PublicationButton.js
+++ src/app/js/admin/publish/PublicationButton.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 import { fromFields } from '../../sharedSelectors';
 import PublishButton from './PublishButton';
 import ValidationButton from './ValidationButton';
-import { fromPublication, fromProgress } from '../selectors';
+import { fromPublication, fromProgress, fromPublish } from '../selectors';
 import compose from 'recompose/compose';
 import { ClearPublishedButton } from '../clear/ClearPublishedButton';
 import { PUBLISH_DOCUMENT } from '../../../../common/progressStatus';
@@ -13,14 +13,14 @@ import { PUBLISH_DOCUMENT } from '../../../../common/progressStatus';
 const mapStateToProps = state => ({
     invalidFields: fromFields.getInvalidFields(state),
     hasPublishedDataset: fromPublication.hasPublishedDataset(state),
-    ...fromProgress.getProgress(state),
+    isLoading: fromPublish.getIsPublishing(state),
 });
 
 export default compose(
     connect(mapStateToProps),
     branch(
-        ({ hasPublishedDataset, status }) =>
-            hasPublishedDataset && status !== PUBLISH_DOCUMENT,
+        ({ hasPublishedDataset, isLoading }) =>
+            hasPublishedDataset && !isLoading,
         renderComponent(ClearPublishedButton),
     ),
     branch(
