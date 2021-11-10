2f8921b4ccb17b46b3ac0b9b98b9208d3504b06b
Guillaume BILLEY <guilbill@gmail.com>
2021-11-10 12:00:07 +0100

    Start progress on Unpublish data

--- src/api/controller/api/publish.js
+++ src/api/controller/api/publish.js
@@ -1,5 +1,7 @@
 import Koa from 'koa';
 import route from 'koa-route';
+import { PUBLISH_DOCUMENT } from '../../../common/progressStatus';
+import progress from '../../services/progress';
 
 import { publisherQueue, PUBLISH } from '../../workers/publisher';
 
@@ -20,10 +22,11 @@ export const clearPublished = async ctx => {
             { $unset: { lodex_published: '' } },
             { multi: true },
         );
+        progress.start(UNPUBLISH_DOCUMENT);
         await ctx.publishedDataset.remove({});
         await ctx.publishedCharacteristic.remove({});
         await ctx.publishedFacet.remove({});
-
+        progress.finish();
         ctx.body = {
             status: 'success',
         };
