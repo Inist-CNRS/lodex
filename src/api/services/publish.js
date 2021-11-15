37a827495a43b0e31d7ed19666ece1e145254401
Guillaume BILLEY <guilbill@gmail.com>
2021-11-15 16:35:03 +0100

    clear published data before publishing new ones

--- src/api/services/publish.js
+++ src/api/services/publish.js
@@ -1,6 +1,7 @@
 import progress from './progress';
 import indexSearchableFields from './indexSearchableFields';
 import { CREATE_INDEX } from '../../common/progressStatus';
+import clearPublished from './clearPublished';
 import {
     SCOPE_COLLECTION,
     SCOPE_DATASET,
@@ -9,6 +10,8 @@ import {
 } from '../../common/scope';
 
 export default async ctx => {
+    await clearPublished(ctx);
+
     const count = await ctx.dataset.count({});
     const fields = await ctx.field.findAll();
 
@@ -18,16 +21,18 @@ export default async ctx => {
 
     await ctx.publishDocuments(ctx, count, collectionScopeFields);
 
+
     const datasetScopeFields = fields.filter(c =>
         [SCOPE_DATASET, SCOPE_GRAPHIC].includes(c.scope),
     );
-
     await ctx.publishCharacteristics(ctx, datasetScopeFields, count);
+
     await ctx.publishFacets(ctx, fields, true);
 
     progress.start(CREATE_INDEX, null);
 
     await indexSearchableFields();
+
 
     progress.finish();
 };
