4f4c6d16786897a3eeaafad42e58da76fc85949d
Anthony Rimet <anthony.rimet03@gmail.com>
2021-11-26 10:29:38 +0100

    Update ressource icon

--- src/app/js/fields/FieldToggleInternalScope.js
+++ src/app/js/fields/FieldToggleInternalScope.js
@@ -3,7 +3,7 @@ import translate from 'redux-polyglot/translate';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import HomeIcon from '@material-ui/icons/Home';
-import DescriptionIcon from '@material-ui/icons/Description';
+import MainResourceIcon from '@material-ui/icons/InsertDriveFile';
 import EqualizerIcon from '@material-ui/icons/Equalizer';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
@@ -40,7 +40,7 @@ export const FieldToggleInternalScopeComponent = ({ input }) => {
                 <HomeIcon />
             </ToggleButton>
             <ToggleButton value="document" aria-label="centered">
-                <DescriptionIcon />
+                <MainResourceIcon />
             </ToggleButton>
             <ToggleButton value="subRessource" aria-label="centered">
                 <FileCopyIcon />
