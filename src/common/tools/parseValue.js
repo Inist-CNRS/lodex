d9c629f910e1963724259760e6138d65e793eb92
Guillaume BILLEY <guilbill@gmail.com>
2021-11-29 15:58:40 +0100

    Add parseValue helper to parse string or object

---
+++ src/common/tools/parseValue.js
@@ -0,0 +1,12 @@
+const parseValue = value => {
+    if (typeof value === 'object') {
+        return value;
+    }
+    try {
+        return JSON.parse(value);
+    } catch (e) {
+        return value;
+    }
+};
+
+export default parseValue;
