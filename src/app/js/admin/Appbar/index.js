5b84d2d2e6820ccf7f6be7f9990998697e539be2
Guillaume BILLEY <guilbill@gmail.com>
2021-12-14 10:43:23 +0100

    Style progress component

--- src/app/js/admin/Appbar/index.js
+++ src/app/js/admin/Appbar/index.js
@@ -5,18 +5,32 @@ import { NavLink } from 'react-router-dom';
 import translate from 'redux-polyglot/translate';
 import compose from 'recompose/compose';
 import StorageIcon from '@material-ui/icons/Storage';
+import Fade from '@material-ui/core/Fade';
 import AspectRatioIcon from '@material-ui/icons/AspectRatio';
-import { AppBar, CircularProgress, Button, Toolbar } from '@material-ui/core';
+import {
+    AppBar,
+    CircularProgress,
+    Button,
+    Toolbar,
+    LinearProgress,
+    Box,
+    Typography,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import PublicationButton from '../publish/PublicationButton';
 import { fromUser } from '../../sharedSelectors';
-import { fromParsing, fromPublication } from '../selectors';
+import {
+    fromParsing,
+    fromProgress,
+    fromPublication,
+    fromPublish,
+} from '../selectors';
 import { polyglot as polyglotPropTypes } from '../../propTypes';
 import Link from '../../lib/components/Link';
 import theme from './../../theme';
 import Menu from './Menu';
-import PublishedButton from './PublishedButton';
+import GoToPublicationButton from './GoToPublicationButton';
 const useStyles = makeStyles({
     linkToHome: {
         color: `${theme.white.primary} !important`,
@@ -56,6 +70,31 @@ const useStyles = makeStyles({
         alignItems: 'stretch',
         height: 64,
     },
+    loadingProgress: {
+        borderBottomLeftRadius: '0px',
+        borderBottomRightRadius: '0px',
+    },
+    progress: {
+        width: '100%',
+        display: 'flex',
+        margin: '0 4px 0',
+    },
+    colorPrimary: { backgroundColor: theme.white.primary },
+    barColorPrimary: { backgroundColor: theme.green.secondary },
+    progressContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        marginLeft: 'auto',
+        width: '130px',
+    },
+    progressLabel: {
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
 });
 
 const activeButtonStyle = {
@@ -69,6 +108,9 @@ const AppbarComponent = ({
     isAdmin,
     p: polyglot,
     hasPublishedDataset,
+    isPublishing,
+    target,
+    progress,
 }) => {
     const classes = useStyles();
 
@@ -107,7 +149,31 @@ const AppbarComponent = ({
         <>
             {isAdmin && (
                 <div style={{ display: 'flex' }}>
-                    {hasPublishedDataset && <PublishedButton />}
+                    <Fade in={isPublishing} out={!isPublishing}>
+                        <Box className={classes.progressContainer}>
+                            <div className={classes.progressLabel}>
+                                <CircularProgress
+                                    variant="indeterminate"
+                                    color={theme.white.primary}
+                                    size={20}
+                                />
+                                <Typography>
+                                    {polyglot.t('publishing')}
+                                </Typography>
+                            </div>
+                            <LinearProgress
+                                // className={classes.progress}
+                                classes={{
+                                    root: classes.progress,
+                                    colorPrimary: classes.colorPrimary,
+                                    barColorPrimary: classes.barColorPrimary,
+                                }}
+                                variant="determinate"
+                                value={target ? (progress / target) * 100 : 0}
+                            />
+                        </Box>
+                    </Fade>
+                    {hasPublishedDataset && <GoToPublicationButton />}
                     <PublicationButton className={classes.button} />
                     <Menu />
                 </div>
@@ -145,6 +211,9 @@ AppbarComponent.propTypes = {
     isAdmin: PropTypes.bool.isRequired,
     p: polyglotPropTypes.isRequired,
     hasPublishedDataset: PropTypes.bool,
+    isPublishing: PropTypes.bool,
+    target: PropTypes.number,
+    progress: PropTypes.number,
 };
 
 AppbarComponent.defaultProps = {
@@ -158,5 +227,7 @@ export default compose(
         isLoading: state.loading,
         isAdmin: fromUser.isAdmin(state),
         hasPublishedDataset: fromPublication.hasPublishedDataset(state),
+        isPublishing: fromPublish.getIsPublishing(state),
+        ...fromProgress.getProgressAndTarget(state),
     })),
 )(AppbarComponent);
