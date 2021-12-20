ca2a597404e45ca526bab63f5ee98cfc49dae829
Guillaume BILLEY <guilbill@gmail.com>
2021-12-20 09:58:11 +0100

    Adapt tests to new chunk size

--- src/api/services/transformAllDocuments.spec.js
+++ src/api/services/transformAllDocuments.spec.js
@@ -7,7 +7,7 @@ describe('tranformAllDocuments', () => {
     let transformerCallCount = 0;
 
     const dataset = {
-        length: 1000,
+        length: 200,
         map: jest.fn().mockImplementation(() => {
             // Emulate fluctuant transformed results
             transformerCallCount++;
@@ -32,13 +32,13 @@ describe('tranformAllDocuments', () => {
     });
 
     it('should load items from the original dataset and insert them in the publishedDataset by page of 100', () => {
-        expect(findLimitFromSkip).toHaveBeenCalledWith(1000, 0, {
+        expect(findLimitFromSkip).toHaveBeenCalledWith(200, 0, {
             lodex_published: { $exists: false },
         });
-        expect(findLimitFromSkip).toHaveBeenCalledWith(1000, 1000, {
+        expect(findLimitFromSkip).toHaveBeenCalledWith(200, 200, {
             lodex_published: { $exists: false },
         });
-        expect(findLimitFromSkip).toHaveBeenCalledWith(1000, 2000, {
+        expect(findLimitFromSkip).toHaveBeenCalledWith(200, 400, {
             lodex_published: { $exists: false },
         });
     });
@@ -52,6 +52,18 @@ describe('tranformAllDocuments', () => {
     });
 
     it('should have called progress.incrementProgress with transformedDataset.length', () => {
-        expect(progress.incrementProgress.mock.calls).toEqual([[1], [2], [3]]);
+        expect(progress.incrementProgress.mock.calls).toEqual([
+            [1],
+            [2],
+            [3],
+            [4],
+            [5],
+            [6],
+            [7],
+            [8],
+            [9],
+            [10],
+            [11],
+        ]);
     });
 });
