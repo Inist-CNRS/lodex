a565ff9ef5571995d870355649af08dd3cca233a
Guillaume BILLEY <guilbill@gmail.com>
2021-12-22 15:33:52 +0100

    export store for utility method

--- src/app/js/admin/index.js
+++ src/app/js/admin/index.js
@@ -53,7 +53,7 @@ const initialState = {
 };
 
 const history = createHashHistory();
-const store = configureStore(
+export const store = configureStore(
     rootReducer,
     sagas,
     window.__PRELOADED_STATE__ || initialState,
