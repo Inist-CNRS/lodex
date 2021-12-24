f4cc5d58f2bb83b8236410f190b43e4b24264856
Guillaume BILLEY <guilbill@gmail.com>
2021-12-24 16:14:32 +0100

    Add condition to stop publishing when job is not active

--- src/api/services/indexSearchableFields.js
+++ src/api/services/indexSearchableFields.js
@@ -2,8 +2,11 @@ import composeAsync from '../../common/lib/composeAsync';
 import mongoClient from './mongoClient';
 import field from '../models/field';
 import publishedDataset from '../models/publishedDataset';
+import progress from './progress';
+import { CREATE_INDEX } from '../../common/progressStatus';
 
 export default async () => {
+    progress.start(CREATE_INDEX, null, null, 'publishing', null, 'publisher');
     return composeAsync(
         await composeAsync(mongoClient, field, f => f.findSearchableNames)(),
         await composeAsync(
--- src/api/services/publish.js
+++ src/api/services/publish.js
@@ -1,6 +1,5 @@
 import progress from './progress';
 import indexSearchableFields from './indexSearchableFields';
-import { CREATE_INDEX } from '../../common/progressStatus';
 import clearPublished from './clearPublished';
 import {
     SCOPE_COLLECTION,
@@ -8,6 +7,8 @@ import {
     SCOPE_DOCUMENT,
     SCOPE_GRAPHIC,
 } from '../../common/scope';
+
+const isJobActive = async ctx => !ctx.job || (await ctx.job.isActive());
 
 export default async ctx => {
     const count = await ctx.dataset.count({});
@@ -17,19 +18,19 @@ export default async ctx => {
         [SCOPE_COLLECTION, SCOPE_DOCUMENT].includes(c.scope),
     );
 
-    await clearPublished(ctx);
-    await ctx.publishDocuments(ctx, count, collectionScopeFields);
+    (await isJobActive(ctx)) && (await clearPublished(ctx));
+    (await isJobActive(ctx)) &&
+        (await ctx.publishDocuments(ctx, count, collectionScopeFields));
 
     const datasetScopeFields = fields.filter(c =>
         [SCOPE_DATASET, SCOPE_GRAPHIC].includes(c.scope),
     );
-    await ctx.publishCharacteristics(ctx, datasetScopeFields, count);
+    (await isJobActive(ctx)) &&
+        (await ctx.publishCharacteristics(ctx, datasetScopeFields, count));
 
-    await ctx.publishFacets(ctx, fields, true);
+    (await isJobActive(ctx)) && (await ctx.publishFacets(ctx, fields, true));
 
-    progress.start(CREATE_INDEX, null, null, 'publishing', null, 'publisher');
-
-    await indexSearchableFields();
+    (await isJobActive(ctx)) && (await indexSearchableFields());
 
     progress.finish();
 };
