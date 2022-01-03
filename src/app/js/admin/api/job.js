7a582cf13ef4a590569ae45bf655c2c6fb9b2387
Guillaume BILLEY <guilbill@gmail.com>
2022-01-03 17:19:46 +0100

    Parse only when localStorage item is a string

--- src/app/js/admin/api/job.js
+++ src/app/js/admin/api/job.js
@@ -1,8 +1,14 @@
 import fetch from '../../lib/fetch';
 import { getJobLogsRequest, getCancelJobRequest } from '../../user';
 
-export const getUserLocalStorageInfo = () =>
-    JSON.parse(window.localStorage.getItem('redux-localstorage').user);
+export const getUserLocalStorageInfo = () => {
+    const localStorageItem = JSON.parse(
+        window.localStorage.getItem('redux-localstorage'),
+    );
+    return typeof localStorageItem === 'string'
+        ? JSON.parse(localStorageItem).user
+        : localStorageItem.user;
+};
 
 const getJobLogs = (queue, jobId) => {
     const userInfo = getUserLocalStorageInfo();
