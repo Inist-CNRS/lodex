b9ce12edaa627ae591a209953e6c76199b5b2e8d
Guillaume BILLEY <guilbill@gmail.com>
2022-01-21 09:48:03 +0100

    Add dataset endpoint

--- src/api/controller/api/dataset.js
+++ src/api/controller/api/dataset.js
@@ -25,11 +25,12 @@ export const clearDataset = async ctx => {
 
 export const getDataset = async ctx => {
     const { skip, limit } = ctx.query;
-    const datas = ctx.dataset
+    const datas = await ctx.dataset
         .find()
-        .skip(skip)
-        .limit(limit);
-    ctx.body(datas);
+        .skip(skip ? parseInt(skip) : 0)
+        .limit(limit ? parseInt(limit, 10) : 10)
+        .toArray();
+    ctx.body = datas;
 };
 
 app.use(route.delete('/', clearDataset));
