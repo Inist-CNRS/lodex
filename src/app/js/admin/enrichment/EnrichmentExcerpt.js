f1ab747942cb35d58a0df83b826ef4649a7a1e2a
Guillaume BILLEY <guilbill@gmail.com>
2022-01-26 16:06:37 +0100

    Add source preview helper

--- src/app/js/admin/enrichment/EnrichmentExcerpt.js
+++ src/app/js/admin/enrichment/EnrichmentExcerpt.js
@@ -14,62 +14,91 @@ import {
     TableHead,
     TableRow,
 } from '@material-ui/core';
+import { HelpOutline as HelpOutlineIcon } from '@material-ui/icons';
 import { isLongText, getShortText } from '../../lib/longTexts';
+import { makeStyles } from '@material-ui/styles';
 
 const EXCERPT_HEIGHT = 350;
 
-export const EnrichmentExcerptComponent = ({ lines, loading, p: polyglot }) => (
-    <TableContainer component={Paper} style={{ height: EXCERPT_HEIGHT }}>
-        <Table stickyHeader>
-            <TableHead>
-                <TableRow>
-                    <TableCell>{polyglot.t('sourcePreview')}</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {!loading &&
-                    lines.map(line => {
-                        const stringifyLine = JSON.stringify(line);
-                        return (
-                            <TableRow key={stringifyLine}>
-                                <TableCell
-                                    component="th"
-                                    scope="row"
-                                    title={stringifyLine}
+const useStyle = makeStyles({
+    header: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+});
+
+export const EnrichmentExcerptComponent = ({
+    lines,
+    loading,
+    advancedMode,
+    p: polyglot,
+}) => {
+    const classes = useStyle();
+    return (
+        <TableContainer component={Paper} style={{ height: EXCERPT_HEIGHT }}>
+            <Table stickyHeader>
+                <TableHead>
+                    <TableRow>
+                        <TableCell className={classes.header}>
+                            <div>{polyglot.t('sourcePreview')}</div>
+                            {advancedMode && (
+                                <div
+                                    title={polyglot.t(
+                                        'advancedModePreviewInfo',
+                                    )}
                                 >
-                                    {isLongText(stringifyLine)
-                                        ? getShortText(stringifyLine)
-                                        : stringifyLine}
-                                </TableCell>
-                            </TableRow>
-                        );
-                    })}
-                {loading && (
-                    <TableRow>
-                        <TableCell component="th" scope="row">
-                            <CircularProgress
-                                variant="indeterminate"
-                                size={20}
-                            />
+                                    <HelpOutlineIcon />
+                                </div>
+                            )}
                         </TableCell>
                     </TableRow>
-                )}
-                {!loading && lines.length === 0 && (
-                    <TableRow>
-                        <TableCell component="th" scope="row">
-                            {polyglot.t('completes_field_none')}
-                        </TableCell>
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
-    </TableContainer>
-);
+                </TableHead>
+                <TableBody>
+                    {!loading &&
+                        lines.map(line => {
+                            const stringifyLine = JSON.stringify(line);
+                            return (
+                                <TableRow key={stringifyLine}>
+                                    <TableCell
+                                        component="th"
+                                        scope="row"
+                                        title={stringifyLine}
+                                    >
+                                        {isLongText(stringifyLine)
+                                            ? getShortText(stringifyLine)
+                                            : stringifyLine}
+                                    </TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    {loading && (
+                        <TableRow>
+                            <TableCell component="th" scope="row">
+                                <CircularProgress
+                                    variant="indeterminate"
+                                    size={20}
+                                />
+                            </TableCell>
+                        </TableRow>
+                    )}
+                    {!loading && lines.length === 0 && (
+                        <TableRow>
+                            <TableCell component="th" scope="row">
+                                {polyglot.t('completes_field_none')}
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+};
 
 EnrichmentExcerptComponent.propTypes = {
     lines: PropTypes.array.isRequired,
     loading: PropTypes.bool,
     p: polyglotPropTypes.isRequired,
+    advancedMode: PropTypes.bool,
 };
 
 const mapStateToProps = () => ({});
