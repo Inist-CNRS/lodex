64b6f3698f7346abb8ea533a62f5e9f1ac93ed5e
Guillaume BILLEY <guilbill@gmail.com>
2022-01-27 11:15:39 +0100

    Clean unused import and useless props

--- src/app/js/admin/EnrichmentRoute.js
+++ src/app/js/admin/EnrichmentRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
+
 import { compose } from 'recompose';
-import { connect } from 'react-redux';
 import translate from 'redux-polyglot/translate';
 import withInitialData from './withInitialData';
 
@@ -26,14 +26,7 @@ export const EnrichmentRouteComponent = () => {
     );
 };
 
-EnrichmentRouteComponent.propTypes = {};
-
-const mapStateToProps = state => ({});
-
-const mapDispatchToProps = {};
-
 export const EnrichmentRoute = compose(
     withInitialData,
-    connect(mapStateToProps, mapDispatchToProps),
     translate,
 )(EnrichmentRouteComponent);
