9762a362d03c585af8ce5f498a2443ea79726b14
Guillaume BILLEY <guilbill@gmail.com>
2022-02-02 14:11:24 +0100

    Adjust AceEditor props

--- src/app/js/lib/components/FormSourceCodeField.js
+++ src/app/js/lib/components/FormSourceCodeField.js
@@ -2,15 +2,18 @@ import React from 'react';
 import AceEditor from 'react-ace';
 import { formField as formFieldPropTypes } from '../../propTypes';
 import 'ace-builds/src-noconflict/mode-ini';
-import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/theme-monokai';
 
-const FormSourceCodeField = ({ input, label, p, dispatch }) => {
+const FormSourceCodeField = ({ input, label, p, dispatch, ...custom }) => {
     return (
         <AceEditor
             mode="ini"
-            theme="github"
+            theme="monokai"
+            wrapEnabled={true}
+            fontSize={14}
             value={input.value}
             onChange={input.onChange}
+            {...custom}
         />
     );
 };
