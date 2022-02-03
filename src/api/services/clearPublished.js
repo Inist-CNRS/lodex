ba1f0e0aa8eea6e132b99eaa1c9d5d08a7f1aa9c
Guillaume BILLEY <guilbill@gmail.com>
2022-02-03 10:07:34 +0100

    Display dépublication progress

--- src/api/services/clearPublished.js
+++ src/api/services/clearPublished.js
@@ -1,11 +1,23 @@
+import { UNPUBLISH_DOCUMENT } from '../../common/progressStatus';
+import progress from './progress';
 
-export default async (ctx) => {
+export default async ctx => {
+    progress.start({
+        status: UNPUBLISH_DOCUMENT,
+        target: 100,
+        label: 'publishing',
+        type: 'publisher',
+    });
     await ctx.dataset.updateMany(
-            {},
-            { $unset: { lodex_published: '' } },
-            { multi: true },
-        );
+        {},
+        { $unset: { lodex_published: '' } },
+        { multi: true },
+    );
+    progress.incrementProgress(25);
     await ctx.publishedDataset.remove({});
+    progress.incrementProgress(25);
     await ctx.publishedCharacteristic.remove({});
+    progress.incrementProgress(25);
     await ctx.publishedFacet.remove({});
-}
+    progress.finish();
+};
