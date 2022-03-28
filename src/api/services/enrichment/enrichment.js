d83c0c36707ee78eb29b9dcb0e542653d334e467
Nicolas Thouvenin <nthouvenin@gmail.com>
2022-03-28 14:53:14 +0200

    better test

--- src/api/services/enrichment/enrichment.js
+++ src/api/services/enrichment/enrichment.js
@@ -234,12 +234,17 @@ export const processEnrichment = async (enrichment, ctx) => {
 
             const logsEnrichedValue = [];
             for (const enrichedValue of enrichedValues) {
-                const value =
-                    enrichedValue.value === undefined ||
-                    enrichedValue.value === null ||
-                    (enrichedValue.error && `[Error] ${enrichedValue.error}`) ||
-                    'n/a';
-
+                let value;
+                if (enrichedValue.error) {
+                    value = `[Error] ${enrichedValue.error}`;
+                } else if (
+                    enrichedValue.value !== undefined &&
+                    enrichedValue.value !== null
+                ) {
+                    value = enrichedValue.value;
+                } else {
+                    value = 'n/a';
+                }
                 const id = enrichedValue.id;
                 const logData = JSON.stringify({
                     level: enrichedValue.error ? 'error' : 'info',
