e605e0579fd14f0036ed700793dc15e98b47809e
Nicolas Thouvenin <nthouvenin@gmail.com>
2022-04-15 12:01:54 +0200

    no working on ezmaster

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 redis-server &
-wait $!
+# wait $! no working on ezmaster so use sleep
+sleep 1
 redis-cli FLUSHALL
 find /app/src/app/custom ! -user daemon -exec chown daemon:daemon {} \; &
 find /tmp ! -user daemon -exec chown daemon:daemon {} \; &
