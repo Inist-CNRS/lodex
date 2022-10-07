32f0a9d57e299692f45021151d6a2b11056df098
Nicolas Thouvenin <nthouvenin@gmail.com>
2022-10-07 18:36:39 +0200

    fix: subpath & multi values

--- src/api/services/enrichment/enrichment.spec.js
+++ src/api/services/enrichment/enrichment.spec.js
@@ -281,7 +281,7 @@ describe('enrichment', () => {
 
             // THEN
             expect(results).toEqual(
-                expect.arrayContaining([['plop', 'plup'], ['plip'], ['ploup']]),
+                expect.arrayContaining([[{"subPath": "plop"}, {"subPath": "plup"}], [{"subPath": "plop"}, {"subPath": "plup"}], [{"subPath": "plip"}], [{"subPath": "plip"}], [{"subPath": "ploup"}], [{"subPath": "ploup"}]]),
             );
         });
         // We skip that test because it's a very specific case where we want to get a subpath in an array that is stringified, that may not happen. If the dataset import a string, then, it's a string.
