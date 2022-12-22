0ca4e244ed8d5ec39a6a8da218346c7ae882d717
Anthony Rimet <anthony.rimet03@gmail.com>
2022-12-22 10:24:10 +0100

    Feat: Add drag for sortable operation

--- src/app/js/fields/TransformerList.js
+++ src/app/js/fields/TransformerList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import compose from 'recompose/compose';
 import translate from 'redux-polyglot/translate';
@@ -8,6 +8,21 @@ import memoize from 'lodash.memoize';
 
 import { polyglot as polyglotPropTypes } from '../propTypes';
 import TransformerListItem from './TransformerListItem';
+
+import {
+    DndContext,
+    closestCenter,
+    KeyboardSensor,
+    PointerSensor,
+    useSensor,
+    useSensors,
+} from '@dnd-kit/core';
+import {
+    arrayMove,
+    SortableContext,
+    sortableKeyboardCoordinates,
+    verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
 
 import {
     getTransformerMetas,
@@ -40,36 +55,90 @@ const TransformerList = ({
     type,
     hideFirstTransformers,
     p: polyglot,
-}) => (
-    <div>
-        <ListSubheader style={styles.header}>
-            {polyglot.t('transformers')}
-        </ListSubheader>
-        {touched && error && <span>{error}</span>}
-        {fields.map((fieldName, index) => (
-            <TransformerListItem
-                key={fieldName}
-                fieldName={fieldName}
-                onRemove={() => fields.remove(index)}
-                operation={fields.get(index).operation}
-                type={type}
-                show={
-                    showTransformer(fields.get(index).operation, type) &&
-                    (!hideFirstTransformers || index >= hideFirstTransformers)
-                }
-            />
-        ))}
-        <div style={{ textAlign: 'center' }}>
-            <Button
-                variant="text"
-                className="add-transformer"
-                onClick={() => fields.push({})}
+}) => {
+    const [fieldsToDrag, setFieldsToDrag] = useState(
+        fields.map(fieldName => fieldName),
+    );
+
+    useEffect(() => {
+        setFieldsToDrag(fields.map(fieldName => fieldName));
+    }, [fields]);
+
+    const sensors = useSensors(
+        useSensor(PointerSensor, { activationConstraint: { distance: 5 } }),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        }),
+    );
+
+    const handleDragEnd = event => {
+        const { active, over } = event;
+        let oldItemIndex;
+        let newItemIndex;
+        fields.map((fieldName, index) => {
+            if (fieldName === active.id) {
+                oldItemIndex = index;
+            }
+            if (fieldName === over.id) {
+                newItemIndex = index;
+            }
+        });
+
+        fields.move(oldItemIndex, newItemIndex);
+        setFieldsToDrag(fieldsToDrag => {
+            return arrayMove(fieldsToDrag, oldItemIndex, newItemIndex);
+        });
+    };
+    return (
+        <div>
+            <ListSubheader style={styles.header}>
+                {polyglot.t('transformers')}
+            </ListSubheader>
+            {touched && error && <span>{error}</span>}
+            <DndContext
+                sensors={sensors}
+                collisionDetection={closestCenter}
+                onDragEnd={handleDragEnd}
             >
-                {polyglot.t('add_transformer')}
-            </Button>
+                <SortableContext
+                    items={fieldsToDrag}
+                    strategy={verticalListSortingStrategy}
+                >
+                    {fieldsToDrag?.map((fieldName, index) => (
+                        <TransformerListItem
+                            key={fieldName}
+                            id={fieldName}
+                            fieldName={fieldName}
+                            onRemove={() => {
+                                fields.remove(index);
+                            }}
+                            operation={fields.get(index)?.operation}
+                            type={type}
+                            show={
+                                showTransformer(
+                                    fields.get(index)?.operation,
+                                    type,
+                                ) &&
+                                (!hideFirstTransformers ||
+                                    index >= hideFirstTransformers)
+                            }
+                        />
+                    ))}
+                </SortableContext>
+            </DndContext>
+
+            <div style={{ textAlign: 'center' }}>
+                <Button
+                    variant="text"
+                    className="add-transformer"
+                    onClick={() => fields.push({})}
+                >
+                    {polyglot.t('add_transformer')}
+                </Button>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 TransformerList.propTypes = {
     hideFirstTransformers: PropTypes.number,
@@ -78,6 +147,7 @@ TransformerList.propTypes = {
         get: PropTypes.func.isRequired,
         remove: PropTypes.func.isRequired,
         push: PropTypes.func.isRequired,
+        move: PropTypes.func.isRequired,
     }).isRequired,
     meta: PropTypes.shape({
         touched: PropTypes.bool,
