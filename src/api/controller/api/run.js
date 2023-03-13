f43e924f56fddb4c0335cef471bf5cf3fd7fd331
thibault-barrat <contact@thibault-barrat.com>
2023-03-13 14:29:36 +0100

    Feat: graph more responsive

--- src/api/controller/api/run.js
+++ src/api/controller/api/run.js
@@ -33,81 +33,92 @@ const middlewareScript = async (ctx, scriptNameCalledParam, fieldsParams) => {
         ctx.throw(404, `Unknown script '${scriptNameCalledParam}'.ini`);
     }
 
-    const [, metaData, , script] = currentScript;
-    ctx.type = metaData.mimeType;
-    ctx.status = 200;
-    if (metaData.fileName) {
-        const attachmentOpts = {
-            fallback: false,
-            type: metaData.type === 'file' ? 'attachment' : 'inline',
-        };
-        ctx.attachment(metaData.fileName, attachmentOpts);
-    }
-    // Legacy
-    const orderBy = [
-        ctx.query.sortBy || '_id',
-        String(ctx.query.sortDir || 'asc').toLowerCase(),
-    ].join('/');
+    try {
+        const [, metaData, , script] = currentScript;
+        ctx.type = metaData.mimeType;
+        ctx.status = 200;
+        if (metaData.fileName) {
+            const attachmentOpts = {
+                fallback: false,
+                type: metaData.type === 'file' ? 'attachment' : 'inline',
+            };
+            ctx.attachment(metaData.fileName, attachmentOpts);
+        }
+        // Legacy
+        const orderBy = [
+            ctx.query.sortBy || '_id',
+            String(ctx.query.sortDir || 'asc').toLowerCase(),
+        ].join('/');
 
-    const field = parseFieldsParams(fieldsParams);
-    const environment = {
-        ...localConfig,
-        ...ctx.query, // like lodex-extended server
-        field,
-    };
-    const host = getCleanHost();
-    const query = {
-        orderBy,
-        field,
-        ...ctx.query, //usefull ?
-        connectionStringURI: mongoConnectionString,
-        host,
-    };
-    const input = new PassThrough({ objectMode: true });
-    const commands = ezs.parseString(script, environment);
-    const errorHandle = err => {
-        ctx.status = 503;
-        ctx.body.destroy();
-        input.destroy();
-        global.console.error('Error with ', ctx.path, ' and', ctx.query, err);
-    };
-    const emptyHandle = () => {
-        if (ctx.headerSent === false) {
-            ctx.body.write('{"total":0}');
+        const field = parseFieldsParams(fieldsParams);
+        const environment = {
+            ...localConfig,
+            ...ctx.query, // like lodex-extended server
+            field,
+        };
+        const host = getCleanHost();
+        const query = {
+            orderBy,
+            field,
+            ...ctx.query, //usefull ?
+            connectionStringURI: mongoConnectionString,
+            host,
+        };
+        const input = new PassThrough({ objectMode: true });
+        const commands = ezs.parseString(script, environment);
+        const errorHandle = err => {
+            ctx.status = 503;
+            ctx.body.destroy();
+            input.destroy();
             global.console.error(
-                'Empty response with ',
+                'Error with ',
                 ctx.path,
                 ' and',
                 ctx.query,
+                err,
             );
+        };
+        const emptyHandle = () => {
+            if (ctx.headerSent === false) {
+                ctx.body.write('{"total":0}');
+                global.console.error(
+                    'Empty response with ',
+                    ctx.path,
+                    ' and',
+                    ctx.query,
+                );
+            }
+        };
+        if (localConfig.pluginsAPI) {
+            const wurl = URL.parse(localConfig.pluginsAPI);
+            wurl.pathname = `/routines/${scriptNameCalledParam}.ini`;
+            wurl.search = qs.stringify(query, { indices: false });
+            const href = URL.format(wurl);
+            // Warning : Don't use proxy with docker virtual network
+            process.env.no_proxy = String(process.env.no_proxy || 'localhost')
+                .split(',')
+                .concat(wurl.hostname)
+                .filter((value, index, self) => self.indexOf(value) === index)
+                .join(',');
+            const response = await fetch(href);
+            ctx.body = response.body
+                .on('finish', emptyHandle)
+                .on('error', errorHandle);
+        } else {
+            const statement = scripts.useCache() ? 'boost' : 'delegate';
+            ctx.body = input
+                .pipe(ezs(statement, { commands }, environment))
+                .pipe(ezs.catch())
+                .on('finish', emptyHandle)
+                .on('error', errorHandle)
+                .pipe(ezs.toBuffer());
         }
-    };
-    if (localConfig.pluginsAPI) {
-        const wurl = URL.parse(localConfig.pluginsAPI);
-        wurl.pathname = `/routines/${scriptNameCalledParam}.ini`;
-        wurl.search = qs.stringify(query, { indices: false });
-        const href = URL.format(wurl);
-        // Warning : Don't use proxy with docker virtual network
-        process.env.no_proxy = String(process.env.no_proxy || 'localhost')
-            .split(',')
-            .concat(wurl.hostname)
-            .filter((value, index, self) => self.indexOf(value) === index)
-            .join(',');
-        const response = await fetch(href);
-        ctx.body = response.body
-            .on('finish', emptyHandle)
-            .on('error', errorHandle);
-    } else {
-        const statement = scripts.useCache() ? 'boost' : 'delegate';
-        ctx.body = input
-            .pipe(ezs(statement, { commands }, environment))
-            .pipe(ezs.catch())
-            .on('finish', emptyHandle)
-            .on('error', errorHandle)
-            .pipe(ezs.toBuffer());
+        input.write(query);
+        input.end();
+    } catch (err) {
+        console.error(err);
+        ctx.throw(500, err);
     }
-    input.write(query);
-    input.end();
 };
 
 export async function getScripts(ctx) {
