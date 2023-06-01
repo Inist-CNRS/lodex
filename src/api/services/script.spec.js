22e2ea0154cbf7428f5102637b6567e474b40202
Nicolas Thouvenin <nthouvenin@gmail.com>
2023-06-01 19:12:30 +0200

    fix e2e tests

--- src/api/services/script.spec.js
+++ src/api/services/script.spec.js
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import Script from './script';
 
-describe('script ', () => {
+describe('script(routines)', () => {
     const routine = new Script('routines');
 
     it('should exist locally', async () => {
@@ -11,5 +11,13 @@ describe('script ', () => {
     it('should not exist', async () => {
         const current = await routine.get('fake-world');
         assert.equal(current, undefined);
+    });
+});
+describe('script(loaders)', () => {
+    const loaders = new Script('loaders');
+
+    it('should exist locally', async () => {
+        const current = await loaders.get('csv');
+        assert.equal(current.length, 4);
     });
 });
