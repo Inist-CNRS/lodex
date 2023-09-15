b8790bcf8bbf2e379741c2e0ead7a944f30c4a53
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-09-15 08:33:10 +0200

    refactor: convert CartographyAdmin Component into a FunctionComponent

--- src/app/js/formats/vega-lite/component/cartography/CartographyAdmin.js
+++ src/app/js/formats/vega-lite/component/cartography/CartographyAdmin.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import translate from 'redux-polyglot/translate';
 import { schemeOrRd } from 'd3-scale-chromatic';
@@ -23,137 +23,130 @@ export const defaultArgs = {
     worldPosition: MAP_WORLD,
 };
 
-class CartographyAdmin extends Component {
-    static propTypes = {
-        args: PropTypes.shape({
-            params: PropTypes.shape({
-                maxSize: PropTypes.number,
-                maxValue: PropTypes.number,
-                minValue: PropTypes.number,
-                orderBy: PropTypes.string,
-            }),
-            colorScheme: PropTypes.arrayOf(PropTypes.string),
-            tooltip: PropTypes.bool,
-            tooltipCategory: PropTypes.string,
-            tooltipValue: PropTypes.string,
-            worldPosition: PropTypes.oneOf([MAP_WORLD, MAP_EUROPE, MAP_FRANCE]),
-        }),
-        onChange: PropTypes.func.isRequired,
-        p: polyglotPropTypes.isRequired,
-        showMaxSize: PropTypes.bool.isRequired,
-        showMaxValue: PropTypes.bool.isRequired,
-        showMinValue: PropTypes.bool.isRequired,
-        showOrderBy: PropTypes.bool.isRequired,
+const CartographyAdmin = props => {
+    const {
+        p: polyglot,
+        args,
+        showMaxSize,
+        showMaxValue,
+        showMinValue,
+        showOrderBy,
+    } = props;
+    const {
+        params,
+        colorScheme,
+        tooltip,
+        tooltipCategory,
+        tooltipValue,
+        worldPosition,
+    } = args;
+
+    const handleParams = params => updateAdminArgs('params', params, props);
+
+    const handleWorldPosition = e => {
+        updateAdminArgs('worldPosition', e.target.value, props);
     };
 
-    static defaultProps = {
-        args: defaultArgs,
-        showMaxSize: true,
-        showMaxValue: true,
-        showMinValue: true,
-        showOrderBy: true,
-    };
-
-    constructor(props) {
-        super(props);
-        this.setTooltipValue = this.setTooltipValue.bind(this);
-        this.setTooltipCategory = this.setTooltipCategory.bind(this);
-    }
-
-    setParams = params => updateAdminArgs('params', params, this.props);
-
-    setWorldPosition = e => {
-        updateAdminArgs('worldPosition', e.target.value, this.props);
-    };
-
-    setColorScheme = (_, colorScheme) => {
+    const handleColorScheme = (_, colorScheme) => {
         updateAdminArgs(
             'colorScheme',
             colorScheme.props.value.split(','),
-            this.props,
+            props,
         );
     };
 
-    toggleTooltip = () => {
-        updateAdminArgs('tooltip', !this.props.args.tooltip, this.props);
+    const toggleTooltip = () => {
+        updateAdminArgs('tooltip', !tooltip, props);
     };
 
-    setTooltipCategory(tooltipCategory) {
-        updateAdminArgs('tooltipCategory', tooltipCategory, this.props);
-    }
+    const handleTooltipCategory = tooltipCategory => {
+        updateAdminArgs('tooltipCategory', tooltipCategory, props);
+    };
 
-    setTooltipValue(tooltipValue) {
-        updateAdminArgs('tooltipValue', tooltipValue, this.props);
-    }
+    const handleTooltipValue = tooltipValue => {
+        updateAdminArgs('tooltipValue', tooltipValue, props);
+    };
 
-    render() {
-        const {
-            p: polyglot,
-            args: {
-                params,
-                colorScheme,
-                tooltip,
-                tooltipCategory,
-                tooltipValue,
-                worldPosition,
-            },
-            showMaxSize,
-            showMaxValue,
-            showMinValue,
-            showOrderBy,
-        } = this.props;
-
-        return (
-            <Box
-                display="flex"
-                flexWrap="wrap"
-                justifyContent="space-between"
-                gap={2}
+    return (
+        <Box
+            display="flex"
+            flexWrap="wrap"
+            justifyContent="space-between"
+            gap={2}
+        >
+            <RoutineParamsAdmin
+                params={params || defaultArgs.params}
+                onChange={handleParams}
+                polyglot={polyglot}
+                showMaxSize={showMaxSize}
+                showMaxValue={showMaxValue}
+                showMinValue={showMinValue}
+                showOrderBy={showOrderBy}
+            />
+            <TextField
+                fullWidth
+                select
+                label={polyglot.t('world_position')}
+                value={worldPosition}
+                onChange={handleWorldPosition}
             >
-                <RoutineParamsAdmin
-                    params={params || defaultArgs.params}
-                    onChange={this.setParams}
-                    polyglot={polyglot}
-                    showMaxSize={showMaxSize}
-                    showMaxValue={showMaxValue}
-                    showMinValue={showMinValue}
-                    showOrderBy={showOrderBy}
-                />
-                <TextField
-                    fullWidth
-                    select
-                    label={polyglot.t('world_position')}
-                    value={worldPosition}
-                    onChange={this.setWorldPosition}
-                >
-                    <MenuItem value={MAP_WORLD}>
-                        {polyglot.t('world_position_world')}
-                    </MenuItem>
-                    <MenuItem value={MAP_EUROPE}>
-                        {polyglot.t('world_position_europe')}
-                    </MenuItem>
-                    <MenuItem value={MAP_FRANCE}>
-                        {polyglot.t('world_position_france')}
-                    </MenuItem>
-                </TextField>
-                <GradientSchemeSelector
-                    label={polyglot.t('color_scheme')}
-                    onChange={this.setColorScheme}
-                    value={colorScheme}
-                />
-                <ToolTips
-                    checked={tooltip}
-                    onChange={this.toggleTooltip}
-                    onCategoryTitleChange={this.setTooltipCategory}
-                    categoryTitle={tooltipCategory}
-                    onValueTitleChange={this.setTooltipValue}
-                    valueTitle={tooltipValue}
-                    polyglot={polyglot}
-                    thirdValue={false}
-                />
-            </Box>
-        );
-    }
-}
+                <MenuItem value={MAP_WORLD}>
+                    {polyglot.t('world_position_world')}
+                </MenuItem>
+                <MenuItem value={MAP_EUROPE}>
+                    {polyglot.t('world_position_europe')}
+                </MenuItem>
+                <MenuItem value={MAP_FRANCE}>
+                    {polyglot.t('world_position_france')}
+                </MenuItem>
+            </TextField>
+            <GradientSchemeSelector
+                label={polyglot.t('color_scheme')}
+                onChange={handleColorScheme}
+                value={colorScheme}
+            />
+            <ToolTips
+                checked={tooltip}
+                onChange={toggleTooltip}
+                onCategoryTitleChange={handleTooltipCategory}
+                categoryTitle={tooltipCategory}
+                onValueTitleChange={handleTooltipValue}
+                valueTitle={tooltipValue}
+                polyglot={polyglot}
+                thirdValue={false}
+            />
+        </Box>
+    );
+};
+
+CartographyAdmin.propTypes = {
+    args: PropTypes.shape({
+        params: PropTypes.shape({
+            maxSize: PropTypes.number,
+            maxValue: PropTypes.number,
+            minValue: PropTypes.number,
+            orderBy: PropTypes.string,
+        }),
+        colorScheme: PropTypes.arrayOf(PropTypes.string),
+        tooltip: PropTypes.bool,
+        tooltipCategory: PropTypes.string,
+        tooltipValue: PropTypes.string,
+        worldPosition: PropTypes.oneOf([MAP_WORLD, MAP_EUROPE, MAP_FRANCE]),
+    }),
+    onChange: PropTypes.func.isRequired,
+    p: polyglotPropTypes.isRequired,
+    showMaxSize: PropTypes.bool.isRequired,
+    showMaxValue: PropTypes.bool.isRequired,
+    showMinValue: PropTypes.bool.isRequired,
+    showOrderBy: PropTypes.bool.isRequired,
+};
+
+CartographyAdmin.defaultProps = {
+    args: defaultArgs,
+    showMaxSize: true,
+    showMaxValue: true,
+    showMinValue: true,
+    showOrderBy: true,
+};
 
 export default translate(CartographyAdmin);
