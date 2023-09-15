642713e9b8b03ea38acebbb535315aa151f7b671
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-09-15 08:18:05 +0200

    refactor: convert HeatMapAdmin Component into a FunctionComponent

--- src/app/js/formats/vega-lite/component/heatmap/HeatMapAdmin.js
+++ src/app/js/formats/vega-lite/component/heatmap/HeatMapAdmin.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import translate from 'redux-polyglot/translate';
 import { schemeOrRd } from 'd3-scale-chromatic';
 import PropTypes from 'prop-types';
@@ -23,140 +23,130 @@ export const defaultArgs = {
     tooltipWeight: 'Weight',
 };
 
-class HeatMapAdmin extends Component {
-    static propTypes = {
-        args: PropTypes.shape({
-            params: PropTypes.shape({
-                maxSize: PropTypes.number,
-                maxValue: PropTypes.number,
-                minValue: PropTypes.number,
-                orderBy: PropTypes.string,
-            }),
-            colorScheme: PropTypes.arrayOf(PropTypes.string),
-            flipAxis: PropTypes.bool,
-            tooltip: PropTypes.bool,
-            tooltipSource: PropTypes.string,
-            tooltipTarget: PropTypes.string,
-            tooltipWeight: PropTypes.string,
-        }),
-        onChange: PropTypes.func.isRequired,
-        p: polyglotPropTypes.isRequired,
-        showMaxSize: PropTypes.bool.isRequired,
-        showMaxValue: PropTypes.bool.isRequired,
-        showMinValue: PropTypes.bool.isRequired,
-        showOrderBy: PropTypes.bool.isRequired,
+const HeatMapAdmin = props => {
+    const {
+        p: polyglot,
+        args,
+        showMaxSize,
+        showMaxValue,
+        showMinValue,
+        showOrderBy,
+    } = props;
+
+    const {
+        params,
+        colorScheme,
+        flipAxis,
+        tooltip,
+        tooltipSource,
+        tooltipTarget,
+        tooltipWeight,
+    } = args;
+
+    const handleParams = params => {
+        updateAdminArgs('params', params, props);
     };
 
-    static defaultProps = {
-        args: defaultArgs,
-        showMaxSize: true,
-        showMaxValue: true,
-        showMinValue: true,
-        showOrderBy: true,
-    };
-
-    constructor(props) {
-        super(props);
-        this.setTooltipSource = this.setTooltipSource.bind(this);
-        this.setTooltipTarget = this.setTooltipTarget.bind(this);
-        this.setTooltipWeight = this.setTooltipWeight.bind(this);
-    }
-
-    setParams = params => {
-        updateAdminArgs('params', params, this.props);
-    };
-
-    handleColorSchemeChange = (_, colorScheme) => {
+    const handleColorSchemeChange = (_, colorScheme) => {
         updateAdminArgs(
             'colorScheme',
             colorScheme.props.value.split(','),
-            this.props,
+            props,
         );
     };
 
-    toggleFlipAxis = () => {
-        updateAdminArgs('flipAxis', !this.props.args.flipAxis, this.props);
+    const toggleFlipAxis = () => {
+        updateAdminArgs('flipAxis', !flipAxis, props);
     };
 
-    toggleTooltip = () => {
-        updateAdminArgs('tooltip', !this.props.args.tooltip, this.props);
+    const toggleTooltip = () => {
+        updateAdminArgs('tooltip', !tooltip, props);
     };
 
-    setTooltipSource(tooltipSource) {
-        updateAdminArgs('tooltipSource', tooltipSource, this.props);
-    }
+    const handleTooltipSource = tooltipSource => {
+        updateAdminArgs('tooltipSource', tooltipSource, props);
+    };
 
-    setTooltipTarget(tooltipTarget) {
-        updateAdminArgs('tooltipTarget', tooltipTarget, this.props);
-    }
+    const handleTooltipTarget = tooltipTarget => {
+        updateAdminArgs('tooltipTarget', tooltipTarget, props);
+    };
 
-    setTooltipWeight(tooltipWeight) {
-        updateAdminArgs('tooltipWeight', tooltipWeight, this.props);
-    }
+    const handleTooltipWeight = tooltipWeight => {
+        updateAdminArgs('tooltipWeight', tooltipWeight, props);
+    };
 
-    render() {
-        const {
-            p: polyglot,
-            args: {
-                params,
-                colorScheme,
-                flipAxis,
-                tooltip,
-                tooltipSource,
-                tooltipTarget,
-                tooltipWeight,
-            },
-            showMaxSize,
-            showMaxValue,
-            showMinValue,
-            showOrderBy,
-        } = this.props;
+    return (
+        <Box
+            display="flex"
+            flexWrap="wrap"
+            justifyContent="space-between"
+            gap={2}
+        >
+            <RoutineParamsAdmin
+                params={params || defaultArgs.params}
+                polyglot={polyglot}
+                onChange={handleParams}
+                showMaxSize={showMaxSize}
+                showMaxValue={showMaxValue}
+                showMinValue={showMinValue}
+                showOrderBy={showOrderBy}
+            />
+            <ToolTips
+                checked={tooltip}
+                onChange={toggleTooltip}
+                onCategoryTitleChange={handleTooltipSource}
+                categoryTitle={tooltipSource}
+                onValueTitleChange={handleTooltipTarget}
+                valueTitle={tooltipTarget}
+                polyglot={polyglot}
+                thirdValue={true}
+                onThirdValueChange={handleTooltipWeight}
+                thirdValueTitle={tooltipWeight}
+            />
+            <GradientSchemeSelector
+                label={polyglot.t('color_scheme')}
+                onChange={handleColorSchemeChange}
+                value={colorScheme}
+            />
+            <FormControlLabel
+                control={
+                    <Checkbox onChange={toggleFlipAxis} checked={flipAxis} />
+                }
+                label={polyglot.t('flip_axis')}
+            />
+        </Box>
+    );
+};
 
-        return (
-            <Box
-                display="flex"
-                flexWrap="wrap"
-                justifyContent="space-between"
-                gap={2}
-            >
-                <RoutineParamsAdmin
-                    params={params || defaultArgs.params}
-                    polyglot={polyglot}
-                    onChange={this.setParams}
-                    showMaxSize={showMaxSize}
-                    showMaxValue={showMaxValue}
-                    showMinValue={showMinValue}
-                    showOrderBy={showOrderBy}
-                />
-                <ToolTips
-                    checked={tooltip}
-                    onChange={this.toggleTooltip}
-                    onCategoryTitleChange={this.setTooltipSource}
-                    categoryTitle={tooltipSource}
-                    onValueTitleChange={this.setTooltipTarget}
-                    valueTitle={tooltipTarget}
-                    polyglot={polyglot}
-                    thirdValue={true}
-                    onThirdValueChange={this.setTooltipWeight}
-                    thirdValueTitle={tooltipWeight}
-                />
-                <GradientSchemeSelector
-                    label={polyglot.t('color_scheme')}
-                    onChange={this.handleColorSchemeChange}
-                    value={colorScheme}
-                />
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            onChange={this.toggleFlipAxis}
-                            checked={flipAxis}
-                        />
-                    }
-                    label={polyglot.t('flip_axis')}
-                />
-            </Box>
-        );
-    }
-}
+HeatMapAdmin.propTypes = {
+    args: PropTypes.shape({
+        params: PropTypes.shape({
+            maxSize: PropTypes.number,
+            maxValue: PropTypes.number,
+            minValue: PropTypes.number,
+            orderBy: PropTypes.string,
+        }),
+        colorScheme: PropTypes.arrayOf(PropTypes.string),
+        flipAxis: PropTypes.bool,
+        tooltip: PropTypes.bool,
+        tooltipSource: PropTypes.string,
+        tooltipTarget: PropTypes.string,
+        tooltipWeight: PropTypes.string,
+    }),
+    onChange: PropTypes.func.isRequired,
+    p: polyglotPropTypes.isRequired,
+    showMaxSize: PropTypes.bool.isRequired,
+    showMaxValue: PropTypes.bool.isRequired,
+    showMinValue: PropTypes.bool.isRequired,
+    showOrderBy: PropTypes.bool.isRequired,
+};
+
+HeatMapAdmin.defaultProps = {
+    args: defaultArgs,
+    showMaxSize: true,
+    showMaxValue: true,
+    showMinValue: true,
+    showOrderBy: true,
+};
 
 export default translate(HeatMapAdmin);
