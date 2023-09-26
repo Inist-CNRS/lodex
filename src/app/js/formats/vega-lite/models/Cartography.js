14bfd05ae9af3617c85d2e0894b39e46b7968261
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-09-26 08:28:15 +0200

    docs: fix typo

--- src/app/js/formats/vega-lite/models/Cartography.js
+++ src/app/js/formats/vega-lite/models/Cartography.js
@@ -27,7 +27,7 @@ class Cartography extends BasicChart {
     }
 
     /**
-     * Function use for rebuild the edited spec
+     * Rebuild the edited spec
      * @param widthIn{number | null}
      */
     buildSpec(widthIn = null) {
