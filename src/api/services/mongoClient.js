4adb61fed681779048d05fcb794b37548275760b
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-10-11 15:52:05 +0200

    docs: add a small doc to informe about the difference between Client and DB

--- src/api/services/mongoClient.js
+++ src/api/services/mongoClient.js
@@ -1,12 +1,20 @@
 import { MongoClient } from 'mongodb';
 import config from 'config';
 
+/**
+ * @type {Map<string, MongoClient>}
+ */
 let clients = new Map();
 
 export const mongoConnectionString = tenant =>
     `mongodb://${config.mongo.host}/${config.mongo.dbName}_${tenant ||
         'default'}`;
 
+/**
+ * Get the DBMS client
+ * @param tenant Name of the Lodex instance
+ * @returns {Promise<MongoClient>}
+ */
 export const mongoClientConnectionFactory = async tenant => {
     if (!tenant) {
         throw new Error(
@@ -32,6 +40,11 @@ export const mongoClientConnectionFactory = async tenant => {
     }
 };
 
+/**
+ * Get the database client
+ * @param tenant Name of the Lodex instance
+ * @returns {Promise<Db>}
+ */
 export const mongoClientFactory = async tenant => {
     return (await mongoClientConnectionFactory(tenant)).db();
 };
