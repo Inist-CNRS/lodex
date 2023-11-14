3efbac354675726ba9b6bf1e369b1ada9b3b9708
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-11-14 07:45:42 +0100

    refactor: use a component to get and use the theme

--- src/app/js/public/index.js
+++ src/app/js/public/index.js
@@ -5,19 +5,13 @@ import React from 'react';
 import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import {
-    createTheme as createThemeMui,
-    ThemeProvider,
-} from '@mui/material/styles';
-
 import rootReducer from './reducers';
 import Routes from './Routes';
 import sagas from './sagas';
 import configureStore from '../configureStore';
 import phrasesFor from '../i18n/translations';
 import getLocale from '../../../common/getLocale';
-import customTheme from '../../custom/customTheme';
-import { themeLoader } from './api/themeLoader';
+import LodexThemeProvider from './LodexThemeProvider';
 
 const locale = getLocale();
 const initialState = {
@@ -42,24 +36,11 @@ const store = configureStore(
     history,
 );
 
-themeLoader()
-    .then(lodexTheme => {
-        hydrate(
-            <Provider {...{ store }}>
-                <ThemeProvider theme={createThemeMui(lodexTheme)}>
-                    <Routes history={history} />
-                </ThemeProvider>
-            </Provider>,
-            document.getElementById('root'),
-        );
-    })
-    .catch(() => {
-        hydrate(
-            <Provider {...{ store }}>
-                <ThemeProvider theme={createThemeMui(customTheme)}>
-                    <Routes history={history} />
-                </ThemeProvider>
-            </Provider>,
-            document.getElementById('root'),
-        );
-    });
+hydrate(
+    <Provider {...{ store }}>
+        <LodexThemeProvider>
+            <Routes history={history} />
+        </LodexThemeProvider>
+    </Provider>,
+    document.getElementById('root'),
+);
