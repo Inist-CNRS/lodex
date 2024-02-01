f1c39c4baa0b99516b85650b42e6efa0d90a2594
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-02-01 08:25:00 +0100

    fix (ezsLodex): correctly use allowDiskUse on aggregate query

--- packages/ezsLodex/src/mongoDatabase.js
+++ packages/ezsLodex/src/mongoDatabase.js
@@ -1,15 +1,17 @@
 import { MongoClient } from 'mongodb';
 
 export const handles = {};
+
+/**
+ * @param connectionStringURI {string}
+ * @returns {Promise<import('mongodb').Db>}
+ */
 async function mongoDatabase(connectionStringURI) {
     if (!handles[connectionStringURI]) {
-        handles[connectionStringURI] = new MongoClient(
-            connectionStringURI,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            },
-        );
+        handles[connectionStringURI] = new MongoClient(connectionStringURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
     }
     const client = await handles[connectionStringURI].connect();
     return client.db();
