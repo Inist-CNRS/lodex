60e9576f580fa23d9f5d41cf86f53a803edfe6a1
adrien guernier <adrien@marmelab.com>
2024-02-02 09:24:05 +0100

    remove useless code

--- src/api/controller/api/hiddenResource.js
+++ src/api/controller/api/hiddenResource.js
@@ -7,18 +7,12 @@ const app = new Koa();
 
 const getExportHiddenResources = async ctx => {
     const hiddenResources = await ctx.hiddenResource.findAll();
-    const filePath = `/tmp/hiddenResources.json`;
+    const date = new Date().getTime();
+    const filePath = `/tmp/hiddenResources${date}.json`;
     const stream = fs.createWriteStream(filePath);
     stream.write(JSON.stringify(hiddenResources));
     stream.end();
-    await new Promise(resolve => stream.on('finish', resolve));
     stream.on('finish', () => unlinkFile(filePath));
-
-    ctx.set(
-        'Content-disposition',
-        `attachment; filename="hiddenResources_${new Date().toISOString()}.json"`,
-    );
-    ctx.set('Content-type', 'application/json');
     ctx.body = fs.createReadStream(filePath);
     ctx.status = 200;
 };
