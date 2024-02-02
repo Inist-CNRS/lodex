05336d280870d506763813ad7b643206b668c536
adrien guernier <adrien@marmelab.com>
2024-02-02 11:02:31 +0100

    Fix: exporting hidden resources without stream

--- src/api/controller/api/hiddenResource.js
+++ src/api/controller/api/hiddenResource.js
@@ -1,19 +1,11 @@
 import Koa from 'koa';
 import route from 'koa-route';
-import fs from 'fs';
-import { unlinkFile } from '../../services/fsHelpers';
 
 const app = new Koa();
 
 const getExportHiddenResources = async ctx => {
     const hiddenResources = await ctx.hiddenResource.findAll();
-    const date = new Date().getTime();
-    const filePath = `/tmp/hiddenResources${date}.json`;
-    const stream = fs.createWriteStream(filePath);
-    stream.write(JSON.stringify(hiddenResources));
-    stream.end();
-    stream.on('finish', () => unlinkFile(filePath));
-    ctx.body = fs.createReadStream(filePath);
+    ctx.body = JSON.stringify(hiddenResources);
     ctx.status = 200;
 };
 
