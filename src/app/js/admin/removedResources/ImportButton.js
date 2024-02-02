d186a59d70328983cd842fe467bc8a88be864ffe
adrien guernier <adrien@marmelab.com>
2024-02-02 17:57:00 +0100

    reload page when upload is done

--- src/app/js/admin/removedResources/ImportButton.js
+++ src/app/js/admin/removedResources/ImportButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { compose } from 'recompose';
 import { polyglot as polyglotPropTypes } from '../../propTypes';
 import translate from 'redux-polyglot/translate';
@@ -6,6 +6,7 @@ import UploadIcon from '@mui/icons-material/Upload';
 import { Button, CircularProgress, styled } from '@mui/material';
 import { importHiddenResources } from '../api/hiddenResource';
 import { toast } from '../../../../common/tools/toast';
+import { useLocation, Redirect } from 'react-router-dom';
 
 const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
@@ -21,7 +22,10 @@ const VisuallyHiddenInput = styled('input')({
 
 const ImportButton = ({ p: polyglot }) => {
     const buttonLabel = polyglot.t('import');
+    const location = useLocation();
     const [uploading, setUploading] = useState(false);
+    const [done, setDone] = useState(false);
+
     const handleFileChange = async event => {
         setUploading(true);
         const file = event.target.files[0];
@@ -29,17 +33,21 @@ const ImportButton = ({ p: polyglot }) => {
         formData.append('file', file);
         const response = await importHiddenResources(formData);
         setUploading(false);
-
         if (response.error) {
             toast(polyglot.t('import_error'), {
                 type: toast.TYPE.ERROR,
             });
         } else {
+            setDone(true);
             toast(polyglot.t('import_successfull'), {
                 type: toast.TYPE.SUCCESS,
             });
         }
     };
+
+    if (done) {
+        return <Redirect to={location} />;
+    }
 
     return (
         <>
