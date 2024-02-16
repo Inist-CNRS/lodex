c48daaebf0607ed8ed8d7fa7e6e667471ef366bf
Nicolas Thouvenin <nthouvenin@gmail.com>
2024-02-16 16:16:39 +0100

    config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,10 @@ module.exports = {
             interpreter_args:
                 '--max-http-header-size=32768 --require @babel/register',
             exec_mode: 'cluster',
-            error_file: '/dev/stderr',
-            out_file: null,
-            pid_file: '/app',
+            wait_ready: true,
+            listen_timeout: 10000,
+            err_file: '/dev/null',
+            out_file: '/dev/null',
         },
         {
             name: 'worker',
@@ -16,9 +17,8 @@ module.exports = {
             interpreter_args: '--require @babel/register',
             exec_mode: 'fork',
             instances: 1,
-            error_file: '/dev/stderr',
-            out_file: null,
-            pid_file: '/app',
+            err_file: '/dev/null',
+            out_file: '/dev/null',
         },
     ],
 };
