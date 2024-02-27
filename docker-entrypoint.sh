9428fe82d2fd643a61a36cd133336668b0480154
Nicolas Thouvenin <nthouvenin@gmail.com>
2024-02-27 09:32:22 +0100

    start daemons at the same way

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-redis-server &
-# wait $! no working on ezmaster so use sleep
-sleep 1
-# redis-cli FLUSHALL || Disabling this command to avoid flushing the redis database
 find /app/src/app/custom ! -user daemon -exec chown daemon:daemon {} \; &
 find /tmp ! -user daemon -exec chown daemon:daemon {} \; &
 
