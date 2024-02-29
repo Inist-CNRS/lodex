281abb9d9cddd3cf570d0afc111576471fe592fc
Nicolas Thouvenin <nthouvenin@gmail.com>
2024-02-29 10:02:36 +0100

    semantic exporters no longer exists

--- packages/ezsLodex/src/JSONLDCompacter.js
+++
@@ -1,9 +0,0 @@
-import jsonld from 'jsonld';
-
-export default async function JSONLDCompacter(data, feed) {
-    if (this.isLast()) {
-        return feed.close();
-    }
-    const out = await jsonld.compact(data, {});
-    feed.send(out);
-}
--- packages/ezsLodex/src/JSONLDString.js
+++
@@ -1,9 +0,0 @@
-import jsonld from 'jsonld';
-
-export default async function JSONLDString(data, feed) {
-    if (this.isLast()) {
-        return feed.close();
-    }
-    const out = await jsonld.toRDF(data, { format: 'application/n-quads' });
-    feed.send(out);
-}
