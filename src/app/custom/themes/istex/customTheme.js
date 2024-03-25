06743346a94ca2f81257d25102f41e678589df05
Nicolas Thouvenin <nthouvenin@gmail.com>
2024-03-25 12:23:08 +0100

    initial import

---
+++ src/app/custom/themes/istex/customTheme.js
@@ -0,0 +1,54 @@
+import colorsTheme from "./colorsTheme";
+
+export default {
+    palette: {
+        primary: {
+            main: colorsTheme.blue.primary,
+            secondary: colorsTheme.blue.secondary,
+            light: colorsTheme.blue.light,
+            contrastText: colorsTheme.white.primary,
+        },
+        secondary: {
+            main: colorsTheme.green.secondary,
+            contrastText: colorsTheme.white.light,
+        },
+        info: {
+            main: colorsTheme.orange.primary,
+            contrastText: colorsTheme.white.primary,
+        },
+        warning: {
+            main: colorsTheme.orange.primary,
+            contrastText: colorsTheme.white.primary,
+        },
+        danger: {
+            main: colorsTheme.red.primary,
+            contrastText: colorsTheme.white.primary,
+        },
+        success: {
+            main: colorsTheme.green.secondary,
+            contrastText: colorsTheme.white.light,
+        },
+        neutral: {
+            main: colorsTheme.gray.primary,
+        },
+        neutralDark: {
+            main: colorsTheme.black.dark,
+            secondary: colorsTheme.black.secondary,
+            veryDark: colorsTheme.black.veryDark,
+            dark: colorsTheme.black.dark,
+            light: colorsTheme.black.light,
+            lighter: colorsTheme.black.lighter,
+            veryLight: colorsTheme.black.veryLight,
+            transparent: colorsTheme.black.transparent,
+        },
+        text: {
+            primary: colorsTheme.black.secondary,
+            main: colorsTheme.black.secondary,
+        },
+        contrast: {
+            main: colorsTheme.white.primary,
+            light: colorsTheme.white.light,
+        },
+        contrastThreshold: 3,
+    },
+};
