6e5fa12f077016892d2efafc1e0d7f47d48de1c5
Nicolas Thouvenin <nthouvenin@gmail.com>
2024-03-26 17:51:56 +0100

    add templatesytem for theme

--- src/api/models/themes.js
+++ src/api/models/themes.js
@@ -1,4 +1,3 @@
-import fs from 'fs/promises';
 import { jsHost, themesHost } from 'config';
 import config from '../../../config.json';
 import path from 'path';
@@ -7,17 +6,11 @@ import defaultCustomTheme from '../../app/custom/customTheme';
 import { version } from '../../../package.json';
 import deepClone from 'lodash/cloneDeep';
 import get from 'lodash/get';
+import { Eta } from 'eta';
 
 const logger = getLogger('system');
 
 const THEMES_VERSION = 2;
-
-const REGEX_JS_HOST = /\{\|__JS_HOST__\|}/g;
-const REGEX_THEMES_HOST = /\{\|__THEMES_HOST__\|}/g;
-const REGEX_LODEX_VERSION = /\{\|__LODEX_VERSION__\|}/g;
-const REGEX_THEME_HREF = /\{\|__THEME_HREF__\|}/g;
-const REGEX_THEME_TITLE = /\{\|__THEME_TITLE__\|}/g;
-const REGEX_THEME_SUMMARY = /\{\|__THEME_SUMMARY__\|}/g;
 
 /**
  * @type {Map<string, {name: {fr: string, en: string}, description: {fr: string, en: string}, index: string, customTheme: *}>}
@@ -73,16 +66,22 @@ const initAvailableThemes = () => {
     }
 };
 
-const loadFile = async (path, themeConfig = {}) => {
-    const { href = '', title = '', summary = '' } = themeConfig;
-    return (await fs.readFile(path))
-        .toString('utf8')
-        .replace(REGEX_JS_HOST, jsHost)
-        .replace(REGEX_THEMES_HOST, themesHost)
-        .replace(REGEX_LODEX_VERSION, version)
-        .replace(REGEX_THEME_HREF, href)
-        .replace(REGEX_THEME_TITLE, title)
-        .replace(REGEX_THEME_SUMMARY, summary);
+const loadFile = async (themeFile, themeConfig = {}) => {
+    const eta = new Eta({
+        views: path.dirname(themeFile),
+        debug: true,
+    });
+    const etaData = {
+        lodex: {
+            version,
+            base: { href: jsHost },
+        },
+        theme: {
+            ...themeConfig,
+            base: { href: themesHost },
+        },
+    };
+    return await eta.renderAsync(path.basename(themeFile), etaData);
 };
 
 const init = async () => {
@@ -122,15 +121,15 @@ const init = async () => {
                     'Lodex may attempt to load this theme. Please note that if you use this theme, it may cause visual problems.',
                 );
             }
-
             let indexLocation = '../../app/custom/index.html';
             if (themeConfig?.files?.theme?.index) {
                 indexLocation = `../../${uri}/${themeConfig.files.theme.index}`;
             }
 
+            const themeLocalConfig = get(config, `theme.${theme}`, {});
             const index = await loadFile(
                 path.resolve(__dirname, indexLocation),
-                get(config, `theme.${theme}`),
+                { ...themeConfig, ...themeLocalConfig },
             );
 
             let customTheme = deepClone(defaultCustomTheme);
@@ -153,6 +152,7 @@ const init = async () => {
             });
             availableThemes.set(theme, true);
         } catch (e) {
+            console.log('>>>>>>>>>>>>', e);
             logger.error(`unable to load ${theme} theme!`);
             logger.error(e);
         }
