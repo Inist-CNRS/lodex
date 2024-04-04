4503cf4b36130e8eb4a793df7de73cbb4de3f176
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-04-04 08:01:30 +0200

    feat: add tenant name in page title

--- src/app/js/lib/getTitle.js
+++ src/app/js/lib/getTitle.js
@@ -2,6 +2,12 @@ import { getCleanHost } from '../../../common/uris';
 
 export default () => {
     const host = getCleanHost();
+    /**
+     * @type {string}
+     */
+    const tenant = window.__TENANT__ ?? 'default';
 
-    return host ? /https?:\/\/([\w-]+)/.exec(host)[1] : 'example';
+    return host
+        ? `${tenant} - ${/https?:\/\/([\w-]+)/.exec(host)[1]}`
+        : `${tenant} - example`;
 };
