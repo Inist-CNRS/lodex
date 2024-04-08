b7547a198004c91dbfc34caae45c22843f117021
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-04-08 07:30:48 +0200

    fix: tenant name in page title

--- src/app/js/lib/getTitle.js
+++ src/app/js/lib/getTitle.js
@@ -1,13 +1,26 @@
 import { getCleanHost } from '../../../common/uris';
 
+const HOST_REGEX = /https?:\/\/([\w-]+)/;
+
 export default () => {
     const host = getCleanHost();
-    /**
-     * @type {string}
-     */
-    const tenant = window.__TENANT__ ?? 'default';
 
-    return host
-        ? `${tenant} - ${/https?:\/\/([\w-]+)/.exec(host)[1]}`
-        : `${tenant} - example`;
+    /**
+     * @type {string[]}
+     */
+    const title = [];
+
+    if (window && window.__TENANT__) {
+        title.push(window.__TENANT__);
+    } else {
+        title.push('default');
+    }
+
+    if (host) {
+        title.push(HOST_REGEX.exec(host)[1]);
+    } else {
+        title.push('example');
+    }
+
+    return title.join(' - ');
 };
