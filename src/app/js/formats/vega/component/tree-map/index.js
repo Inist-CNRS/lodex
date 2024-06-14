46822c5e258b139b2dc3f7d7932776b764717a01
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-06-14 08:19:57 +0200

    fix: Update treemap icon to always take the correct font size

--- src/app/js/formats/vega/component/tree-map/index.js
+++ src/app/js/formats/vega/component/tree-map/index.js
@@ -1,15 +1,12 @@
 import DefaultFormat from '../../../utils/components/default-format';
 import AdminComponent, { defaultArgs } from './TreeMapAdmin';
 import Component from './TreeMapView';
-import ViewQuiltIcon from '@mui/icons-material/ViewQuilt';
-import React from 'react';
+import Icon from './TreeMapIcon';
 
 export default {
     ...DefaultFormat,
     Component,
     AdminComponent,
     defaultArgs,
-    Icon: (props) => {
-        return <ViewQuiltIcon {...props}></ViewQuiltIcon>;
-    },
+    Icon,
 };
