0bb361cd27d979b43fa0fe4b0a4ff169e2b10515
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-06-25 13:12:13 +0200

    feat: replace fieldset with accordion

--- src/app/js/formats/utils/components/field-set/FormatFieldSets.js
+++ src/app/js/formats/utils/components/field-set/FormatFieldSets.js
@@ -1,38 +1,60 @@
 import React from 'react';
-import { Box } from '@mui/material';
 import PropTypes from 'prop-types';
 import translate from 'redux-polyglot/translate';
 import { polyglot as polyglotPropTypes } from '../../../../propTypes';
 import { formatAdminStyle } from '../../adminStyles';
+
+import Box from '@mui/material/Box';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 /**
  * Default field set component to use for translating declination
  * @param children {React.ReactNode} Field set content
  * @param title {string} translation key use to get the field set legend
  * @param p {{t: (string) => string}} polyglot use to get the text from a translation key
+ * @param defaultExpanded
+ * @param id
  * @returns {JSX.Element}
  */
-const FormatFieldSet = ({ children, title, p }) => {
+const FormatFieldSet = ({ children, title, p, defaultExpanded, id }) => {
     return (
-        <fieldset style={formatAdminStyle.fieldset}>
-            <legend style={formatAdminStyle.legend}>{p.t(title)}</legend>
-            <Box
-                display="flex"
-                flexWrap="wrap"
-                justifyContent="space-between"
-                gap={2}
-                style={formatAdminStyle.box}
+        <Accordion defaultExpanded={defaultExpanded}>
+            <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls={id}
+                id={id}
             >
-                {children}
-            </Box>
-        </fieldset>
+                <Typography>{p.t(title)}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Box
+                    display="flex"
+                    flexWrap="wrap"
+                    justifyContent="space-between"
+                    gap={2}
+                    style={formatAdminStyle.box}
+                >
+                    {children}
+                </Box>
+            </AccordionDetails>
+        </Accordion>
     );
+};
+
+FormatFieldSet.defaultProps = {
+    defaultExpanded: false,
 };
 
 FormatFieldSet.propTypes = {
     children: PropTypes.node.isRequired,
     title: PropTypes.string.isRequired,
     p: polyglotPropTypes.isRequired,
+    defaultExpanded: PropTypes.bool,
+    id: PropTypes.string.isRequired,
 };
 
 /**
@@ -47,7 +69,10 @@ const TranslatedVegaFieldSet = translate(FormatFieldSet);
  */
 export const FormatDefaultParamsFieldSet = ({ children }) => {
     return (
-        <TranslatedVegaFieldSet title="format_default_params">
+        <TranslatedVegaFieldSet
+            title="format_default_params"
+            id="format-field-set-default-param"
+        >
             {children}
         </TranslatedVegaFieldSet>
     );
@@ -64,7 +89,10 @@ FormatDefaultParamsFieldSet.propTypes = {
  */
 export const FormatDataParamsFieldSet = ({ children }) => {
     return (
-        <TranslatedVegaFieldSet title="format_data_params">
+        <TranslatedVegaFieldSet
+            title="format_data_params"
+            id="format-field-set-data-param"
+        >
             {children}
         </TranslatedVegaFieldSet>
     );
@@ -81,7 +109,10 @@ FormatDataParamsFieldSet.propTypes = {
  */
 export const FormatChartParamsFieldSet = ({ children }) => {
     return (
-        <TranslatedVegaFieldSet title="format_chart_params">
+        <TranslatedVegaFieldSet
+            title="format_chart_params"
+            id="format-field-set-chart-param"
+        >
             {children}
         </TranslatedVegaFieldSet>
     );
@@ -98,7 +129,10 @@ FormatChartParamsFieldSet.propTypes = {
  */
 export const FormatSubFormatParamsFieldSet = ({ children }) => {
     return (
-        <TranslatedVegaFieldSet title="format_sub_format_params">
+        <TranslatedVegaFieldSet
+            title="format_sub_format_params"
+            id="format-field-set-sub-format-param"
+        >
             {children}
         </TranslatedVegaFieldSet>
     );
