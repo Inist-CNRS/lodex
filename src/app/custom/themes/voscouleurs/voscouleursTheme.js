a3b8af22b32ed588ba7429deec7bf6aaba9bcee5
HeleneCreusot <helene.creusot@inist.fr>
2024-07-17 15:34:19 +0200

    perf(themes variables): add info documentation to config theme Voscouleurs
    idem for istex theme
    
    Closes #2092

--- src/app/custom/themes/voscouleurs/voscouleursTheme.js
+++ src/app/custom/themes/voscouleurs/voscouleursTheme.js
@@ -13,67 +13,67 @@
  * @type {import('@mui/material/styles').Theme['palette']}
  */
 const palette = {
-    mode: "light",
+    mode: 'light',
     background: {
-        default: "#f2eae4",
-        paper: "#fff",
+        default: '#fafafa',
+        paper: '#fafafa',
     },
     primary: {
         //icone navigation repos
-        main: "#353535",
-        contrastText: "#fff",
+        main: '#7dbd42',
+        contrastText: '#fff',
     },
     secondary: {
         //icone navigation active
         //rouge cea
-        main: "#e50019",
-        contrastText: "#fff",
+        main: '#f48022',
+        contrastText: '#fff',
     },
     info: {
         //default : purple
         //icon :hover
-        main: "#e50019",
+        main: '#b22f90',
     },
     warning: {
-        main: "#f48022",
-        contrastText: "#fff",
+        main: '#f48022',
+        contrastText: '#fff',
     },
     error: {
-        main: "#f44336",
+        main: '#f44336',
     },
     success: {
-        main: "#7dbd42",
-        contrastText: "#fff",
+        main: '#7dbd42',
+        contrastText: '#fff',
     },
     text: {
-        main: "rgb(98,99,104)", // Do not exist in mui
-        primary: "#353535",
-        secondary: "#222",
-        disabled: "rgba(98,99,104,0.55)",
+        main: 'rgb(98,99,104)', // Do not exist in mui
+        primary: '#353535',
+        secondary: '#222',
+        disabled: 'rgba(98,99,104,0.55)',
     },
     contrastThreshold: 3, // Mui default (we need to look if is pertinent to change it)
     // Do not exist in mui / Custom variable
     danger: {
-        main: "#f44336",
-        contrastText: "#fff",
+        main: '#f44336',
+        contrastText: '#fff',
     },
     neutral: {
-        main: "#e0e0e0",
+        main: '#e0e0e0',
     },
     // Use mui palette.grey ???
     neutralDark: {
-        main: "#555",
-        secondary: "rgb(98, 99, 104)",
-        veryDark: "#333",
-        dark: "#555",
-        light: "rgb(98, 99, 104, 0.5)",
-        lighter: "rgb(98, 99, 104, 0.15)",
-        veryLight: "rgb(98, 99, 104, 0.1)",
-        transparent: "rgba(0, 0, 0, .1)",
+        main: '#555',
+        secondary: 'rgb(98, 99, 104)',
+        veryDark: '#333',
+        dark: '#555',
+        light: 'rgb(98, 99, 104, 0.5)',
+        lighter: 'rgb(98, 99, 104, 0.15)',
+        veryLight: 'rgb(98, 99, 104, 0.1)',
+        transparent: 'rgba(0, 0, 0, .1)',
     },
     contrast: {
-        main: "#fff",
-        light: "rgba(255, 255, 255, .2)",
+        main: '#fff',
+        light: 'rgba(255, 255, 255, .2)',
     },
 };
 
