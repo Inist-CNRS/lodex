d99463c44a1d043694a4867a6da9a45c225b13d1
HeleneCreusot <helene.creusot@inist.fr>
2024-09-12 18:05:07 +0200

    refactor(theme istex): add colors charte istex

--- src/app/custom/themes/istex/istexTheme.js
+++ src/app/custom/themes/istex/istexTheme.js
@@ -12,21 +12,44 @@
  * https://zenoo.github.io/mui-theme-creator/
  * @type {import('@mui/material/styles').Theme['palette']}
  */
-/*TODO for :hover :focus secondary for active tertyary add blue: {
-        secondary: '#31768f',
-        tertiary: '#1a5a71',
-    }, */
+
+/*canard: {
+    primary: '#458CA5', //canard 0
+    dark: '#31768f', //canard -1
+    darker: '#1a5a71', //canard -2
+    light: '#69A1B6', //canard 1
+    lighter: '#8FBAC9', //canard 2
+    background: '#F6F9FA', //canard 3
+},
+lime: {
+    primary: '#C4D733', //lime 0
+    light: '#E3EF63', //lime 1
+    lighter: '#F9FBEB', //lime 2
+    dark: '#A9BB1E', //lime -1
+},
+gris: {
+    black: '#000000',
+    dark: '#1D1D1D', //gris 0
+    main: '#FAFAFA', //gris 1
+    light: '#8F8F8F', //gris 2
+    lighter: '#F0F0F0', //gris 3
+    white: '#FFFFFF',
+},*/
 const palette = {
     mode: 'light',
     primary: {
         main: '#458cA5',
-        secondary: '#31768F' /*secondary: '#337288' */,
-        light: '#f4f9fa',
+        secondary: '#31768F',
+        light: '#69A1B6',
+        lighter: '#8FBAC9',
+        veryLight: '#f6f9fa',
         contrastText: '#fff',
     },
     secondary: {
-        main: '#AABF23',
-        contrastText: '#f0f0f0',
+        main: '#A9BB1E',
+        light: '#E3EF63',
+        lighter: '#F9FBEB',
+        contrastText: '#fff',
     },
     info: {
         main: '#C4D733',
@@ -41,32 +64,27 @@ const palette = {
         contrastText: '#fff',
     },
     success: {
-        main: '#AABF23',
-        contrastText: '#f0f0f0',
+        main: '#A9BB1E',
+        contrastText: '#fff',
     },
     text: {
         primary: '#1d1d1d',
         main: '#4a4a4a',
     },
-    contrastThreshold: 3, // Mui default (we need to look if is pertinent to change it)
     // Do not exist in mui / Custom variable
     danger: {
         main: '#f44336',
         contrastText: '#fff',
     },
-    neutral: {
-        main: '#e0e0e0',
-    },
+    contrastThreshold: 4.1, // Mui default 3
     // Use mui palette.grey ???
     neutralDark: {
-        main: '#555',
+        main: '#8f8f8f',
         secondary: '#4a4a4a',
-        veryDark: '#000',
-        dark: '#555',
-        light: '#8f8f8f',
-        lighter: '#f0f0f0',
-        veryLight: '#f6f9fa',
-        transparent: 'rgba(0, 0, 0, .1)',
+        dark: '#1D1D1D',
+        light: '#f0f0f0',
+        lighter: '#f6f9fa',
+        transparent: 'rgba(0, 0, 0, 0.01)',
     },
     contrast: {
         main: '#fff',
