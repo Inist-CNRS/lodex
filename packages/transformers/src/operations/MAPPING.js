71d746d0c8e29231ff5440b8efb149765b040d14
Nicolas Thouvenin <nthouvenin@gmail.com>
2024-09-12 17:24:25 +0200

    change behaviour for MAPPING statement

--- packages/transformers/src/operations/MAPPING.js
+++ packages/transformers/src/operations/MAPPING.js
@@ -3,16 +3,23 @@ import { transformerWithArg } from './transformer';
 
 export const mapping = (value, input) => {
     const template = typeof input === 'string' ? input : input.toString();
-    const cleantemplate = template.trim().replace(/^[{[]+/, '').replace(/[}\]]+$/, '');
+    const cleantemplate = template
+        .trim()
+        .replace(/^[{[]+/, '')
+        .replace(/[}\]]+$/, '');
     const list = JSON6.parse(`{${cleantemplate}}`);
-    return Object
-        .keys(list)
-        .reduce((currentValue, searchValue) => currentValue
-            .split(searchValue)
-            .join(list[searchValue]), value);
+    const cleanedValue = String(value).trim();
+    const newValue = Object.keys(list)
+        .map((searchValue) =>
+            cleanedValue === searchValue ? list[searchValue] : null,
+        )
+        .filter(Boolean)
+        .shift();
+    return newValue || value;
 };
 
-const transformation = (_, args) => (value) => (transformerWithArg(mapping, 'list', value, args));
+const transformation = (_, args) => (value) =>
+    transformerWithArg(mapping, 'list', value, args);
 
 transformation.getMetas = () => ({
     name: 'MAPPING',
