de33107cfa5d83537d9f62cba4f1bb42f9268600
ThieryMichel <thiery@marmelab.com>
2025-01-24 16:13:20 +0100

    2325: add e2e test

--- cypress/e2e/phase_4/annotation.cy.js
+++ cypress/e2e/phase_4/annotation.cy.js
@@ -1,6 +1,7 @@
 import * as annotation from '../../support/annotation';
 import { teardown } from '../../support/authentication';
 import * as datasetImportPage from '../../support/datasetImportPage';
+import * as searchDrawer from '../../support/searchDrawer';
 import * as menu from '../../support/menu';
 
 describe('Annotation', () => {
@@ -33,11 +34,57 @@ describe('Annotation', () => {
         datasetImportPage.goToPublishedResources();
     });
 
+    // const getRowObject = (rowIndex) => {
+    //     return cy
+    //         .get(`[role='cell'][data-rowindex='${rowIndex}']`)
+    //         .then((values) => {
+    //             let rowObject = {};
+    //             values.map((i, elem) => {
+    //                 rowObject[headers[i]] = Cypress.$(elem).text();
+    //             });
+    //             return rowObject;
+    //         });
+    // };
+
     describe('createAnnotation', () => {
-        it('should create an annotation', () => {
+        it.only('should create an annotation on resource field', () => {
+            cy.findByText('Search').click();
+            searchDrawer.search('Terminator 2');
+            searchDrawer.waitForLoading();
+            cy.findAllByText('Terminator 2').eq(1).click();
             annotation.createAnnotation({
-                fieldLabel: 'Nombre de films',
+                fieldLabel: 'actors',
                 comment: 'This is a comment',
+            });
+            cy.findByText('Search').click();
+            searchDrawer.search('RoboCop');
+            searchDrawer.waitForLoading();
+            cy.findAllByText('RoboCop').eq(0).click();
+            annotation.createAnnotation({
+                fieldLabel: 'rating',
+                comment: 'This is another comment',
+            });
+            cy.findByText('More').click();
+            menu.goToAdminDashboard();
+            cy.findByText('Annotations').click();
+
+            cy.findAllByRole('columnheader').then((headers) => {
+                expect(
+                    headers.toArray().map((header) => header.textContent),
+                ).to.deep.equal(['Comment', 'Resource', 'Submission date']);
+            });
+
+            cy.findAllByRole('cell').then((cells) => {
+                expect(
+                    cells.toArray().map((cell) => cell.textContent),
+                ).to.deep.equal([
+                    'This is another comment',
+                    'RoboCop',
+                    '1/24/2025',
+                    'This is a comment',
+                    'Terminator 2',
+                    '1/24/2025',
+                ]);
             });
         });
 
