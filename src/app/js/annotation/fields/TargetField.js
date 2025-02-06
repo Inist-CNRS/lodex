9c454524a9c45df8bd5f668be3a84339fb375d10
ThieryMichel <thiery@marmelab.com>
2025-02-06 15:40:31 +0100

    2485: better style on TargetField

--- src/app/js/annotation/fields/TargetField.js
+++ src/app/js/annotation/fields/TargetField.js
@@ -1,12 +1,14 @@
-import { MenuItem, MenuList } from '@mui/material';
+import { MenuItem, MenuList, useTheme } from '@mui/material';
 import PropTypes from 'prop-types';
 import React from 'react';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 import { useTranslate } from '../../i18n/I18NContext';
 import { COMMENT_STEP, VALUE_STEP } from '../steps';
 import { useField } from '@tanstack/react-form';
 
 export function TargetField({ form, initialValue, goToStep }) {
+    const theme = useTheme();
     const { translate } = useTranslate();
 
     const initialValueField = useField({
@@ -20,8 +22,20 @@ export function TargetField({ form, initialValue, goToStep }) {
         <form.Field name="target">
             {(field) => {
                 return (
-                    <MenuList>
+                    <MenuList
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: 2,
+                        }}
+                    >
                         <MenuItem
+                            sx={{
+                                border: `1px solid ${theme.palette.primary.main}`,
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                fontWeight: 'bold',
+                            }}
                             onClick={() => {
                                 field.handleChange('title');
                                 initialValueField.handleChange(null);
@@ -29,8 +43,18 @@ export function TargetField({ form, initialValue, goToStep }) {
                             }}
                         >
                             {translate('annotation_comment_target_title')}
+                            <ArrowForwardIosIcon
+                                color={theme.palette.primary.main}
+                                htmlColor={theme.palette.primary.main}
+                            />
                         </MenuItem>
                         <MenuItem
+                            sx={{
+                                border: `1px solid ${theme.palette.primary.main}`,
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                fontWeight: 'bold',
+                            }}
                             onClick={() => {
                                 field.handleChange('value');
                                 if (isList) {
@@ -43,6 +67,10 @@ export function TargetField({ form, initialValue, goToStep }) {
                             }}
                         >
                             {translate('annotation_comment_target_value')}
+                            <ArrowForwardIosIcon
+                                color={theme.palette.primary.main}
+                                htmlColor={theme.palette.primary.main}
+                            />
                         </MenuItem>
                     </MenuList>
                 );
