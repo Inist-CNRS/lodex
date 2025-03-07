ac21fea659c87dcd10c668e6662baa02205720be
ThieryMichel <thiery@marmelab.com>
2025-03-07 16:41:14 +0100

    2602: replace tooltip by title in annotation list

--- src/app/js/admin/annotations/CellWithTooltip.js
+++ src/app/js/admin/annotations/CellWithTooltip.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Tooltip, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 
 export const CellWithTooltip = ({ value }) => {
     return (
-        <Tooltip title={value}>
-            <Typography
-                sx={{
-                    textOverflow: 'ellipsis',
-                    overflow: 'hidden',
-                }}
-            >
-                {value}
-            </Typography>
-        </Tooltip>
+        <Typography
+            title={value}
+            sx={{
+                textOverflow: 'ellipsis',
+                overflow: 'hidden',
+            }}
+        >
+            {value}
+        </Typography>
     );
 };
 
