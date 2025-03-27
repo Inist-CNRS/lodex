0013ba6e97bb8603d5bfb0ab4e39b295cdf1791c
Nicolas Thouvenin <nthouvenin@gmail.com>
2025-03-27 17:54:07 +0100

    a little bit more secure

--- src/app/js/formats/text/html/HtmlView.js
+++ src/app/js/formats/text/html/HtmlView.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import DOMPurify from "dompurify";
 import { field as fieldPropTypes } from '../../../propTypes';
 
-const HtmlView = ({ className, resource, field }) => (
-    <div
-        className={className}
-        dangerouslySetInnerHTML={{ __html: resource[field.name] }}
-    />
-);
+const HtmlView = ({ className, resource, field }) => {
+    const sanitizedHTML = DOMPurify.sanitize(resource[field.name]);
+    return (
+        <div
+            className={className}
+            dangerouslySetInnerHTML={{ __html: sanitizedHTML }}
+        />
+    )
+};
+
 
 HtmlView.propTypes = {
     className: PropTypes.string,
