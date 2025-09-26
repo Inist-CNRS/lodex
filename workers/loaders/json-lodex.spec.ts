b3798d13696fa985285267ca4a610c0775d9f76b
Matthieu Hochlander <matthieu@marmelab.com>
2025-09-26 17:51:51 +0200

    Migrate workers directory to typescript.

--- workers/loaders/json-lodex.spec.ts
+++ workers/loaders/json-lodex.spec.ts
@@ -2,16 +2,19 @@ const ezs = require('@ezs/core');
 const from = require('from');
 
 describe('json-lodex.ini', () => {
-    it('should parse a JSON', (done) => {
+    it('should parse a JSON', done => {
+        // @ts-expect-error TS(7034) FIXME: Variable 'res' implicitly has type 'any[]' in some... Remove this comment to see the full error message
         const res = [];
         const expected = [{ a: '1', b: '2' }];
         const input = { data: expected };
         from([JSON.stringify(input)])
             .pipe(ezs('delegate', { file: __dirname + '/json-lodex.ini' }))
-            .on('data', (chunk) => {
+            // @ts-expect-error TS(7006) FIXME: Parameter 'chunk' implicitly has an 'any' type.
+            .on('data', chunk => {
                 res.push(chunk);
             })
             .on('end', () => {
+                // @ts-expect-error TS(2552) FIXME: Cannot find name 'expect'. Did you mean 'expected'... Remove this comment to see the full error message
                 expect(res).toEqual(expected);
                 done();
             });
