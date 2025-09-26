4ed90792f3df49a8dcf3c811192022d8c146283f
ThieryMichel <thiery@marmelab.com>
2025-09-26 16:34:27 +0200

    fix after rebase

--- src/api/services/precomputed/precomputed.ts
+++ src/api/services/precomputed/precomputed.ts
@@ -9,6 +9,7 @@ import fetch from 'fetch-with-proxy';
 import path from 'path';
 import { tmpdir } from 'os';
 import { createReadStream } from 'fs';
+// @ts-expect-error TS(2792): Cannot find module 'stream-to-promise'. Did you me... Remove this comment to see the full error message
 import ezs from '@ezs/core';
 // @ts-expect-error TS(2792): Cannot find module '@ezs/lodex'. Did you mean to s... Remove this comment to see the full error message
 import Lodex from '@ezs/lodex';
