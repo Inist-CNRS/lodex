a675956bb8e8808c49fb12e4e74af18ec8b44ec0
Matthieu Hochlander <matthieu@marmelab.com>
2025-09-26 17:51:51 +0200

    Migrate workers directory to typescript.

--- workers/loaders/json-conditor.spec.ts
+++ workers/loaders/json-conditor.spec.ts
@@ -2,7 +2,8 @@ const ezs = require('@ezs/core');
 const from = require('from');
 
 describe('json-conditor.ini', () => {
-    it('should parse a JSON', (done) => {
+    it('should parse a JSON', done => {
+        // @ts-expect-error TS(7034) FIXME: Variable 'res' implicitly has type 'any[]' in some... Remove this comment to see the full error message
         const res = [];
         const expected = [
             {
@@ -303,10 +304,12 @@ describe('json-conditor.ini', () => {
         ];
         from([JSON.stringify(input)])
             .pipe(ezs('delegate', { file: __dirname + '/json-conditor.ini' }))
-            .on('data', (chunk) => {
+            // @ts-expect-error TS(7006) FIXME: Parameter 'chunk' implicitly has an 'any' type.
+            .on('data', chunk => {
                 res.push(chunk);
             })
             .on('end', () => {
+                // @ts-expect-error TS(2552) FIXME: Cannot find name 'expect'. Did you mean 'expected'... Remove this comment to see the full error message
                 expect(res).toEqual(expected);
                 done();
             });
