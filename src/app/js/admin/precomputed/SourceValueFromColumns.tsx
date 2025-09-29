457a73a6e7ba98b3fa6e19920dce2a633adcb9c3
ThieryMichel <thiery@marmelab.com>
2025-09-29 16:43:27 +0200

    refactor PrcomputedForm to use react-hook-form

--- src/app/js/admin/precomputed/SourceValueFromColumns.tsx
+++ src/app/js/admin/precomputed/SourceValueFromColumns.tsx
@@ -1,33 +1,47 @@
 import React, { useEffect } from 'react';
-import { formField as formFieldPropTypes } from '../../propTypes';
 import {
     Autocomplete,
     FormControl,
     FormHelperText,
     TextField,
 } from '@mui/material';
+import { useController, type Control } from 'react-hook-form';
+
+type SourceValueFromColumnsProps = {
+    name: string;
+    label: string;
+    options: string[];
+    control: Control<any>;
+    validate: (value: unknown) => string | undefined;
+};
 
 const SourceValueFromColumns = ({
-    // @ts-expect-error TS7031
-    input,
-    // @ts-expect-error TS7031
-    meta: { touched, error, dirty },
-    // @ts-expect-error TS7031
+    name,
     label,
+    options,
+    control,
+    validate,
     ...props
-}) => {
+}: SourceValueFromColumnsProps) => {
+    const { field, fieldState } = useController({
+        name,
+        control,
+        rules: {
+            validate,
+        },
+    });
+    const { error, isDirty, isTouched } = fieldState;
     const [autocompleteValue, setAutocompleteValue] = React.useState(
-        input.value,
+        field.value,
     );
     useEffect(() => {
-        setAutocompleteValue(input.value);
-    }, [input.value]);
+        setAutocompleteValue(field.value);
+    }, [field.value]);
 
     return (
         <FormControl fullWidth error={!!error}>
             <Autocomplete
                 data-testid="source-value-from-columns"
-                // @ts-expect-error TS2322
                 multiple
                 disableCloseOnSelect
                 value={autocompleteValue || []}
@@ -37,26 +51,22 @@ const SourceValueFromColumns = ({
                             placeholder={`${label} *`}
                             label={`${label} *`}
                             {...params}
-                            error={(dirty || touched) && !!error}
+                            error={(isDirty || isTouched) && !!error}
                             variant="outlined"
                             aria-label="input-path"
                         />
                     );
                 }}
-                // @ts-expect-error TS6133
-                onChange={(event, newValue) => {
-                    setAutocompleteValue(newValue);
-                    input.onChange(newValue);
-                }}
+                options={options || []}
                 {...props}
+                onChange={(_event, newValue) => {
+                    setAutocompleteValue(newValue);
+                    field.onChange(newValue);
+                }}
             />
-            {(dirty || touched) && error && (
-                <FormHelperText error>{error}</FormHelperText>
-            )}
+            {error && <FormHelperText error>{error.message}</FormHelperText>}
         </FormControl>
     );
 };
-
-SourceValueFromColumns.propTypes = formFieldPropTypes;
 
 export default SourceValueFromColumns;
