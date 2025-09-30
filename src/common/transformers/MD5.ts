deaf79b91dd7b0c084e0659e90372aec14a542b8
Matthieu Hochlander <matthieu@marmelab.com>
2025-09-30 16:01:38 +0200

    Fix require imports and apply prettier.

--- src/common/transformers/MD5.ts
+++ src/common/transformers/MD5.ts
@@ -1,7 +1,8 @@
 // @ts-expect-error TS(2792): Cannot find module 'md5'. Did you mean to set the ... Remove this comment to see the full error message
 import md5 from 'md5';
 
-const MD5 = () => (value: any) => Array.isArray(value) ? value.map(md5) : md5(value);
+const MD5 = () => (value: any) =>
+    Array.isArray(value) ? value.map(md5) : md5(value);
 
 MD5.getMetas = () => ({
     name: 'MD5',
