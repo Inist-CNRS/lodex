aa3bfbc41b1690bb43331dc4dce3d57496b99ae0
Matthieu Hochlander <matthieu@marmelab.com>
2025-09-30 12:28:29 +0200

    Revert "Merge pull request #2916 from Inist-CNRS/typescript/workers"
    This reverts commit 81ff1a36b02bdc2443e8527cef46246e5be451dd, reversing
    changes made to d2678b2b80ae008f1e97757bd58b9b4b0284d60c.

--- workers/loaders/atom.spec.js
+++ workers/loaders/atom.spec.js
@@ -2,19 +2,16 @@ const ezs = require('@ezs/core');
 const from = require('from');
 
 describe('atom.ini', () => {
-    it('should parse an ATOM XML feed', done => {
-        // @ts-expect-error TS(7034) FIXME: Variable 'res' implicitly has type 'any[]' in some... Remove this comment to see the full error message
+    it('should parse an ATOM XML feed', (done) => {
         const res = [];
         from([
             `<feed><entry><any>value</any><other>thing</other></entry></feed>`,
         ])
             .pipe(ezs('delegate', { file: __dirname + '/atom.ini' }))
-            // @ts-expect-error TS(7006) FIXME: Parameter 'chunk' implicitly has an 'any' type.
-            .on('data', chunk => {
+            .on('data', (chunk) => {
                 res.push(chunk);
             })
             .on('end', () => {
-                // @ts-expect-error TS(2304) FIXME: Cannot find name 'expect'.
                 expect(res).toEqual([ { 'any/$t': 'value', 'other/$t': 'thing' } ]); // eslint-disable-line
                 done();
             });
--- workers/loaders/rss.spec.js
+++ workers/loaders/rss.spec.js
@@ -2,19 +2,16 @@ const ezs = require('@ezs/core');
 const from = require('from');
 
 describe('rss.ini', () => {
-    it('should parse a RSS XML', done => {
-        // @ts-expect-error TS(7034) FIXME: Variable 'res' implicitly has type 'any[]' in some... Remove this comment to see the full error message
+    it('should parse a RSS XML', (done) => {
         const res = [];
         from([
             `<rss><channel><item><any>value</any><other>thing</other></item></channel></rss>`,
         ])
             .pipe(ezs('delegate', { file: __dirname + '/rss.ini' }))
-            // @ts-expect-error TS(7006) FIXME: Parameter 'chunk' implicitly has an 'any' type.
-            .on('data', chunk => {
+            .on('data', (chunk) => {
                 res.push(chunk);
             })
             .on('end', () => {
-                // @ts-expect-error TS(2304) FIXME: Cannot find name 'expect'.
                 expect(res).toEqual([ { 'any/$t': 'value', 'other/$t': 'thing' } ]); // eslint-disable-line
                 done();
             });
