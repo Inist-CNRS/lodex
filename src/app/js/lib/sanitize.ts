4a36a53d44f0925e8f8a1862e54e5ff74cdcfea5
ThieryMichel <thiery@marmelab.com>
2025-10-02 10:15:30 +0200

    handle non string value in sanitize

--- src/app/js/lib/sanitize.ts
+++ src/app/js/lib/sanitize.ts
@@ -1,13 +1,11 @@
 import DOMPurify from 'dompurify';
 
-export const sanitize = (
-    dirty: string | null | undefined,
-    defaultValue = '',
-): string => {
-    if ([null, undefined, ''].includes(dirty)) {
+export const sanitize = (dirty: unknown, defaultValue = ''): string => {
+    if ([null, undefined, ''].includes(dirty?.toString())) {
         return defaultValue;
     }
-    return DOMPurify.sanitize(dirty as string, {
+
+    return DOMPurify.sanitize(dirty!.toString(), {
         ALLOWED_ATTR: [],
         ALLOWED_TAGS: [],
     });
