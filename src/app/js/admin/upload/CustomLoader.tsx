71fe59dace50c487f56444c7bab6304776550363
ThieryMichel <thiery@marmelab.com>
2025-10-02 16:38:23 +0200

    convert CustomLoader form to react-hook-form

--- src/app/js/admin/upload/CustomLoader.tsx
+++ src/app/js/admin/upload/CustomLoader.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import compose from 'recompose/compose';
-import PropTypes from 'prop-types';
 import FormSourceCodeField from '../../lib/components/FormSourceCodeField';
 import {
     Box,
@@ -11,7 +10,7 @@ import {
     DialogContent,
     DialogTitle,
 } from '@mui/material';
-import { Field, formValueSelector, reduxForm } from 'redux-form';
+import { useForm, Controller } from 'react-hook-form';
 import { connect } from 'react-redux';
 import { fromUpload } from '../selectors';
 import {
@@ -19,32 +18,49 @@ import {
     deleteCustomLoader,
     upsertCustomLoader,
 } from './index';
-import { polyglot as polyglotPropTypes } from '../../propTypes';
 import loaderApi from '../api/loader';
 import CancelButton from '../../lib/components/CancelButton';
 import { translate } from '../../i18n/I18NContext';
+import type { State } from '../reducers';
+
+type CustomLoaderFormData = {
+    customLoader: string;
+};
+
+type CustomLoaderProps = {
+    handleClose: () => void;
+    isOpen: boolean;
+    loaderName: string;
+    onDeleteCustomLoader: () => void;
+    onUpsertCustomLoader: (loader: string) => void;
+    onChangeLoaderName: (name: string) => void;
+    initialValues: { customLoader: string };
+    p: {
+        t: (key: string) => string;
+    };
+};
 
 const CustomLoader = ({
-    // @ts-expect-error TS7031
-    formValue,
-    // @ts-expect-error TS7031
     handleClose,
-    // @ts-expect-error TS7031
     isOpen,
-    // @ts-expect-error TS7031
     loaderName,
-    // @ts-expect-error TS7031
     onDeleteCustomLoader,
-    // @ts-expect-error TS7031
     onUpsertCustomLoader,
-    // @ts-expect-error TS7031
     onChangeLoaderName,
-    // @ts-expect-error TS7031
+    initialValues,
     p: polyglot,
-}) => {
-    const [isLoading, setIsLoading] = React.useState(false);
+}: CustomLoaderProps) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { control, setValue, getValues } = useForm<CustomLoaderFormData>({
+        defaultValues: {
+            customLoader: initialValues?.customLoader || '',
+        },
+        mode: 'onChange',
+    });
 
     const handleSave = () => {
+        const formValue = getValues('customLoader');
         onUpsertCustomLoader(formValue);
         onChangeLoaderName('custom-loader');
         handleClose();
@@ -62,7 +78,7 @@ const CustomLoader = ({
             const res = await loaderApi.getLoaderWithScript({
                 name: loaderName,
             });
-            onUpsertCustomLoader(res.script);
+            setValue('customLoader', res.script);
             setIsLoading(false);
         };
 
@@ -70,7 +86,7 @@ const CustomLoader = ({
             return;
         }
         if (isOpen) fetchLoaderWithScript();
-    }, [isOpen, loaderName]);
+    }, [isOpen, loaderName, setValue]);
 
     return (
         <Dialog open={isOpen} onClose={handleClose} scroll="body" maxWidth="xl">
@@ -80,11 +96,18 @@ const CustomLoader = ({
                     {isLoading ? (
                         <CircularProgress />
                     ) : (
-                        <Field
+                        <Controller
                             name="customLoader"
-                            component={FormSourceCodeField}
-                            label={polyglot.t('expand_rules')}
-                            height="350px"
+                            control={control}
+                            render={({ field }) => (
+                                <FormSourceCodeField
+                                    input={field}
+                                    label={polyglot.t('expand_rules')}
+                                    height="350px"
+                                    p={polyglot}
+                                    dispatch={() => {}}
+                                />
+                            )}
                         />
                     )}
                 </Box>
@@ -114,11 +137,7 @@ const CustomLoader = ({
     );
 };
 
-const formSelector = formValueSelector('CUSTOM_LOADER_FORM');
-
-// @ts-expect-error TS7006
-const mapStateToProps = (state) => ({
-    formValue: formSelector(state, 'customLoader'),
+const mapStateToProps = (state: State) => ({
     initialValues: { customLoader: fromUpload.getCustomLoader(state) },
     loaderName: fromUpload.getLoaderName(state),
 });
@@ -129,23 +148,7 @@ const mapDispatchToProps = {
     onDeleteCustomLoader: deleteCustomLoader,
 };
 
-CustomLoader.propTypes = {
-    formValue: PropTypes.string,
-    handleClose: PropTypes.func.isRequired,
-    isOpen: PropTypes.bool.isRequired,
-    loaderName: PropTypes.string.isRequired,
-    onUpsertCustomLoader: PropTypes.func.isRequired,
-    onDeleteCustomLoader: PropTypes.func.isRequired,
-    onChangeLoaderName: PropTypes.func.isRequired,
-    p: polyglotPropTypes.isRequired,
-};
-
-export default compose(
+export default compose<CustomLoaderProps, CustomLoaderProps>(
     translate,
     connect(mapStateToProps, mapDispatchToProps),
-    reduxForm({
-        form: 'CUSTOM_LOADER_FORM',
-        enableReinitialize: true,
-    }),
-    // @ts-expect-error TS2345
 )(CustomLoader);
