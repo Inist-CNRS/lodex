1e2e688c7eece63c6e02459f674812e01200669c
ThieryMichel <thiery@marmelab.com>
2025-10-03 13:44:52 +0200

    type ListDialog

--- src/app/js/admin/upload/ListDialog.tsx
+++ src/app/js/admin/upload/ListDialog.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, type ReactNode, type Ref } from 'react';
 import PropTypes from 'prop-types';
 import {
     Button,
@@ -13,8 +13,7 @@ import {
 } from '@mui/material';
 import FilterIcon from '@mui/icons-material/FilterList';
 
-import { polyglot as polyglotPropTypes } from '../../propTypes';
-import { translate } from '../../i18n/I18NContext';
+import { useTranslate } from '../../i18n/I18NContext';
 
 const styles = {
     item: {
@@ -32,114 +31,26 @@ const styles = {
     },
 };
 
-export const ListDialogComponent = ({
-    // @ts-expect-error TS7031
-    p: polyglot,
-    // @ts-expect-error TS7031
-    loaders,
-    // @ts-expect-error TS7031
-    value,
-    // @ts-expect-error TS7031
-    setLoader,
-    // @ts-expect-error TS7031
-    open,
-    // @ts-expect-error TS7031
-    handleClose,
-    // @ts-expect-error TS7031
-    actions,
-}) => {
-    // @ts-expect-error TS7006
-    const [filteredLoaders, setFilter] = useState(loaders.map((l) => l.name));
+type ListItemComponentProps = {
+    selected: boolean;
+    comment: string;
+    title: string;
+    value: string;
+    changeValue: (value: string) => void;
+};
 
-    // @ts-expect-error TS7006
-    const scrollTo = (el) => {
+const ListItemComponent = ({
+    value,
+    title,
+    comment,
+    selected,
+    changeValue,
+}: ListItemComponentProps) => {
+    const scrollTo: Ref<HTMLLIElement> = (el) => {
         if (el) {
             el.scrollIntoView({ inline: 'center', block: 'center' });
         }
     };
-
-    useEffect(() => {
-        // @ts-expect-error TS7006
-        setFilter(loaders.map((l) => l.name));
-    }, [loaders]);
-
-    // @ts-expect-error TS7006
-    const changeValue = (newValue) => {
-        setLoader(newValue);
-        handleClose();
-    };
-
-    const loaderNames = loaders
-        // @ts-expect-error TS7006
-        .map((loader) => loader.name)
-        // @ts-expect-error TS7006
-        .filter((loader) => filteredLoaders.includes(loader))
-        // @ts-expect-error TS7006
-        .sort((x, y) => polyglot.t(x).localeCompare(polyglot.t(y)))
-        // @ts-expect-error TS7006
-        .map((pn) => (
-            <ListItemComponent
-                key={pn}
-                value={pn}
-                title={polyglot.t(pn)}
-                comment={polyglot.t(`${pn}-comment`)}
-                selected={value === pn}
-                changeValue={changeValue}
-                scrollTo={scrollTo}
-            />
-        ));
-
-    return (
-        <Dialog open={open} onClose={handleClose} scroll="body" maxWidth="xl">
-            <DialogContent sx={{ padding: 0, width: '1100px' }}>
-                <FilterComponent
-                    p={polyglot}
-                    loaders={loaders}
-                    filter={filteredLoaders}
-                    setFilter={setFilter}
-                />
-                <List sx={{ height: '70vh' }}>
-                    <ListItemComponent
-                        key={'automatic'}
-                        value={'automatic'}
-                        title={polyglot.t('automatic-loader')}
-                        selected={value === 'automatic'}
-                        comment={polyglot.t('automatic-loader-comment')}
-                        changeValue={changeValue}
-                        scrollTo={scrollTo}
-                    />
-                    {loaderNames}
-                </List>
-            </DialogContent>
-            <DialogActions>{actions}</DialogActions>
-        </Dialog>
-    );
-};
-
-ListDialogComponent.propTypes = {
-    p: polyglotPropTypes.isRequired,
-    loaders: PropTypes.array,
-    setLoader: PropTypes.func.isRequired,
-    value: PropTypes.string.isRequired,
-    open: PropTypes.bool.isRequired,
-    handleClose: PropTypes.func.isRequired,
-    actions: PropTypes.node,
-};
-
-const ListItemComponent = ({
-    // @ts-expect-error TS7031
-    value,
-    // @ts-expect-error TS7031
-    title,
-    // @ts-expect-error TS7031
-    comment,
-    // @ts-expect-error TS7031
-    selected,
-    // @ts-expect-error TS7031
-    changeValue,
-    // @ts-expect-error TS7031
-    scrollTo,
-}) => {
     return (
         <ListItem
             value={value}
@@ -161,60 +72,53 @@ const ListItemComponent = ({
     );
 };
 
-ListItemComponent.propTypes = {
-    selected: PropTypes.bool,
-    comment: PropTypes.string,
-    title: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    changeValue: PropTypes.func.isRequired,
-    scrollTo: PropTypes.func.isRequired,
+type FilterComponentProps = {
+    filter: string[];
+    setFilter: (value: string[]) => void;
+    loaders?: Loader[];
 };
 
-// @ts-expect-error TS7031
-const FilterComponent = ({ loaders, filter, setFilter, p: polyglot }) => {
-    const allFilters = {
-        // @ts-expect-error TS7006
-        allFormat: loaders.map((l) => l.name),
-        csvFormat: loaders
-            // @ts-expect-error TS7006
-            .filter((l) => l.name.startsWith('csv'))
-            // @ts-expect-error TS7006
-            .map((l) => l.name),
-        tsvFormat: loaders
-            // @ts-expect-error TS7006
-            .filter((l) => l.name.startsWith('tsv'))
-            // @ts-expect-error TS7006
-            .map((l) => l.name),
-        xmlFormat: loaders
-            // @ts-expect-error TS7006
-            .filter((l) =>
-                [
-                    'xml',
-                    'rss',
-                    'atom',
-                    'mods',
-                    'tei',
-                    'tei-persee',
-                    'skos',
-                ].includes(l.name),
-            )
-            // @ts-expect-error TS7006
-            .map((l) => l.name),
-        jsonFormat: loaders
-            // @ts-expect-error TS7006
-            .filter((l) => l.name.startsWith('json'))
-            // @ts-expect-error TS7006
-            .map((l) => l.name),
-    };
-    // @ts-expect-error TS2339
-    allFilters.otherFormat = allFilters.allFormat.filter(
-        // @ts-expect-error TS7006
-        (l) =>
-            !allFilters.csvFormat.includes(l) &&
-            !allFilters.tsvFormat.includes(l) &&
-            !allFilters.xmlFormat.includes(l) &&
-            !allFilters.jsonFormat.includes(l),
+type FormatCategory =
+    | 'allFormat'
+    | 'csvFormat'
+    | 'tsvFormat'
+    | 'xmlFormat'
+    | 'jsonFormat'
+    | 'otherFormat';
+
+const FilterComponent = ({
+    loaders = [],
+    filter,
+    setFilter,
+}: FilterComponentProps) => {
+    const { translate } = useTranslate();
+
+    const allFormat = loaders.map((l) => l.name);
+
+    const csvFormat = allFormat.filter((name) => name.startsWith('csv'));
+    const tsvFormat = allFormat.filter((name) => name.startsWith('tsv'));
+    const xmlFormat = allFormat.filter((name) =>
+        ['xml', 'rss', 'atom', 'mods', 'tei', 'tei-persee', 'skos'].includes(
+            name,
+        ),
     );
+    const jsonFormat = allFormat.filter((name) => name.startsWith('json'));
+    const otherFormat = allFormat.filter(
+        (l) =>
+            !csvFormat.includes(l) &&
+            !tsvFormat.includes(l) &&
+            !xmlFormat.includes(l) &&
+            !jsonFormat.includes(l),
+    );
+
+    const allFilters: Record<FormatCategory, string[]> = {
+        allFormat,
+        csvFormat,
+        tsvFormat,
+        xmlFormat,
+        jsonFormat,
+        otherFormat,
+    };
 
     return (
         <Grid
@@ -236,17 +140,18 @@ const FilterComponent = ({ loaders, filter, setFilter, p: polyglot }) => {
                 <Box key={key}>
                     <Button
                         variant={
-                            // @ts-expect-error TS7053
-                            allFilters[key].toString() === filter.toString()
+                            allFilters[key as FormatCategory].toString() ===
+                            filter.toString()
                                 ? 'contained'
                                 : 'outlined'
                         }
                         color="primary"
-                        // @ts-expect-error TS7053
-                        onClick={() => setFilter(allFilters[key])}
+                        onClick={() =>
+                            setFilter(allFilters[key as FormatCategory])
+                        }
                         className="format-category"
                     >
-                        {polyglot.t(key)}
+                        {translate(key)}
                     </Button>
                 </Box>
             ))}
@@ -254,15 +159,86 @@ const FilterComponent = ({ loaders, filter, setFilter, p: polyglot }) => {
     );
 };
 
-FilterComponent.propTypes = {
-    filter: PropTypes.array,
-    setFilter: PropTypes.func.isRequired,
-    loaders: PropTypes.array.isRequired,
-    p: polyglotPropTypes.isRequired,
+type Loader = {
+    name: string;
 };
 
-FilterComponent.defaultProps = {
-    loaders: [],
+type ListDialogComponentProps = {
+    loaders: Loader[];
+    value: string;
+    setLoader: (loader: string) => void;
+    open: boolean;
+    handleClose: () => void;
+    actions: ReactNode;
 };
 
-export default translate(ListDialogComponent);
+export const ListDialogComponent = ({
+    loaders,
+    value,
+    setLoader,
+    open,
+    handleClose,
+    actions,
+}: ListDialogComponentProps) => {
+    const { translate } = useTranslate();
+    const [filteredLoaders, setFilter] = useState(loaders.map((l) => l.name));
+
+    useEffect(() => {
+        setFilter(loaders.map((l) => l.name));
+    }, [loaders]);
+
+    const changeValue = (newValue: string) => {
+        setLoader(newValue);
+        handleClose();
+    };
+
+    const loaderNames = loaders
+        .map((loader) => loader.name)
+        .filter((loader) => filteredLoaders.includes(loader))
+        .sort((x, y) => translate(x).localeCompare(translate(y)))
+        .map((pn) => (
+            <ListItemComponent
+                key={pn}
+                value={pn}
+                title={translate(pn)}
+                comment={translate(`${pn}-comment`)}
+                selected={value === pn}
+                changeValue={changeValue}
+            />
+        ));
+
+    return (
+        <Dialog open={open} onClose={handleClose} scroll="body" maxWidth="xl">
+            <DialogContent sx={{ padding: 0, width: '1100px' }}>
+                <FilterComponent
+                    loaders={loaders}
+                    filter={filteredLoaders}
+                    setFilter={setFilter}
+                />
+                <List sx={{ height: '70vh' }}>
+                    <ListItemComponent
+                        key={'automatic'}
+                        value={'automatic'}
+                        title={translate('automatic-loader')}
+                        selected={value === 'automatic'}
+                        comment={translate('automatic-loader-comment')}
+                        changeValue={changeValue}
+                    />
+                    {loaderNames}
+                </List>
+            </DialogContent>
+            <DialogActions>{actions}</DialogActions>
+        </Dialog>
+    );
+};
+
+ListDialogComponent.propTypes = {
+    loaders: PropTypes.array,
+    setLoader: PropTypes.func.isRequired,
+    value: PropTypes.string.isRequired,
+    open: PropTypes.bool.isRequired,
+    handleClose: PropTypes.func.isRequired,
+    actions: PropTypes.node,
+};
+
+export default ListDialogComponent;
