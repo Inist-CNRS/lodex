b4ee51c2dbcb4ff5a0d45a2ebe0bb3fcae1aa867
ThieryMichel <thiery@marmelab.com>
2025-10-07 17:40:31 +0200

    fix test

--- src/app/js/user/LoginForm.tsx
+++ src/app/js/user/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { useTranslate } from '../i18n/I18NContext';
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { TextField } from '../reactHookFormFields/TextField';
 import ButtonWithStatus from '../lib/components/ButtonWithStatus';
 import { CardActions, CardContent } from '@mui/material';
@@ -10,48 +10,51 @@ type LoginFormProps = {
 
 export const LoginFormComponent = ({ onSubmit }: LoginFormProps) => {
     const { translate } = useTranslate();
-    const { handleSubmit, formState } = useForm<{
+    const formMethods = useForm<{
         username: string;
         password: string;
     }>({
         mode: 'onChange',
     });
+    const { handleSubmit, formState } = formMethods;
 
     const { isSubmitting, isValid } = formState;
 
     return (
-        <form id="login_form" onSubmit={handleSubmit(onSubmit)}>
-            <CardContent>
-                <TextField
-                    name="username"
-                    label={translate('Username')}
-                    required
-                    autoFocus
-                    fullWidth
-                    variant="standard"
-                />
-                <TextField
-                    name="password"
-                    label={translate('Password')}
-                    type="password"
-                    required
-                    fullWidth
-                    variant="standard"
-                />
-            </CardContent>
-            <CardActions>
-                {/* 
+        <FormProvider {...formMethods}>
+            <form id="login_form" onSubmit={handleSubmit(onSubmit)}>
+                <CardContent>
+                    <TextField
+                        name="username"
+                        label={translate('Username')}
+                        required
+                        autoFocus
+                        fullWidth
+                        variant="standard"
+                    />
+                    <TextField
+                        name="password"
+                        label={translate('Password')}
+                        type="password"
+                        required
+                        fullWidth
+                        variant="standard"
+                    />
+                </CardContent>
+                <CardActions>
+                    {/* 
                 // @ts-expect-error TS2740 */}
-                <ButtonWithStatus
-                    loading={isSubmitting}
-                    type="submit"
-                    disabled={!isValid || isSubmitting}
-                    color="primary"
-                >
-                    {translate('Sign in')}
-                </ButtonWithStatus>
-            </CardActions>
-        </form>
+                    <ButtonWithStatus
+                        loading={isSubmitting}
+                        type="submit"
+                        disabled={!isValid || isSubmitting}
+                        color="primary"
+                    >
+                        {translate('Sign in')}
+                    </ButtonWithStatus>
+                </CardActions>
+            </form>
+        </FormProvider>
     );
 };
 
