067787a4d4ab884942f0ea7d3ea8166f8a8fc126
ThieryMichel <thiery@marmelab.com>
2025-10-09 14:25:12 +0200

    type SubResourceList

--- src/app/js/admin/subresource/SubresourceList.tsx
+++ src/app/js/admin/subresource/SubresourceList.tsx
@@ -1,10 +1,6 @@
-// @ts-expect-error TS6133
-import React from 'react';
-import compose from 'recompose/compose';
 import AddBoxIcon from '@mui/icons-material/AddBox';
-import PropTypes from 'prop-types';
 
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Box, Button, Tooltip } from '@mui/material';
 import {
     DataGrid,
@@ -15,49 +11,50 @@ import {
 } from '@mui/x-data-grid';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router';
-import { polyglot as polyglotPropTypes } from '../../propTypes';
-import { translate } from '../../i18n/I18NContext';
+import { useTranslate } from '../../i18n/I18NContext';
+import { fromSubresources } from '../selectors';
+import { useRef } from 'react';
 
-// @ts-expect-error TS7031
-export const SubresourceList = ({ subresources, p: polyglot }) => {
+const CustomToolbar = () => {
+    const { translate } = useTranslate();
+    const ref = useRef(null);
+
+    return (
+        <GridToolbarContainer ref={ref}>
+            <Tooltip title={translate(`column_tooltip`)}>
+                <GridToolbarColumnsButton touchRippleRef={ref} />
+            </Tooltip>
+            <GridToolbarFilterButton />
+            <Tooltip title={translate(`density_tooltip`)}>
+                <GridToolbarDensitySelector touchRippleRef={ref} />
+            </Tooltip>
+            <Tooltip title={translate(`add_more_subresource`)}>
+                <Button
+                    component={Link}
+                    to="/display/document/subresource/add"
+                    startIcon={<AddBoxIcon />}
+                    size="small"
+                    sx={{
+                        '&.MuiButtonBase-root:hover': {
+                            color: 'primary.main',
+                        },
+                    }}
+                >
+                    {translate('add_more')}
+                </Button>
+            </Tooltip>
+        </GridToolbarContainer>
+    );
+};
+
+export const SubresourceList = () => {
+    const { translate } = useTranslate();
     const history = useHistory();
-    // @ts-expect-error TS7006
-    const handleRowClick = (params) => {
+    const handleRowClick = (params: { row: { _id: string } }) => {
         history.push(`/display/document/subresource/${params.row._id}`);
     };
 
-    const CustomToolbar = () => {
-        return (
-            <GridToolbarContainer>
-                <Tooltip title={polyglot.t(`column_tooltip`)}>
-                    {/*
-                     // @ts-expect-error TS2741 */}
-                    <GridToolbarColumnsButton />
-                </Tooltip>
-                <GridToolbarFilterButton />
-                <Tooltip title={polyglot.t(`density_tooltip`)}>
-                    {/*
-                     // @ts-expect-error TS2741 */}
-                    <GridToolbarDensitySelector />
-                </Tooltip>
-                <Tooltip title={polyglot.t(`add_more_subresource`)}>
-                    <Button
-                        component={Link}
-                        to="/display/document/subresource/add"
-                        startIcon={<AddBoxIcon />}
-                        size="small"
-                        sx={{
-                            '&.MuiButtonBase-root:hover': {
-                                color: 'primary.main',
-                            },
-                        }}
-                    >
-                        {polyglot.t('add_more')}
-                    </Button>
-                </Tooltip>
-            </GridToolbarContainer>
-        );
-    };
+    const subresources = useSelector(fromSubresources.getSubresources);
 
     return (
         <Box>
@@ -65,12 +62,12 @@ export const SubresourceList = ({ subresources, p: polyglot }) => {
                 columns={[
                     {
                         field: 'name',
-                        headerName: polyglot.t('subresource_name'),
+                        headerName: translate('subresource_name'),
                         flex: 1,
                     },
                     {
                         field: 'path',
-                        headerName: polyglot.t('subresource_path'),
+                        headerName: translate('subresource_path'),
                         flex: 1,
                         renderCell: (params) => {
                             return params.value ? params.value : '-';
@@ -78,7 +75,7 @@ export const SubresourceList = ({ subresources, p: polyglot }) => {
                     },
                     {
                         field: 'identifier',
-                        headerName: polyglot.t('subresource_id'),
+                        headerName: translate('subresource_id'),
                         flex: 1,
                         renderCell: (params) => {
                             return params.value ? params.value : '-';
@@ -88,8 +85,6 @@ export const SubresourceList = ({ subresources, p: polyglot }) => {
                 rows={subresources}
                 getRowId={(row) => row._id}
                 autoHeight
-                // @ts-expect-error TS2322
-                width="100%"
                 onRowClick={handleRowClick}
                 components={{
                     Toolbar: CustomToolbar,
@@ -98,21 +93,11 @@ export const SubresourceList = ({ subresources, p: polyglot }) => {
                     '& .MuiDataGrid-cell:hover': {
                         cursor: 'pointer',
                     },
+                    width: '100%',
                 }}
             />
         </Box>
     );
 };
 
-SubresourceList.propTypes = {
-    subresources: PropTypes.array.isRequired,
-    p: polyglotPropTypes.isRequired,
-};
-
-// @ts-expect-error TS7006
-const mapStateToProps = (state) => ({
-    subresources: state.subresource.subresources,
-});
-
-// @ts-expect-error TS2345
-export default compose(translate, connect(mapStateToProps))(SubresourceList);
+export default SubresourceList;
