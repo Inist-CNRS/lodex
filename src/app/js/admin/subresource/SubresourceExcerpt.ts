2d5f54cae24e8d5a33f81ce9fe18a11810c115e5
ThieryMichel <thiery@marmelab.com>
2025-10-09 14:13:10 +0200

    type SubresourceExcerpt

--- src/app/js/admin/subresource/SubresourceExcerpt.ts
+++ src/app/js/admin/subresource/SubresourceExcerpt.ts
@@ -1,13 +1,14 @@
-// TODO: check if this is still used
 import { connect } from 'react-redux';
 
 import Excerpt from '../preview/Excerpt';
 import { fromPublicationPreview } from '../selectors';
+import type { State } from '../reducers';
 
-// @ts-expect-error TS7006
-const mapStateToProps = (state, { fields }) => ({
+const mapStateToProps = (
+    state: State,
+    { fields }: { fields: { name: string }[] },
+) => ({
     lines: fromPublicationPreview.getPublicationPreview(state),
-    // @ts-expect-error TS7006
     columns: fields.filter((field) => {
         // Remove subresource field uri from editable columns
         return !field.name.endsWith('_uri');
