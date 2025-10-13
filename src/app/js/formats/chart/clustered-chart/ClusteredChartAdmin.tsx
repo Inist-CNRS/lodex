2ce21ff9ac8e3951b65057711a4adaddaa7c7161
ThieryMichel <thiery@marmelab.com>
2025-10-13 17:25:27 +0200

    refactor ClusteredChartAdmin to use new hook

--- src/app/js/formats/chart/clustered-chart/ClusteredChartAdmin.tsx
+++ src/app/js/formats/chart/clustered-chart/ClusteredChartAdmin.tsx
@@ -1,21 +1,27 @@
-// @ts-expect-error TS6133
-import React from 'react';
-import PropTypes from 'prop-types';
 import { Checkbox, FormControlLabel, TextField } from '@mui/material';
 
-import { polyglot as polyglotPropTypes } from '../../../propTypes';
 import ColorPickerParamsAdmin from '../../utils/components/admin/ColorPickerParamsAdmin';
 import { MONOCHROMATIC_DEFAULT_COLORSET } from '../../utils/colorUtils';
-import updateAdminArgs from '../../utils/updateAdminArgs';
-import RoutineParamsAdmin from '../../utils/components/admin/RoutineParamsAdmin';
+import RoutineParamsAdmin, {
+    type RoutineParams,
+} from '../../utils/components/admin/RoutineParamsAdmin';
 import {
     FormatChartParamsFieldSet,
     FormatDataParamsFieldSet,
 } from '../../utils/components/field-set/FormatFieldSets';
 import FormatGroupedFieldSet from '../../utils/components/field-set/FormatGroupedFieldSet';
-import { translate } from '../../../i18n/I18NContext';
+import { useTranslate } from '../../../i18n/I18NContext';
+import { useUpdateAdminArgs } from '../../utils/updateAdminArgs';
 
-export const defaultArgs = {
+type ClusterChartArgs = {
+    params: RoutineParams;
+    colors?: string;
+    xTitle?: string;
+    yTitle?: string;
+    flipAxis?: boolean;
+};
+
+export const defaultArgs: ClusterChartArgs = {
     params: {
         maxSize: 200,
         orderBy: 'value/asc',
@@ -24,41 +30,61 @@ export const defaultArgs = {
     flipAxis: false,
 };
 
-// @ts-expect-error TS7006
-const ClusteredChartAdmin = (props) => {
-    const { args, p } = props;
+type ClusteredChartAdminProps = {
+    args?: ClusterChartArgs;
+    onChange: (args: ClusterChartArgs) => void;
+};
+
+const ClusteredChartAdmin = (props: ClusteredChartAdminProps) => {
+    const { translate } = useTranslate();
+    const { args = defaultArgs, onChange } = props;
     const { params, colors, xTitle, yTitle, flipAxis } = args;
 
-    // @ts-expect-error TS7006
-    const handleParams = (params) => {
-        updateAdminArgs('params', params, props);
-    };
+    const handleParams = useUpdateAdminArgs<ClusterChartArgs, 'params'>(
+        'params',
+        {
+            args,
+            onChange,
+        },
+    );
 
-    // @ts-expect-error TS7006
-    const handleColors = (colors) => {
-        updateAdminArgs('colors', colors || defaultArgs.colors, props);
-    };
+    const handleColors = useUpdateAdminArgs<ClusterChartArgs, 'colors'>(
+        'colors',
+        {
+            args,
+            onChange,
+        },
+    );
 
-    // @ts-expect-error TS7006
-    const handleXAxisTitle = (event) => {
-        updateAdminArgs('xTitle', event.target.value, props);
-    };
+    const handleXAxisTitle = useUpdateAdminArgs<ClusterChartArgs, 'xTitle'>(
+        'xTitle',
+        {
+            args,
+            onChange,
+        },
+    );
 
-    // @ts-expect-error TS7006
-    const handleYAxisTitle = (event) => {
-        updateAdminArgs('yTitle', event.target.value, props);
-    };
+    const handleYAxisTitle = useUpdateAdminArgs<ClusterChartArgs, 'yTitle'>(
+        'yTitle',
+        {
+            args,
+            onChange,
+        },
+    );
 
-    const toggleFlipAxis = () => {
-        updateAdminArgs('flipAxis', !flipAxis, props);
-    };
+    const toggleFlipAxis = useUpdateAdminArgs<ClusterChartArgs, 'flipAxis'>(
+        'flipAxis',
+        {
+            args,
+            onChange,
+        },
+    );
 
     return (
         <FormatGroupedFieldSet>
             <FormatDataParamsFieldSet>
                 <RoutineParamsAdmin
-                    params={params || defaultArgs.params}
-                    polyglot={p}
+                    params={params}
                     onChange={handleParams}
                     showMaxSize
                     showMaxValue
@@ -70,27 +96,26 @@ const ClusteredChartAdmin = (props) => {
                 <FormControlLabel
                     control={
                         <Checkbox
-                            onChange={toggleFlipAxis}
+                            onChange={(e) => toggleFlipAxis(e.target.checked)}
                             checked={flipAxis}
                         />
                     }
-                    label={p.t('flip_axis')}
+                    label={translate('flip_axis')}
                 />
                 <ColorPickerParamsAdmin
                     colors={colors}
                     onChange={handleColors}
-                    polyglot={p}
                 />
                 <TextField
                     fullWidth
-                    label={p.t('format_x_axis_title')}
-                    onChange={handleXAxisTitle}
+                    label={translate('format_x_axis_title')}
+                    onChange={(e) => handleXAxisTitle(e.target.value)}
                     value={xTitle}
                 />
                 <TextField
                     fullWidth
-                    label={p.t('format_y_axis_title')}
-                    onChange={handleYAxisTitle}
+                    label={translate('format_y_axis_title')}
+                    onChange={(e) => handleYAxisTitle(e.target.value)}
                     value={yTitle}
                 />
             </FormatChartParamsFieldSet>
@@ -98,25 +123,4 @@ const ClusteredChartAdmin = (props) => {
     );
 };
 
-ClusteredChartAdmin.defaultProps = {
-    args: defaultArgs,
-};
-
-ClusteredChartAdmin.propTypes = {
-    args: PropTypes.shape({
-        params: PropTypes.shape({
-            maxSize: PropTypes.number,
-            maxValue: PropTypes.number,
-            minValue: PropTypes.number,
-            orderBy: PropTypes.string,
-        }),
-        colors: PropTypes.string,
-        xTitle: PropTypes.string,
-        yTitle: PropTypes.string,
-        flipAxis: PropTypes.bool,
-    }),
-    onChange: PropTypes.func.isRequired,
-    p: polyglotPropTypes.isRequired,
-};
-
-export default translate(ClusteredChartAdmin);
+export default ClusteredChartAdmin;
