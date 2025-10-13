75c2de7609d15783a166c8636a4f75aef21a1668
ThieryMichel <thiery@marmelab.com>
2025-10-13 18:05:09 +0200

    refactor SteamGraphAdmin to use new hook

--- src/app/js/formats/chart/streamgraph/StreamgraphAdmin.tsx
+++ src/app/js/formats/chart/streamgraph/StreamgraphAdmin.tsx
@@ -1,11 +1,7 @@
-// @ts-expect-error TS6133
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { TextField } from '@mui/material';
-import { translate } from '../../../i18n/I18NContext';
+import { useTranslate } from '../../../i18n/I18NContext';
 
-import { polyglot as polyglotPropTypes } from '../../../propTypes';
-import updateAdminArgs from '../../utils/updateAdminArgs';
+import { useUpdateAdminArgs } from '../../utils/updateAdminArgs';
 import RoutineParamsAdmin from '../../utils/components/admin/RoutineParamsAdmin';
 import ColorPickerParamsAdmin from '../../utils/components/admin/ColorPickerParamsAdmin';
 import { MULTICHROMATIC_DEFAULT_COLORSET_STREAMGRAPH } from '../../utils/colorUtils';
@@ -15,125 +11,134 @@ import {
 } from '../../utils/components/field-set/FormatFieldSets';
 import FormatGroupedFieldSet from '../../utils/components/field-set/FormatGroupedFieldSet';
 
+type StreamgraphArgs = {
+    params?: {
+        maxSize?: number;
+        maxValue?: number;
+        minValue?: number;
+        orderBy?: string;
+        uri?: string;
+    };
+    colors?: string;
+    maxLegendLength?: number;
+    height?: number;
+};
+
 export const defaultArgs = {
     params: {
         maxSize: 200,
         orderBy: 'value/desc',
+        maxValue: undefined,
+        minValue: undefined,
+        uri: undefined,
     },
     colors: MULTICHROMATIC_DEFAULT_COLORSET_STREAMGRAPH,
     maxLegendLength: 30,
     height: 300,
 };
 
-class StreamgraphAdmin extends Component {
-    static propTypes = {
-        args: PropTypes.shape({
-            params: PropTypes.shape({
-                maxSize: PropTypes.number,
-                maxValue: PropTypes.number,
-                minValue: PropTypes.number,
-                orderBy: PropTypes.string,
-            }),
-            colors: PropTypes.string,
-            maxLegendLength: PropTypes.number,
-            height: PropTypes.number.isRequired,
-        }),
-        onChange: PropTypes.func.isRequired,
-        p: polyglotPropTypes.isRequired,
-        showMaxSize: PropTypes.bool.isRequired,
-        showMaxValue: PropTypes.bool.isRequired,
-        showMinValue: PropTypes.bool.isRequired,
-        showOrderBy: PropTypes.bool.isRequired,
+type StreamgraphAdminProps = {
+    args?: StreamgraphArgs;
+    onChange: (args: StreamgraphArgs) => void;
+    showMaxSize?: boolean;
+    showMaxValue?: boolean;
+    showMinValue?: boolean;
+    showOrderBy?: boolean;
+};
+
+const StreamgraphAdmin = ({
+    args = defaultArgs,
+    onChange,
+    showMaxSize = true,
+    showMaxValue = false,
+    showMinValue = false,
+    showOrderBy = true,
+}: StreamgraphAdminProps) => {
+    const { translate } = useTranslate();
+
+    const handleParams = useUpdateAdminArgs<StreamgraphArgs, 'params'>(
+        'params',
+        {
+            args,
+            onChange,
+        },
+    );
+
+    const updateColors = useUpdateAdminArgs<StreamgraphArgs, 'colors'>(
+        'colors',
+        {
+            args,
+            onChange,
+        },
+    );
+
+    const handleColors = (newColors: string) => {
+        const color = newColors || defaultArgs.colors;
+        updateColors(color);
     };
 
-    static defaultProps = {
-        args: defaultArgs,
-        showMaxSize: true,
-        showMaxValue: false,
-        showMinValue: false,
-        showOrderBy: true,
+    const updateMaxLegendLength = useUpdateAdminArgs<
+        StreamgraphArgs,
+        'maxLegendLength'
+    >('maxLegendLength', {
+        args,
+        onChange,
+    });
+
+    const handleMaxLegendLength = (e: React.ChangeEvent<HTMLInputElement>) => {
+        updateMaxLegendLength(parseInt(e.target.value, 10));
     };
 
-    // @ts-expect-error TS7006
-    constructor(props) {
-        super(props);
-        this.handleColors = this.handleColors.bind(this);
-        this.state = {
-            // @ts-expect-error TS2339
-            colors: this.props.args.colors || defaultArgs.colors,
-        };
-    }
+    const updateHeight = useUpdateAdminArgs<StreamgraphArgs, 'height'>(
+        'height',
+        {
+            args,
+            onChange,
+        },
+    );
 
-    // @ts-expect-error TS7006
-    handleParams = (params) => updateAdminArgs('params', params, this.props);
-
-    // @ts-expect-error TS7006
-    handleColors(colors) {
-        updateAdminArgs('colors', colors || defaultArgs.colors, this.props);
-    }
-
-    // @ts-expect-error TS7006
-    handleMaxLegendLength = (e) => {
-        updateAdminArgs('maxLegendLength', e.target.value, this.props);
+    const handleHeight = (e: React.ChangeEvent<HTMLInputElement>) => {
+        updateHeight(parseInt(e.target.value, 10));
     };
 
-    // @ts-expect-error TS7006
-    handleHeight = (e) => {
-        updateAdminArgs('height', e.target.value, this.props);
-    };
+    const { params, maxLegendLength, height } = args;
 
-    render() {
-        const {
-            // @ts-expect-error TS2339
-            p: polyglot,
-            // @ts-expect-error TS2339
-            args: { params, maxLegendLength, height },
-            // @ts-expect-error TS2339
-            showMaxSize,
-            // @ts-expect-error TS2339
-            showMaxValue,
-            // @ts-expect-error TS2339
-            showMinValue,
-            // @ts-expect-error TS2339
-            showOrderBy,
-        } = this.props;
+    return (
+        <FormatGroupedFieldSet>
+            <FormatDataParamsFieldSet>
+                <RoutineParamsAdmin
+                    params={{
+                        ...defaultArgs.params,
+                        ...params,
+                    }}
+                    onChange={handleParams}
+                    showMaxSize={showMaxSize}
+                    showMaxValue={showMaxValue}
+                    showMinValue={showMinValue}
+                    showOrderBy={showOrderBy}
+                    showUri={false}
+                />
+            </FormatDataParamsFieldSet>
+            <FormatChartParamsFieldSet defaultExpanded>
+                <ColorPickerParamsAdmin
+                    colors={args.colors}
+                    onChange={handleColors}
+                />
+                <TextField
+                    label={translate('max_char_number_in_legends')}
+                    onChange={handleMaxLegendLength}
+                    value={maxLegendLength || defaultArgs.maxLegendLength}
+                    fullWidth
+                />
+                <TextField
+                    label={translate('height_px')}
+                    onChange={handleHeight}
+                    value={height || defaultArgs.height}
+                    fullWidth
+                />
+            </FormatChartParamsFieldSet>
+        </FormatGroupedFieldSet>
+    );
+};
 
-        return (
-            <FormatGroupedFieldSet>
-                <FormatDataParamsFieldSet>
-                    <RoutineParamsAdmin
-                        params={params || defaultArgs.params}
-                        polyglot={polyglot}
-                        onChange={this.handleParams}
-                        showMaxSize={showMaxSize}
-                        showMaxValue={showMaxValue}
-                        showMinValue={showMinValue}
-                        showOrderBy={showOrderBy}
-                    />
-                </FormatDataParamsFieldSet>
-                <FormatChartParamsFieldSet defaultExpanded>
-                    <ColorPickerParamsAdmin
-                        // @ts-expect-error TS2339
-                        colors={this.state.colors}
-                        onChange={this.handleColors}
-                        polyglot={polyglot}
-                    />
-                    <TextField
-                        label={polyglot.t('max_char_number_in_legends')}
-                        onChange={this.handleMaxLegendLength}
-                        value={maxLegendLength}
-                        fullWidth
-                    />
-                    <TextField
-                        label={polyglot.t('height_px')}
-                        onChange={this.handleHeight}
-                        value={height}
-                        fullWidth
-                    />
-                </FormatChartParamsFieldSet>
-            </FormatGroupedFieldSet>
-        );
-    }
-}
-
-export default translate(StreamgraphAdmin);
+export default StreamgraphAdmin;
