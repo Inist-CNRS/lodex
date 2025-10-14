1a05a27e87527a67f8467e77d5ba9f56c93fb644
Matthieu Hochlander <matthieu@marmelab.com>
2025-10-14 18:20:06 +0200

    Fix search form inputs not updated properly after mutation.

--- src/app/js/admin/Search/usePatchSortField.ts
+++ src/app/js/admin/Search/usePatchSortField.ts
@@ -2,20 +2,26 @@ import { useMutation } from '@tanstack/react-query';
 import { toast } from '../../../../common/tools/toast';
 import fieldApi from '../../admin/api/field';
 import { useTranslate } from '../../i18n/I18NContext';
+import { useDispatch } from 'react-redux';
+import { changeSortField } from '../../fields';
 
 export function usePatchSortField() {
     const { translate } = useTranslate();
+    const dispatch = useDispatch();
 
     return useMutation({
         mutationFn(payload) {
             return fieldApi.patchSortField(payload);
         },
-        onSuccess(res) {
+        onSuccess(res, payload) {
             if (!res.ok) {
                 toast(translate('syndication_error'), {
                     type: toast.TYPE.ERROR,
                 });
+                return;
             }
+
+            dispatch(changeSortField(payload));
         },
     });
 }
