5c9b2b237b5fb75b055b07b2803824645192e711
ThieryMichel <thiery@marmelab.com>
2025-10-14 16:29:46 +0200

    refactor RadarChartAdmin to use new hook

--- src/app/js/formats/utils/aspectRatio.ts
+++ src/app/js/formats/utils/aspectRatio.ts
@@ -1,11 +1,11 @@
 // You can find aspect ratio preview at: https://www.w3schools.com/howto/howto_css_aspect_ratio.asp
-export const ASPECT_RATIO_1_1 = '1 / 1';
-export const ASPECT_RATIO_3_2 = '3 / 2';
-export const ASPECT_RATIO_4_3 = '4 / 3';
-export const ASPECT_RATIO_8_5 = '8 / 5';
-export const ASPECT_RATIO_16_6 = '16 / 6';
-export const ASPECT_RATIO_16_9 = '16 / 9';
-export const ASPECT_RATIO_NONE = 'none';
+export const ASPECT_RATIO_1_1 = '1 / 1' as const;
+export const ASPECT_RATIO_3_2 = '3 / 2' as const;
+export const ASPECT_RATIO_4_3 = '4 / 3' as const;
+export const ASPECT_RATIO_8_5 = '8 / 5' as const;
+export const ASPECT_RATIO_16_6 = '16 / 6' as const;
+export const ASPECT_RATIO_16_9 = '16 / 9' as const;
+export const ASPECT_RATIO_NONE = 'none' as const;
 
 export const ASPECT_RATIOS = [
     ASPECT_RATIO_1_1,
