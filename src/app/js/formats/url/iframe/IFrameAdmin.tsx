fc744f190ec25f1adb06faa9c224350e45c2ac4e
ThieryMichel <thiery@marmelab.com>
2025-10-14 14:12:03 +0200

    refactor IFrameAdmin to use new hook

--- src/app/js/formats/url/iframe/IFrameAdmin.tsx
+++ src/app/js/formats/url/iframe/IFrameAdmin.tsx
@@ -1,46 +1,51 @@
-// @ts-expect-error TS6133
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { translate } from '../../../i18n/I18NContext';
-import { polyglot as polyglotPropTypes } from '../../../propTypes';
 import { TextField, MenuItem } from '@mui/material';
 import { FormatDefaultParamsFieldSet } from '../../utils/components/field-set/FormatFieldSets';
 import FormatGroupedFieldSet from '../../utils/components/field-set/FormatGroupedFieldSet';
-import updateAdminArgs from '../../utils/updateAdminArgs';
+import { useUpdateAdminArgs } from '../../utils/updateAdminArgs';
 import { ASPECT_RATIO_16_9 } from '../../utils/aspectRatio';
 import AspectRatioSelector from '../../utils/components/admin/AspectRatioSelector';
+import { useTranslate } from '../../../i18n/I18NContext';
 
 export const defaultArgs = {
     viewWidth: '100%',
     aspectRatio: ASPECT_RATIO_16_9,
 };
 
-const IFrameAdmin = ({
-    args = defaultArgs,
-    // @ts-expect-error TS7031
-    onChange,
-    // @ts-expect-error TS7031
-    p: polyglot,
-}) => {
-    const [viewWidth, setViewWidth] = useState(
-        args.viewWidth || defaultArgs.viewWidth,
-    );
-    const [aspectRatio, setAspectRatio] = useState(
-        args.aspectRatio || defaultArgs.aspectRatio,
-    );
+type IFrameArgs = {
+    viewWidth?: string;
+    aspectRatio?: string;
+};
 
-    // @ts-expect-error TS7006
-    const handleViewWidth = (event) => {
-        const newViewWidth = event.target.value;
-        updateAdminArgs('viewWidth', newViewWidth, { args, onChange });
-        setViewWidth(newViewWidth);
-    };
+type IFrameAdminProps = {
+    args?: IFrameArgs;
+    onChange: (args: IFrameArgs) => void;
+};
 
-    // @ts-expect-error TS7006
-    const handleAspectRatio = (newAspectRatio) => {
-        updateAdminArgs('aspectRatio', newAspectRatio, { args, onChange });
-        setAspectRatio(newAspectRatio);
-    };
+const IFrameAdmin = ({ args = defaultArgs, onChange }: IFrameAdminProps) => {
+    const {
+        viewWidth = defaultArgs.viewWidth,
+        aspectRatio = defaultArgs.aspectRatio,
+    } = args;
+    const { translate } = useTranslate();
+
+    const handleViewWidth = useUpdateAdminArgs<
+        IFrameArgs,
+        'viewWidth',
+        React.ChangeEvent<HTMLInputElement>
+    >('viewWidth', {
+        args,
+        onChange,
+        parseValue: (event: React.ChangeEvent<HTMLInputElement>) =>
+            event.target.value,
+    });
+
+    const handleAspectRatio = useUpdateAdminArgs<IFrameArgs, 'aspectRatio'>(
+        'aspectRatio',
+        {
+            args,
+            onChange,
+        },
+    );
 
     return (
         <FormatGroupedFieldSet>
@@ -48,25 +53,25 @@ const IFrameAdmin = ({
                 <TextField
                     fullWidth
                     select
-                    label={polyglot.t('list_format_select_image_width')}
+                    label={translate('list_format_select_image_width')}
                     onChange={handleViewWidth}
                     value={viewWidth}
                 >
-                    <MenuItem value="10%">{polyglot.t('ten_percent')}</MenuItem>
+                    <MenuItem value="10%">{translate('ten_percent')}</MenuItem>
                     <MenuItem value="20%">
-                        {polyglot.t('twenty_percent')}
+                        {translate('twenty_percent')}
                     </MenuItem>
                     <MenuItem value="30%">
-                        {polyglot.t('thirty_percent')}
+                        {translate('thirty_percent')}
                     </MenuItem>
                     <MenuItem value="50%">
-                        {polyglot.t('fifty_percent')}
+                        {translate('fifty_percent')}
                     </MenuItem>
                     <MenuItem value="80%">
-                        {polyglot.t('eighty_percent')}
+                        {translate('eighty_percent')}
                     </MenuItem>
                     <MenuItem value="100%">
-                        {polyglot.t('hundred_percent')}
+                        {translate('hundred_percent')}
                     </MenuItem>
                 </TextField>
                 <AspectRatioSelector
@@ -78,13 +83,4 @@ const IFrameAdmin = ({
     );
 };
 
-IFrameAdmin.propTypes = {
-    args: PropTypes.shape({
-        viewWidth: PropTypes.string,
-        aspectRatio: PropTypes.string,
-    }),
-    onChange: PropTypes.func.isRequired,
-    p: polyglotPropTypes.isRequired,
-};
-
-export default translate(IFrameAdmin);
+export default IFrameAdmin;
