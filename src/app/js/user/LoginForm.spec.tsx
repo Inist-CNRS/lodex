5a0aa825ea85fdc91d884a4d257147c7f988481f
ThieryMichel <thiery@marmelab.com>
2025-10-16 17:11:27 +0200

    refactor login form to handle request success and error without saga

--- src/app/js/user/LoginForm.spec.tsx
+++ src/app/js/user/LoginForm.spec.tsx
@@ -1,47 +1,45 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 
 import { LoginFormComponent } from './LoginForm';
 import { act, fireEvent, render } from '../../../test-utils';
+import { MemoryRouter } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useLogin } from '../api/login';
 
-// Mock store for redux-form
-const store = createStore(() => ({
-    form: formReducer(undefined, { type: '@@INIT' }),
+jest.mock('../api/login', () => ({
+    useLogin: jest
+        .fn()
+        .mockReturnValue({ login: jest.fn(), error: null, isLoading: false }),
 }));
 
 // Wrapper component to provide Redux context
 const TestWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-    <Provider store={store}>{children}</Provider>
+    <QueryClientProvider client={new QueryClient()}>
+        <MemoryRouter>{children}</MemoryRouter>
+    </QueryClientProvider>
 );
 
 describe('<LoginForm />', () => {
-    const defaultProps = {
-        p: { t: (key: string) => key },
-        handleSubmit: jest.fn(),
-        onSubmit: jest.fn(),
-    };
-
     it('should render form', () => {
         const screen = render(
             <TestWrapper>
-                <LoginFormComponent {...defaultProps} />
+                <LoginFormComponent />
             </TestWrapper>,
         );
         expect(screen.getByLabelText('Username *')).toBeInTheDocument();
         expect(screen.getByLabelText('Password *')).toBeInTheDocument();
     });
     it('should enable submit button when form is valid', async () => {
-        const props = {
-            ...defaultProps,
-            onSubmit: jest.fn(),
-        };
-
+        const login = jest.fn();
+        (useLogin as jest.Mock).mockReturnValue({
+            login,
+            error: null,
+            isLoading: false,
+        });
         const screen = render(
             <TestWrapper>
-                <LoginFormComponent {...props} />
+                <LoginFormComponent />
             </TestWrapper>,
         );
 
@@ -64,18 +62,17 @@ describe('<LoginForm />', () => {
         await act(async () => {
             fireEvent.click(screen.getByText('Sign in'));
         });
-        expect(props.onSubmit).toHaveBeenCalledTimes(1);
+        expect(login).toHaveBeenCalledTimes(1);
+        expect(login).toHaveBeenCalledWith({
+            username: 'testuser',
+            password: 'testpassword',
+        });
     });
 
     it('should display error when trying to submit with empty fields', async () => {
-        const propsWithError = {
-            ...defaultProps,
-            onSubmit: jest.fn(),
-        };
-
         const screen = render(
             <TestWrapper>
-                <LoginFormComponent {...propsWithError} />
+                <LoginFormComponent />
             </TestWrapper>,
         );
 
