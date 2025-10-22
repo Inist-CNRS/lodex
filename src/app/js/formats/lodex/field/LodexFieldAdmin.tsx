50280899ede0ee97526f9d83d7e7709ba56c5b66
ThieryMichel <thiery@marmelab.com>
2025-10-22 12:53:23 +0200

    remove defaultProps from class component by converting them to function

--- src/app/js/formats/lodex/field/LodexFieldAdmin.tsx
+++ src/app/js/formats/lodex/field/LodexFieldAdmin.tsx
@@ -1,11 +1,10 @@
-import { Component } from 'react';
-import { translate } from '../../../i18n/I18NContext';
 import { Checkbox, FormControlLabel, TextField } from '@mui/material';
 import FormatGroupedFieldSet from '../../utils/components/field-set/FormatGroupedFieldSet';
 import {
     FormatDataParamsFieldSet,
     FormatDefaultParamsFieldSet,
 } from '../../utils/components/field-set/FormatFieldSets';
+import { useTranslate } from '../../../i18n/I18NContext';
 
 export const defaultArgs = {
     param: {
@@ -21,68 +20,57 @@ interface LodexFieldAdminProps {
         };
     };
     onChange(...args: unknown[]): unknown;
-    p: unknown;
 }
 
-class LodexFieldAdmin extends Component<LodexFieldAdminProps> {
-    static defaultProps = {
-        args: defaultArgs,
-    };
-
+const LodexFieldAdmin = ({
+    args = defaultArgs,
+    onChange,
+}: LodexFieldAdminProps) => {
+    const { translate } = useTranslate();
     // @ts-expect-error TS7006
-    handleRequest = (e) => {
+    const handleRequest = (e) => {
         const labelArray = (e.target.value || '').split(';');
-        // @ts-expect-error TS2339
-        const { param, ...args } = this.props.args;
-        const newArgs = { ...args, param: { ...param, labelArray } };
-        this.props.onChange(newArgs);
+        const { param, ...argsRest } = args;
+        const newArgs = { ...argsRest, param: { ...param, labelArray } };
+        onChange(newArgs);
     };
 
     // @ts-expect-error TS7006
-    handleHiddenInfo = (event) => {
+    const handleHiddenInfo = (event) => {
         const hiddenInfo = event.target.checked;
-        // @ts-expect-error TS2339
-        const { param, ...state } = this.props.args;
+        const { param, ...state } = args;
         const newState = { ...state, param: { ...param, hiddenInfo } };
-        this.props.onChange(newState);
+        onChange(newState);
     };
 
-    render() {
-        const {
-            p: polyglot,
-            // @ts-expect-error TS2339
-            args: { param },
-        } = this.props;
-        const { labelArray, hiddenInfo } = param || defaultArgs.param;
-        const label = labelArray.join(';');
+    const { param } = args || defaultArgs;
+    const { labelArray = [], hiddenInfo = false } = param || {};
+    const label = labelArray.join(';');
 
-        return (
-            <FormatGroupedFieldSet>
-                <FormatDataParamsFieldSet>
-                    <TextField
-                        // @ts-expect-error TS18046
-                        label={polyglot.t('param_labels')}
-                        multiline
-                        onChange={this.handleRequest}
-                        value={label}
-                        fullWidth
-                    />
-                </FormatDataParamsFieldSet>
-                <FormatDefaultParamsFieldSet defaultExpanded>
-                    <FormControlLabel
-                        control={
-                            <Checkbox
-                                onChange={this.handleHiddenInfo}
-                                checked={hiddenInfo}
-                            />
-                        }
-                        // @ts-expect-error TS18046
-                        label={polyglot.t('hidden_info')}
-                    />
-                </FormatDefaultParamsFieldSet>
-            </FormatGroupedFieldSet>
-        );
-    }
-}
+    return (
+        <FormatGroupedFieldSet>
+            <FormatDataParamsFieldSet>
+                <TextField
+                    label={translate('param_labels')}
+                    multiline
+                    onChange={handleRequest}
+                    value={label}
+                    fullWidth
+                />
+            </FormatDataParamsFieldSet>
+            <FormatDefaultParamsFieldSet defaultExpanded>
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            onChange={handleHiddenInfo}
+                            checked={Boolean(hiddenInfo)}
+                        />
+                    }
+                    label={translate('hidden_info')}
+                />
+            </FormatDefaultParamsFieldSet>
+        </FormatGroupedFieldSet>
+    );
+};
 
-export default translate(LodexFieldAdmin);
+export default LodexFieldAdmin;
--- src/app/js/formats/text/sentence/SentenceAdmin.tsx
+++ src/app/js/formats/text/sentence/SentenceAdmin.tsx
@@ -1,8 +1,7 @@
-import { Component } from 'react';
 import { TextField } from '@mui/material';
 import { FormatDefaultParamsFieldSet } from '../../utils/components/field-set/FormatFieldSets';
 import FormatGroupedFieldSet from '../../utils/components/field-set/FormatGroupedFieldSet';
-import { translate } from '../../../i18n/I18NContext';
+import { useTranslate } from '../../../i18n/I18NContext';
 
 export const defaultArgs = {
     prefix: '',
@@ -15,56 +14,47 @@ interface SentenceAdminProps {
         suffix?: string;
     };
     onChange(...args: unknown[]): unknown;
-    p: unknown;
 }
 
-class SentenceAdmin extends Component<SentenceAdminProps> {
-    static defaultProps = {
-        args: defaultArgs,
+const SentenceAdmin = ({
+    args = defaultArgs,
+    onChange,
+}: SentenceAdminProps) => {
+    const { translate } = useTranslate();
+    // @ts-expect-error TS7006
+    const handlePrefix = (prefix) => {
+        const newArgs = { ...args, prefix };
+        onChange(newArgs);
     };
 
     // @ts-expect-error TS7006
-    handlePrefix = (prefix) => {
-        const newArgs = { ...this.props.args, prefix };
-        this.props.onChange(newArgs);
+    const handleSuffix = (suffix) => {
+        const newArgs = { ...args, suffix };
+        onChange(newArgs);
     };
 
-    // @ts-expect-error TS7006
-    handleSuffix = (suffix) => {
-        const newArgs = { ...this.props.args, suffix };
-        this.props.onChange(newArgs);
-    };
+    const { prefix, suffix } = args || defaultArgs;
 
-    render() {
-        const {
-            p: polyglot,
-            // @ts-expect-error TS2339
-            args: { prefix, suffix },
-        } = this.props;
+    return (
+        <FormatGroupedFieldSet>
+            <FormatDefaultParamsFieldSet defaultExpanded>
+                <TextField
+                    key="prefix"
+                    label={translate('prefix')}
+                    onChange={(e) => handlePrefix(e.target.value)}
+                    value={prefix}
+                    sx={{ flexGrow: 1 }}
+                />
+                <TextField
+                    key="suffix"
+                    label={translate('suffix')}
+                    onChange={(e) => handleSuffix(e.target.value)}
+                    value={suffix}
+                    sx={{ flexGrow: 1 }}
+                />
+            </FormatDefaultParamsFieldSet>
+        </FormatGroupedFieldSet>
+    );
+};
 
-        return (
-            <FormatGroupedFieldSet>
-                <FormatDefaultParamsFieldSet defaultExpanded>
-                    <TextField
-                        key="prefix"
-                        // @ts-expect-error TS18046
-                        label={polyglot.t('prefix')}
-                        onChange={(e) => this.handlePrefix(e.target.value)}
-                        value={prefix}
-                        sx={{ flexGrow: 1 }}
-                    />
-                    <TextField
-                        key="suffix"
-                        // @ts-expect-error TS18046
-                        label={polyglot.t('suffix')}
-                        onChange={(e) => this.handleSuffix(e.target.value)}
-                        value={suffix}
-                        sx={{ flexGrow: 1 }}
-                    />
-                </FormatDefaultParamsFieldSet>
-            </FormatGroupedFieldSet>
-        );
-    }
-}
-
-export default translate(SentenceAdmin);
+export default SentenceAdmin;
