f5fa4893d825d606c20d2db292b4af351d2a303b
ThieryMichel <thiery@marmelab.com>
2025-10-28 11:45:53 +0100

    display status and disable when unfinished in selector

--- packages/admin-app/src/enrichment/EnrichmentStatus.tsx
+++ packages/admin-app/src/enrichment/EnrichmentStatus.tsx
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { fromEnrichments } from '../selectors';
 import { Chip } from '@mui/material';
-import { TaskStatus } from '@lodex/common';
+import { labelByStatus } from '@lodex/common';
 import { useTranslate } from '@lodex/frontend-common/i18n/I18NContext';
 import type { State } from '../reducers';
 
@@ -18,70 +18,11 @@ type EnrichmentStatusProps = {
 
 export const EnrichmentStatus = ({ status }: EnrichmentStatusProps) => {
     const { translate } = useTranslate();
-    if (status === TaskStatus.PENDING) {
-        return (
-            <Chip
-                component="span"
-                label={translate('enrichment_status_pending')}
-                color="warning"
-            />
-        );
-    }
-    if (status === TaskStatus.IN_PROGRESS) {
-        return (
-            <Chip
-                component="span"
-                label={translate('enrichment_status_running')}
-                color="info"
-            />
-        );
-    }
-
-    if (status === TaskStatus.PAUSED) {
-        return (
-            <Chip
-                component="span"
-                label={translate('enrichment_status_paused')}
-                color="info"
-            />
-        );
-    }
-
-    if (status === TaskStatus.FINISHED) {
-        return (
-            <Chip
-                component="span"
-                label={translate('enrichment_status_done')}
-                color="success"
-            />
-        );
-    }
-
-    if (status === TaskStatus.ERROR) {
-        return (
-            <Chip
-                component="span"
-                label={translate('enrichment_status_error')}
-                color="error"
-            />
-        );
-    }
-
-    if (status === TaskStatus.CANCELED) {
-        return (
-            <Chip
-                component="span"
-                label={translate('enrichment_status_canceled')}
-                color="warning"
-            />
-        );
-    }
-
     return (
         <Chip
             component="span"
-            label={translate('enrichment_status_not_started')}
-            sx={{ backgroundColor: 'neutral' }}
+            label={translate(labelByStatus[status ?? ''])}
+            color="warning"
         />
     );
 };
