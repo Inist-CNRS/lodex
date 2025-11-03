0cd5dc39e834847e8c7b517cefa14722c6d59c31
ThieryMichel <thiery@marmelab.com>
2025-11-03 17:42:05 +0100

    fix run-dev

--- packages/public-app/src/annotation/CreateAnnotationButton.tsx
+++ packages/public-app/src/annotation/CreateAnnotationButton.tsx
@@ -17,7 +17,10 @@ import { MODE_ALL, MODE_CLOSED, MODE_MINE } from './HistoryDrawer.const';
 import { useCanAnnotate } from './useCanAnnotate';
 import { useCreateAnnotation } from './useCreateAnnotation';
 import { useResourceUri } from './useResourceUri';
-import { getReadableValue } from '@lodex/frontend-common/formats/getFormat';
+import {
+    getIsFieldValueAnnotable,
+    getReadableValue,
+} from '@lodex/frontend-common/formats/getFormat';
 
 interface UserAnnotationCountProps {
     fieldAnnotationIds: string[];
