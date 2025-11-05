5d3dc4d997a3a3903b79450727278648bfcdc9a6
ThieryMichel <thiery@marmelab.com>
2025-11-05 11:12:09 +0100

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -51,8 +51,8 @@ module.exports = {
         {
             displayName: 'public-app',
             rootDir: `${__dirname}/packages/public-app`,
-            setupFiles: [`${__dirname}/src/app/setupTest.ts`],
-            setupFilesAfterEnv: [`${__dirname}/src/app/setupTestAfterEnv.ts`],
+            setupFiles: [`${__dirname}/setupTest.ts`],
+            setupFilesAfterEnv: [`${__dirname}/setupTestAfterEnv.ts`],
             testMatch: [
                 '/**/*.spec.js',
                 '/**/*.spec.jsx',
