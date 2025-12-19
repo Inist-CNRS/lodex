2f9952a6c21cf68e33183e6a0e3b28fa89e6aa91
Nicolas Thouvenin <nthouvenin@gmail.com>
2025-12-19 07:43:43 +0100

    first test

---
+++ packages/ezsLodex/src/createVectorEmbeddings.ts
@@ -0,0 +1,27 @@
+import get from 'lodash/get.js';
+import set from 'lodash/set.js';
+import { pipeline } from "@huggingface/transformers";
+
+const createVectorEmbeddings = async (data: any, feed: any, ctx: any) => {
+    if (ctx.isLast()) {
+        return feed.close();
+    }
+    const path = Array().concat(ctx.getParam('path', 'value')).shift();
+    const value = get(data, path);
+    const extractor = await pipeline(
+        'feature-extraction',
+        'Xenova/all-MiniLM-L6-v2'
+    );
+    const result = await extractor(
+        [value],
+        {
+            pooling: "mean",
+            normalize: true,
+        }
+    );
+    set(data, path, result.data);
+    console.log({ data });
+    feed.send(data);
+}
+
+export default createVectorEmbeddings;
