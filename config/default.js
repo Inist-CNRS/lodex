ba258cc44eeed714635d6759bb00e5d00c1bb55b
Nicolas Thouvenin <nthouvenin@gmail.com>
2026-02-09 14:52:30 +0100

    first import

--- config/default.js
+++ config/default.js
@@ -101,6 +101,7 @@ module.exports = {
             'values-precomputed',
             'values-precomputed-nofilter',
             'raw-precomputed-nofilter',
+            'vsearch-precomputed,
             'group-and-sum-with',
             'hello-world',
             'istex-facet',
