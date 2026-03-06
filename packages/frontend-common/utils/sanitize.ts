0d3d9628a1afc401066b41693aaa072f55a506c0
Nicolas Thouvenin <nthouvenin@gmail.com>
2026-03-06 18:38:31 +0100

    Allow the target attribute for all formats that allow HTML input or generation.

--- packages/frontend-common/utils/sanitize.ts
+++ packages/frontend-common/utils/sanitize.ts
@@ -1,12 +1,20 @@
 import DOMPurify from 'dompurify';
 
+DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+    // set all elements owning target to target=_blank
+    if ('target' in node) {
+        node.setAttribute('target', '_blank');
+        node.setAttribute('rel', 'noopener');
+    }
+});
+
 export const sanitize = (dirty: unknown, defaultValue = ''): string => {
     if ([null, undefined, ''].includes(dirty?.toString())) {
         return defaultValue;
     }
 
     return DOMPurify.sanitize(dirty!.toString(), {
-        ALLOWED_ATTR: [],
+        ALLOWED_ATTR: ['target'],
         ALLOWED_TAGS: [],
     });
 };
