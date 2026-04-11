082387aeae4c78b7272353d6f207a229b31f94c4
Nicolas Thouvenin <nthouvenin@gmail.com>
2026-04-11 13:31:47 +0200

    allox internal link

--- packages/frontend-common/utils/sanitize.ts
+++ packages/frontend-common/utils/sanitize.ts
@@ -1,23 +1,22 @@
 import DOMPurify from 'dompurify';
 
 if (typeof window === 'object') {
-    // currently dompurify don't support NodeJS as a runtime
+    DOMPurify.removeHooks('afterSanitizeAttributes');
     DOMPurify.addHook('afterSanitizeAttributes', function (node) {
-        // set all elements owning target to target=_blank
-        if ('target' in node) {
-            node.setAttribute('target', '_blank');
+        if (node.tagName === 'A' && node.getAttribute('target') === '_blank') {
             node.setAttribute('rel', 'noopener');
         }
     });
 }
 
 export const sanitize = (dirty: unknown, defaultValue = ''): string => {
-    if ([null, undefined, ''].includes(dirty?.toString())) {
+    if (dirty == null || dirty === '') {
         return defaultValue;
     }
 
     return DOMPurify.sanitize(dirty!.toString(), {
-        ALLOWED_ATTR: ['target'],
-        ALLOWED_TAGS: [],
+        ALLOWED_TAGS: ['a'],
+        ALLOWED_ATTR: ['href', 'rel'],
+        ADD_ATTR: ['target'],
     });
 };
