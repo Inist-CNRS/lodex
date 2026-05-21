5a8aa3dca06108d2d6b5390bdc98a5360d1bbb34
Nicolas Thouvenin <nthouvenin@gmail.com>
2026-05-21 07:59:48 +0200

    lint

--- packages/ezsLodex/src/mongoDatabase.ts
+++ packages/ezsLodex/src/mongoDatabase.ts
@@ -10,7 +10,10 @@ async function mongoDatabase(connectionStringURI: any) {
     // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
     if (!handles[connectionStringURI]) {
         // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-        handles[connectionStringURI] = new MongoClient(connectionStringURI, global.MongoOptions || {});
+        handles[connectionStringURI] = new MongoClient(
+            connectionStringURI,
+            global.MongoOptions || {},
+        );
     }
     // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
     const client = await handles[connectionStringURI].connect();
