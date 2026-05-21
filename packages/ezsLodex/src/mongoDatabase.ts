dadeedd26605d8b63b229ba74fd8ea4d32cd2e59
Nicolas Thouvenin <nthouvenin@gmail.com>
2026-05-21 09:26:24 +0200

    type-check

--- packages/ezsLodex/src/mongoDatabase.ts
+++ packages/ezsLodex/src/mongoDatabase.ts
@@ -12,6 +12,7 @@ async function mongoDatabase(connectionStringURI: any) {
         // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
         handles[connectionStringURI] = new MongoClient(
             connectionStringURI,
+            // @ts-expect-error TS7017: Element implicitly has an 'any' type because type 'typeof globalThis' has no index signature.
             global.MongoOptions || {},
         );
     }
