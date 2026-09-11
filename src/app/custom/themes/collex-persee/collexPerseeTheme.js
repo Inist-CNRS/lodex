1b480f442025cb72265d3ff8d8bfdea47acc155a
HeleneCreusot <helene.creusot@inist.fr>
2026-09-11 14:07:32 +0200

    style(add colors theme and param index.ejs): mettre en route le thème, le tester

--- src/app/custom/themes/collex-persee/collexPerseeTheme.js
+++ src/app/custom/themes/collex-persee/collexPerseeTheme.js
@@ -15,59 +15,22 @@
 const palette = {
     mode: 'light',
     primary: {
-        main: '#c94d62',
+        main: '#8f152a',
         secondary: '#e58985',
-        light: 'rgba(253, 242, 237, 1)',
         contrastText: '#fff',
+        light: 'rgba(253, 242, 237, 1)',
     },
     secondary: {
-        main: '#8f152a',
+        main: '#c94d62',
         contrastText: '#fff',
     },
     info: {
-        main: '#c94d62',
-        contrastText: '#fff',
-    },
-    warning: {
-        main: '#f92f51',
-        contrastText: '#fff',
-    },
-    error: {
-        main: '#f92f51',
-        contrastText: '#fff',
-    },
-    success: {
-        main: '#7ab937',
-        contrastText: '#fff',
+        main: 'rgb(165, 67, 84)', //utilisé pour hover des liens
     },
     text: {
         primary: '#000',
-        main: 'rgb(95, 99, 104)', // Do not exist in mui
     },
-    contrastThreshold: 3, // Mui default (we need to look if is pertinent to change it)
-    // Do not exist in mui / Custom variable
-    danger: {
-        main: '#f92f51',
-        contrastText: '#fff',
-    },
-    neutral: {
-        main: 'rgba(245, 245, 245, 1)',
-    },
-    // Use mui palette.grey ???
-    neutralDark: {
-        main: '#555',
-        secondary: 'rgb(95, 99, 104)',
-        veryDark: '#333',
-        dark: '#555',
-        light: 'rgb(95, 99, 104, 0.5)',
-        lighter: 'rgb(95, 99, 104, 0.15)',
-        veryLight: 'rgb(95, 99, 104, 0.1)',
-        transparent: 'rgba(0, 0, 0, .1)',
-    },
-    contrast: {
-        main: '#fff',
-        light: '#e2e2e2',
-    },
+    contrastThreshold: 4, // Mui default (we need to look if is pertinent to change it)
 };
 
 export default {
